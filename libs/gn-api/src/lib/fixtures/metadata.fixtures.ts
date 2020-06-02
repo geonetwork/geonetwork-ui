91a7fb0a3a46f69bc0f27702b2241ac628144054
Florent gravin <florent.gravin@camptocamp.com>
2020-06-02 16:59:56 +0200

    feat: add models and api for geonetwork api

---
+++ libs/gn-api/src/lib/fixtures/metadata.fixtures.ts
@@ -0,0 +1,7 @@
+import { Metadata } from '../model/metadata'
+
+export const BasicMetadata: Metadata = {
+  uuid: 'reazrezafdsqfdqjlkm',
+  title: 'Metadata title',
+  abstract: 'Metadata abstract',
+}
---
+++ libs/gn-api/src/lib/model/metadata.ts
@@ -0,0 +1,5 @@
+export interface Metadata {
+  uuid: string
+  title?: string
+  abstract?: string
+}
