f7538dd573282b582acdf50c79ad516f70bd9f5e
AGF <arnaud.degroof@spacebel.be>
2020-06-02 15:53:40 +0200

    Add translation to the dropdown choice element

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { DropdownSelectorComponent } from './dropdown-selector/dropdown-selector.component'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { BrowserModule } from '@angular/platform-browser'
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [DropdownSelectorComponent],
-  imports: [NgbModule, BrowserModule],
+  imports: [NgbModule, BrowserModule, TranslateModule],
   exports: [DropdownSelectorComponent],
 })
 export class UiModule {}
