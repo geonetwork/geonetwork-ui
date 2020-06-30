43f20c9826304329d7c09e293007a02c46db972e
Florent gravin <florent.gravin@camptocamp.com>
2020-06-30 14:43:51 +0200

    chore: add script to generate openapi typescript client

---
+++ tools/generate-api.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -rf libs/gn-api/src/lib/openapi
+
+./node_modules/.bin/openapi-generator generate \
+  -i ./tools/spec.yml \
+  -g typescript-angular \
+  -o libs/gn-api/src/lib/openapi \
+  -c openapi-codegen-config.json \
+  --skip-validate-spec \
+
+sed -i "s/' | }/' }/" libs/gn-api/src/lib/openapi/**/*.ts
+sed -i "s/SetApiModel/Set/" libs/gn-api/src/lib/openapi/**/*.ts
+sed -i "s/import { Set } from '.\/set.api.model'//" libs/gn-api/src/lib/openapi/**/*.ts
+
+#prettier --write 'libs/gn-api/src/lib/openapi/**/*.ts'
