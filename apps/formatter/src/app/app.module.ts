c261e71d285da3d95e1b8fe0e1329da923194c70
Florent gravin <florent.gravin@camptocamp.com>
2020-06-30 18:45:43 +0200

    chore: add universal for ssr to formatter app

--- apps/formatter/src/app/app.module.ts
+++ apps/formatter/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
   providers: [],
   bootstrap: [AppComponent],
 })
