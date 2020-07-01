9fc49cc5a045f6eb0ac1968968c06b8110103133
Florent gravin <florent.gravin@camptocamp.com>
2020-07-01 10:51:06 +0200

    formatter: fetch and display metadata

--- apps/formatter/src/app/app.component.ts
+++ apps/formatter/src/app/app.component.ts
@@ -1,45 +1,41 @@
-import { Component } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { RecordSimple } from '@lib/search'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>Welcome to {{ title }}!</h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img
-        width="300"
-        alt="Angular Logo"
-        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
-      />
+    <div class="container" *ngIf="metadata">
+      <h1>{{ metadata.title }}</h1>
+      <p>{{ metadata.abstract }}</p>
     </div>
-    <h2>Here are some links to help you start:</h2>
-    <ul>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/tutorial"
-            >Tour of Heroes</a
-          >
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/cli"
-            >CLI Documentation</a
-          >
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://blog.angular.io/"
-            >Angular blog</a
-          >
-        </h2>
-      </li>
-    </ul>
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'formatter'
+export class AppComponent implements OnInit {
+  metadata: RecordSimple
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http
+      .get(
+        'https://apps.titellus.net/es/gn-records/_doc/8a078848-eff3-4cb2-bf4a-309a14279abb'
+      )
+      .pipe(
+        map((hit: any) => {
+          return {
+            title: hit._source.resourceTitleObject
+              ? hit._source.resourceTitleObject.default
+              : 'no title',
+            abstract: hit._source.resourceAbstractObject
+              ? hit._source.resourceAbstractObject.default
+              : 'no abstract',
+            thumbnailUrl: hit._source.overview ? hit._source.overview.url : '',
+            url: `/geonetwork/srv/eng/catalog.search#/metadata/${hit._source.uuid}`,
+          }
+        })
+      )
+      .subscribe((record) => (this.metadata = record))
+  }
 }
