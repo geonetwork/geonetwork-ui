3fb8d4a9dee6d13ebc5e103acff4ba6fee2f0902
Florent gravin <florent.gravin@camptocamp.com>
2020-07-01 10:53:01 +0200

    feat: use Universal HttpState
    avoid to have both request server side and client side

--- apps/formatter/src/app/app.module.ts
+++ apps/formatter/src/app/app.module.ts
@@ -1,11 +1,17 @@
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { TransferHttpCacheModule } from '@nguniversal/common'
 
 import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
+    TransferHttpCacheModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
