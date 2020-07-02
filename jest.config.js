0b5122d58e193b416666568806784123c2dccdb1
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-07-02 15:10:41 +0200

    chore: fix tests to work in CLI and IDE as well as output coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   moduleNameMapper: {
     '@lib/search': '<rootDir>libs/search/src/index.ts',
     '@lib/gn-api': '<rootDir>libs/gn-api/src/index.ts',
+    '@lib/ui': '<rootDir>libs/ui/src/index.ts',
+    '@lib/common': '<rootDir>libs/common/src/index.ts',
   },
 }
--- libs/common/jest.config.js
+++ libs/common/jest.config.js
@@ -1,7 +1,12 @@
 const { defaults } = require('jest-config')
 
 module.exports = {
-  verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: [
+    'libs/common/src/**/*.ts',
+    '!libs/common/src/public-api.ts',
+    '!libs/common/src/**/model.ts',
+    '!libs/common/src/**/*.module.ts',
+    '!libs/common/src/**/*.stories.ts',
+  ],
 }
