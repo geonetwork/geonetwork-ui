0f92860e5d489467f5cf13d8efebf72171b9b39e
Florent gravin <florent.gravin@camptocamp.com>
2020-09-30 15:29:32 +0200

    test: fix story webcomponent page

--- webcomponents/search-wc/stories/elements.stories.ts
+++ webcomponents/search-wc/stories/elements.stories.ts
@@ -8,8 +8,6 @@ import { NO_ERRORS_SCHEMA } from '@angular/core'
 import '../dist/runtime-es2015'
 import '../dist/main-es2015'
 import '../dist/polyfills-es2015'
-import '../dist/vendor-es2015'
-import '../dist/styles-es2015'
 
 const moduleMetadatas = {
   schemas: [NO_ERRORS_SCHEMA],
