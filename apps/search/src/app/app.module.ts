4cdd3466fd5be6dafd5da5fb130fcf72bbd07a1e
Florent gravin <florent.gravin@camptocamp.com>
2020-10-13 13:36:36 +0200

    chore: add ngx-translate-extract

--- apps/search/src/app/app.module.ts
+++ apps/search/src/app/app.module.ts
@@ -33,7 +33,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         useFactory: HttpLoaderFactory,
         deps: [HttpClient],
       },
-      defaultLanguage: 'en',
+      defaultLanguage: 'fr',
     }),
     LibSearchModule,
     StoreModule.forRoot({}, { metaReducers }),
