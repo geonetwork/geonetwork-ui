dfd406d1731c3718cbac5a107f1f82033815ce23
Michel Gabriël <michel.gabriel@geocat.net>
2020-10-14 14:16:44 +0200

    Add 3 new list items: card, item large and item small
    Introduce new class: `.clamp-x`

--- libs/ui/src/lib/record-preview-list/record-preview-list.stories.ts
+++ libs/ui/src/lib/record-preview-list/record-preview-list.stories.ts
@@ -20,10 +20,13 @@ RecordPreviewListComponentStory.args = {
   record: {
     uuid: 'uiiudiiddeaafdjsqmlkfdq',
     title: 'A very nice record',
+    category: 'My Category',
     thumbnailUrl:
       'https://sextant.ifremer.fr/var/storage/images/_aliases/listitem_thumbnail/medias-ifremer/medias-sextant/accueil/cartes-thematiques/adcp/1595636-3-fre-FR/ADCP.png',
-    abstract: 'this is a great abstract',
+    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices turpis nec eleifend ornare. Etiam ante est, pulvinar ac diam nec, sollicitudin tristique leo. Vivamus a dui sed ex mattis tempus non in urna. Phasellus vel ante a risus rhoncus finibus ac sed nisl. Sed sapien lorem, gravida sit amet congue maximus, lobortis a sapien.',
     url: 'www.goto.com',
+    more: 'Read more',
+    open: 'Open record',
   },
 }
 
