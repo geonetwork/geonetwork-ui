010d2771b18d1e81f86c31e9987740f5b1078b5f
François Prunayre <fx.prunayre@gmail.com>
2020-10-16 13:17:03 +0200

    Build / WC / Keep script simple

--- tools/build/webcomponent/build-all-wc.sh
+++ tools/build/webcomponent/build-all-wc.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-
-WC_FOLDER='webcomponents/'
-
-for d in ${WC_FOLDER}*/ ; do
-  WC_NAME=$(grep -oP '(?<='${WC_FOLDER}').*?(?=\/)' <<< $d)
-  if [[ $WC_NAME == gn-* ]]
-  then
-    echo "-- Build Web Component for:" $WC_NAME
-    ng build $WC_NAME
-  fi
+for c in webcomponents/gn-* ; do
+  echo "-- Build Web Component for:" `basename $c`
+  ng build `basename $c`
 done
