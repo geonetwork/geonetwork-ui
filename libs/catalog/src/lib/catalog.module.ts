bb8a184dc904a3446c52639d4325f4ac511926fb
Francois Prunayre <fx.prunayre@gmail.com>
2020-10-17 08:10:08 +0200

    Catalog title / Move siteApiService to common and subscribe and emit to make the dumb component to update.

--- libs/catalog/src/lib/catalog.module.ts
+++ libs/catalog/src/lib/catalog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { SiteTitleComponent } from './site-title/site-title.component'
-import { UiModule } from '../../../ui/src'
-import { GnApiModule } from '../../../gn-api/src'
+import { UiModule } from '@lib/ui'
+import { GnApiModule } from '@lib/gn-api'
 import { CommonModule } from '@angular/common'
 
 @NgModule({
--- libs/catalog/src/lib/site-title/site-title.component.ts
+++ libs/catalog/src/lib/site-title/site-title.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core'
-import { Observable } from 'rxjs'
-import { SettingsListResponseApiModel, SiteApiService } from '@lib/gn-api'
-import { shareReplay } from 'rxjs/operators'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { SettingsListResponseApiModel } from '@lib/gn-api'
+import { CommonService } from '@lib/common'
+import { BehaviorSubject } from 'rxjs'
 
 @Component({
   selector: 'catalog-site-title',
   templateUrl: './site-title.component.html',
   styleUrls: ['./site-title.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SiteTitleComponent implements OnInit {
-  siteInfo$: Observable<SettingsListResponseApiModel>
+  info: SettingsListResponseApiModel
+  infoEmitter$ = new BehaviorSubject<SettingsListResponseApiModel>(this.info)
 
-  constructor(private siteApiService: SiteApiService) {}
+  constructor(private commonService: CommonService) {}
 
   ngOnInit(): void {
-    this.siteInfo$ = this.siteApiService.get4().pipe(shareReplay())
+    this.commonService.siteInfoReady().subscribe((info) => {
+      this.info = info
+      this.infoEmitter$.next(this.info)
+    })
   }
 }
