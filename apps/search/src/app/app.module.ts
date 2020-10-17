85fb3f1a21f9eec3e45008c6e861ef4bad6346f8
Francois Prunayre <fx.prunayre@gmail.com>
2020-10-17 07:22:46 +0200

    Catalog lib / Register alias.

--- apps/search/src/app/app.module.ts
+++ apps/search/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { I18nModule } from '@lib/common'
 import { BASE_PATH } from '@lib/gn-api'
 import { LibSearchModule } from '@lib/search'
+import { LibCatalogModule } from '@lib/catalog'
 import { EffectsModule } from '@ngrx/effects'
 import { MetaReducer, StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
@@ -11,7 +12,6 @@ import { storeFreeze } from 'ngrx-store-freeze'
 import { environment } from '../environments/environment'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { LibCatalogModule } from '../../../../libs/catalog/src/lib/catalog.module'
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
   ? [storeFreeze]
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleNameMapper: {
     '@lib/search': '<rootDir>libs/search/src/index.ts',
+    '@lib/catalog': '<rootDir>libs/catalog/src/index.ts',
     '@lib/gn-api': '<rootDir>libs/gn-api/src/index.ts',
     '@lib/ui': '<rootDir>libs/ui/src/index.ts',
     '@lib/common': '<rootDir>libs/common/src/index.ts',
