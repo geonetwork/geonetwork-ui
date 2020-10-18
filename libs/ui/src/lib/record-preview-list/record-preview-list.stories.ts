bee97b07a3ad16da88c2e2ae9324b4fd1c9258a0
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-10-18 11:10:46 +0200

    Record preview / rework story to include knobs

--- libs/ui/src/lib/record-preview-list/record-preview-list.stories.ts
+++ libs/ui/src/lib/record-preview-list/record-preview-list.stories.ts
@@ -1,31 +1,30 @@
 import { withA11y } from '@storybook/addon-a11y'
-import { withKnobs } from '@storybook/addon-knobs'
+import { text, withKnobs } from '@storybook/addon-knobs'
 import { Meta, Story } from '@storybook/angular'
 import { RecordPreviewListComponent } from './record-preview-list.component'
 
 export default {
   title: 'UI/Record preview',
-  component: RecordPreviewListComponent,
   decorators: [withKnobs, withA11y],
 } as Meta
 
-const Template: Story<RecordPreviewListComponent> = (args) => ({
+export const RecordPreviewListComponentStory: Story<RecordPreviewListComponent> = () => ({
   component: RecordPreviewListComponent,
-  props: args,
-})
-
-export const RecordPreviewListComponentStory = Template.bind({})
-
-RecordPreviewListComponentStory.args = {
-  record: {
-    uuid: 'uiiudiiddeaafdjsqmlkfdq',
-    title: 'A very nice record',
-    thumbnailUrl:
-      'https://sextant.ifremer.fr/var/storage/images/_aliases/listitem_thumbnail/medias-ifremer/medias-sextant/accueil/cartes-thematiques/adcp/1595636-3-fre-FR/ADCP.png',
-    abstract:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus euismod libero, eu ullamcorper nisl placerat sit amet. Nulla vel sapien odio. Integer convallis scelerisque lorem, eget ultricies elit ultrices sit amet. Mauris nunc felis, vulputate laoreet lacinia et, volutpat et ligula. Sed a magna et augue convallis pretium. Fusce euismod dui in sapien tincidunt aliquet. Curabitur porttitor mauris a bibendum eleifend.',
-    url: 'www.goto.com',
+  props: {
+    record: {
+      uuid: 'uiiudiiddeaafdjsqmlkfdq',
+      title: text('Title', 'A very nice record'),
+      thumbnailUrl: text(
+        'Thumbnail URL',
+        'https://sextant.ifremer.fr/var/storage/images/_aliases/listitem_thumbnail/medias-ifremer/medias-sextant/accueil/cartes-thematiques/adcp/1595636-3-fre-FR/ADCP.png'
+      ),
+      abstract: text(
+        'Abstract',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus euismod libero, eu ullamcorper nisl placerat sit amet. Nulla vel sapien odio. Integer convallis scelerisque lorem, eget ultricies elit ultrices sit amet. Mauris nunc felis, vulputate laoreet lacinia et, volutpat et ligula. Sed a magna et augue convallis pretium. Fusce euismod dui in sapien tincidunt aliquet. Curabitur porttitor mauris a bibendum eleifend.'
+      ),
+      url: text('Record URL', 'www.goto.com'),
+    },
   },
-}
+})
 
 RecordPreviewListComponentStory.storyName = 'List record'
