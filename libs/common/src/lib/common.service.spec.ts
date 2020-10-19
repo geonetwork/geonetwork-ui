594b86168918d04481896ede2eb105bbef4f4303
Francois Prunayre <fx.prunayre@gmail.com>
2020-10-19 09:08:18 +0200

    Catalog title / Tentative test
    Fails with
    ```
    Test suite failed to run
    
        Configuration error:
    
        Could not locate module @lib/gn-api mapped as:
        /data/dev/gn/ui/libs/common/libs/gn-api/src/index.ts.
    
        Please check your configuration for these entries:
        {
          "moduleNameMapper": {
            "/@lib\/gn-api/": "/data/dev/gn/ui/libs/common/libs/gn-api/src/index.ts"
          },
          "resolver": undefined
        }
    
          1 | import { Injectable } from '@angular/core'
        > 2 | import { SiteApiService } from '@lib/gn-api'
            | ^
          3 | import { map, shareReplay } from 'rxjs/operators'
          4 |
          5 | @Injectable({
    
          at createNoMappedModuleFoundError (../../node_modules/jest-resolve/build/index.js:553:17)
          at Object.<anonymous> (src/lib/common.service.ts:2:1)
    
    ```

--- libs/common/src/lib/common.service.spec.ts
+++ libs/common/src/lib/common.service.spec.ts
@@ -1,13 +1,33 @@
 import { TestBed } from '@angular/core/testing'
 
 import { CommonService } from './common.service'
+import { of } from 'rxjs'
+import { SiteApiService } from '@lib/gn-api'
+
+const siteInfoMock = {
+  get4: () =>
+    of({
+      'system/site/name': 'GeoNetwork 4',
+      'system/site/organization': 'titellus',
+      'system/site/siteId': '04fe602b-fc46-4e6b-ac5a-138c6153eceb',
+      'system/platform/version': '4.0.0',
+      'system/platform/subVersion': '0',
+    }),
+}
 
 describe('CommonService', () => {
   let service: CommonService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
-    service = TestBed.inject(CommonService)
+    TestBed.configureTestingModule({
+      providers: [
+        CommonService,
+        {
+          provide: SiteApiService,
+          useValue: siteInfoMock,
+        },
+      ],
+    })
   })
 
   it('should be created', () => {
