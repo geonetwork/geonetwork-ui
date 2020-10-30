64b6d6d0318f7f43d16bcfc308598f2480ff3bc9
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-10-30 16:46:16 +0100

    feat(facet): generate checkbox input component in facet module

---
+++ libs/ui/src/lib/facet/checkbox-input/checkbox-input.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ui-checkbox-input',
+  templateUrl: './checkbox-input.component.html',
+  styleUrls: ['./checkbox-input.component.css'],
+})
+export class CheckboxInputComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
