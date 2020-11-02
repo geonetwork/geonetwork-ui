bb3fab74eeac7eb4faeb5306b53a18e2e20dd6b7
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-02 14:02:59 +0100

    feat(checkbox-input): inputs, outputs and story

--- libs/ui/src/lib/facet/checkbox-input/checkbox-input.component.ts
+++ libs/ui/src/lib/facet/checkbox-input/checkbox-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 
 @Component({
   selector: 'ui-checkbox-input',
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./checkbox-input.component.css'],
 })
 export class CheckboxInputComponent implements OnInit {
+  @Input() label: string
+  @Input() count: number
+  @Input() selected: boolean
+  @Input() excluded: boolean
+
+  @Output() selectedChange = new EventEmitter<boolean>()
+  @Output() excludedChange = new EventEmitter<boolean>()
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelectedChange(value: boolean) {
+    this.selectedChange.emit(value)
+  }
+
+  onExcludedChange(value: boolean) {
+    this.excludedChange.emit(value)
+  }
 }
--- libs/ui/src/lib/facet/checkbox-input/checkbox-input.stories.ts
+++ libs/ui/src/lib/facet/checkbox-input/checkbox-input.stories.ts
@@ -1,8 +1,8 @@
 import { I18nModule } from '@lib/common'
 import { withA11y } from '@storybook/addon-a11y'
 import { action } from '@storybook/addon-actions'
-import { object, text, withKnobs } from '@storybook/addon-knobs'
-import { Meta, moduleMetadata, Story } from '@storybook/angular'
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs'
+import { Meta, moduleMetadata } from '@storybook/angular'
 import { CheckboxInputComponent } from './checkbox-input.component'
 
 const moduleMetadatas = {
@@ -10,11 +10,19 @@ const moduleMetadatas = {
 }
 
 export default {
-  title: 'UI',
+  title: 'UI/Facet',
   decorators: [moduleMetadata(moduleMetadatas), withKnobs, withA11y],
 } as Meta
 
 export const CheckboxInputStory = () => ({
   component: CheckboxInputComponent,
+  props: {
+    label: text('label', 'my label'),
+    count: number('count', 0),
+    selected: boolean('selected', false),
+    excluded: boolean('excluded', false),
+    selectedChange: action('output'),
+    excludedChange: action('output'),
+  },
 })
 CheckboxInputStory.storyName = 'Checkbox input'
