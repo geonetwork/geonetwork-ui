e686b9b5b7021c143c55ee26251b626abdbf46b6
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-02 17:29:17 +0100

    feat(list): inputs, outputs and story

--- libs/ui/src/lib/facet/list/list.component.ts
+++ libs/ui/src/lib/facet/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 
 @Component({
   selector: 'ui-list',
@@ -6,7 +6,41 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  @Input() title: string
+  @Input() canFilter: boolean
+  @Input() filter: string
+  @Input() model: any
+
+  @Output() filterChange = new EventEmitter<string>()
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onFilterChange(value: string) {
+    this.filterChange.emit(value)
+  }
+
+  onListItemSelectedChange(selected: boolean, item: any) {
+    // TODO: ???
+    console.log('onListItemSelectedChange', selected, item)
+  }
+
+  onListItemInvertedChange(inverted: boolean, item: any) {
+    // TODO: ???
+    console.log('onListItemInvertedChange', inverted, item)
+  }
+
+  onMoreClick() {
+    // TODO: ???
+    console.log('onMoreClick')
+  }
+
+  getListItems() {
+    return this.model.items
+  }
+
+  canShowMore() {
+    return this.model.more
+  }
 }
