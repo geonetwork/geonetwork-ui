c6cd23d163e51fe19242252e88770edcd4349873
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-02 13:58:57 +0100

    feat(facet): use forms module to manage components internal state

--- libs/ui/src/lib/facet/facet.module.ts
+++ libs/ui/src/lib/facet/facet.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { CheckboxInputComponent } from './checkbox-input/checkbox-input.component'
 
 @NgModule({
   declarations: [CheckboxInputComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class FacetModule {}
