029fa031433ec2de738ea87b2ab379deb51a5707
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-02 16:34:31 +0100

    feat(facet): generate list component in facet module

---
+++ libs/ui/src/lib/facet/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ui-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
