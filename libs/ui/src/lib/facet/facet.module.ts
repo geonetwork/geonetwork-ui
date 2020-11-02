7937d538806a6be12186865ba0a17e77dd800794
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-02 16:34:31 +0100

    feat(facet): generate list component in facet module

--- libs/ui/src/lib/facet/facet.module.ts
+++ libs/ui/src/lib/facet/facet.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { CheckboxInputComponent } from './checkbox-input/checkbox-input.component'
+import { ListComponent } from './list/list.component'
 
 @NgModule({
-  declarations: [CheckboxInputComponent],
+  declarations: [CheckboxInputComponent, ListComponent],
   imports: [CommonModule, FormsModule],
 })
 export class FacetModule {}
