b33400e6a6790b2acf7360c8b2feed2d15cd7386
y-vynohradov <05121995Dota2>
2020-11-03 13:41:08 +0200

    Task GSGEODPT43-97
    Create a new application apps/datafeeder
    Create a new called lib-editor in libs/editor

---
+++ libs/editor/src/lib/editor.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-editor',
+  template: `
+    <p>
+      editor works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/editor/src/lib/editor.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { EditorComponent } from './editor.component';
+
+
+
+@NgModule({
+  declarations: [EditorComponent],
+  imports: [
+  ],
+  exports: [EditorComponent]
+})
+export class EditorModule { }
