19ff371b01598f55702efa68c56649028d882b0b
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-06 15:46:19 +0100

    test(facets-container): minimal working test

--- libs/search/src/lib/facets/facets-container/facets-container.component.spec.ts
+++ libs/search/src/lib/facets/facets-container/facets-container.component.spec.ts
@@ -1,4 +1,8 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreModule } from '@ngrx/store'
+import { initialState, reducer, SEARCH_FEATURE_KEY } from '../../state/reducer'
 
 import { FacetsContainerComponent } from './facets-container.component'
 
@@ -9,6 +13,14 @@ describe('FacetsContainerComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [FacetsContainerComponent],
+      imports: [
+        EffectsModule.forRoot(),
+        StoreModule.forRoot({}),
+        StoreModule.forFeature(SEARCH_FEATURE_KEY, reducer, {
+          initialState,
+        }),
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
   }))
 
