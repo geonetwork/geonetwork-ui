2661b55906eb1d3e33fdd818c892eb6ef80f4795
y-vynohradov <05121995Dota2>
2020-11-17 15:19:10 +0200

    chore: Add color service. Add analysis. progress page.  Add new page to

---
+++ apps/datafeeder/src/app/presentation/pages/analysis-progress-page/analysis-progress.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { LogService } from '@lib/common'
+import { interval, Subscription } from 'rxjs'
+import { take } from 'rxjs/operators'
+import { marker } from '@biesbjerg/ngx-translate-extract-marker'
+
+marker('datafeeder.analysisProgressBar.title')
+marker('datafeeder.analysisProgressBar.subtitle')
+
+@Component({
+  selector: 'app-analysis-progress-page',
+  templateUrl: './analysis-progress.page.html',
+  styleUrls: ['./analysis-progress.page.css'],
+})
+export class AnalysisProgressPageComponent implements OnInit, OnDestroy {
+  progress = 0
+  private routeParamsSub: Subscription
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private logService: LogService
+  ) {}
+
+  ngOnInit(): void {
+    this.routeParamsSub = this.activatedRoute.params.subscribe(({ id }) => {
+      this.logService.log(`id: ${id}`)
+    })
+
+    interval(250)
+      .pipe(take(100))
+      .subscribe((value) => {
+        this.progress = Math.floor((value * 100) / 100)
+      })
+  }
+
+  ngOnDestroy() {
+    this.routeParamsSub.unsubscribe()
+  }
+}
