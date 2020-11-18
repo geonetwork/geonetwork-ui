29136986702b57ac975e7b9b07db2c908d214697
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-18 10:09:29 +0100

    refactor(storybook): use fixture for facet block

--- libs/ui/src/lib/facets/facet-block/facet-block.stories.ts
+++ libs/ui/src/lib/facets/facet-block/facet-block.stories.ts
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions'
 import { boolean, object, text, withKnobs } from '@storybook/addon-knobs'
 import { Meta, moduleMetadata } from '@storybook/angular'
 import { FacetItemComponent } from '../facet-item/facet-item.component'
+import { BLOCK_MODEL_FIXTURE } from '../fixtures'
 import { FacetBlockComponent } from './facet-block.component'
 
 const moduleMetadatas = {
@@ -22,38 +23,7 @@ export const FacetBlockStory = () => ({
     title: text('title', 'my title'),
     canFilter: boolean('canFilter', true),
     filter: text('filter', ''),
-    model: object('excluded', {
-      key: 'tag',
-      items: [
-        { value: 'Hungary', count: 20, path: ['tag', 'Hungary'] },
-        { value: 'Austria', count: 17, path: ['tag', 'Austria'] },
-        { value: 'Belgium', count: 17, path: ['tag', 'Belgium'] },
-        { value: 'Bulgaria', count: 17, path: ['tag', 'Bulgaria'] },
-        { value: 'Croatia', count: 17, path: ['tag', 'Croatia'] },
-        { value: 'Cyprus', count: 17, path: ['tag', 'Cyprus'] },
-        { value: 'Czechia', count: 17, path: ['tag', 'Czechia'] },
-        { value: 'Denmark', count: 17, path: ['tag', 'Denmark'] },
-        { value: 'Estonia', count: 17, path: ['tag', 'Estonia'] },
-        { value: 'Finland', count: 17, path: ['tag', 'Finland'] },
-        { value: 'France', count: 17, path: ['tag', 'France'] },
-        { value: 'Germany', count: 17, path: ['tag', 'Germany'] },
-        { value: 'Italy', count: 17, path: ['tag', 'Italy'] },
-        { value: 'Latvia', count: 17, path: ['tag', 'Latvia'] },
-        { value: 'Luxembourg', count: 17, path: ['tag', 'Luxembourg'] },
-        { value: 'Malta', count: 17, path: ['tag', 'Malta'] },
-        { value: 'Netherlands', count: 17, path: ['tag', 'Netherlands'] },
-        { value: 'Poland', count: 17, path: ['tag', 'Poland'] },
-        { value: 'Portugal', count: 17, path: ['tag', 'Portugal'] },
-        { value: 'Romania', count: 17, path: ['tag', 'Romania'] },
-        { value: 'Slovakia', count: 17, path: ['tag', 'Slovakia'] },
-      ],
-      path: ['tag'],
-      type: 'terms',
-      size: 21,
-      more: true,
-      includeFilter: true,
-      excludeFilter: false,
-    }),
+    model: object('excluded', BLOCK_MODEL_FIXTURE),
     filterChange: action('output'),
   },
 })
