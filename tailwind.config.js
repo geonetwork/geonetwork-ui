6341c69b39e16c6a016d2d5f960a11fc4750b9f1
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-18 10:56:46 +0100

    feat(facet-item): change color, pointer, visibility on group hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,9 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    visibility: ['responsive', 'group-hover'],
+  },
   plugins: [],
 }
