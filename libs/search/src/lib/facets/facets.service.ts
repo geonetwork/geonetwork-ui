23829223a20e5e73cbbef2306f307eeb2d30f49b
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2020-11-20 15:57:52 +0100

    fix: lint errors

--- libs/search/src/lib/facets/facets.service.ts
+++ libs/search/src/lib/facets/facets.service.ts
@@ -15,44 +15,46 @@ export class FacetsService {
     isNested: boolean,
     path: string[] = []
   ) {
-    if (requestAggregations == undefined) {
+    if (requestAggregations === undefined) {
       return
     }
     const listModel = []
-    for (let key in responseAggregations) {
-      const requestAgg = requestAggregations[key]
-      const responseAgg = responseAggregations[key]
+    for (const key in responseAggregations) {
+      if (responseAggregations.hasOwnProperty(key)) {
+        const requestAgg = requestAggregations[key]
+        const responseAgg = responseAggregations[key]
 
-      let blockModel: any = {
-        key,
-        items: [],
-        path: [...path, key],
-      }
-      if (requestAgg.hasOwnProperty(AggreationsTypesEnum.TERMS)) {
-        blockModel = {
-          ...blockModel,
-          type: AggreationsTypesEnum.TERMS,
-          size: requestAgg[AggreationsTypesEnum.TERMS].size,
-          more: responseAgg.sum_other_doc_count > 0,
-          includeFilter: requestAgg.terms.include !== undefined,
-          excludeFilter: requestAgg.terms.exclude !== undefined,
+        let blockModel: any = {
+          key,
+          items: [],
+          path: [...path, key],
         }
-
-        responseAgg.buckets.forEach((bucket) => {
-          if (bucket.key) {
-            const value = bucket.key_as_string || bucket.key
-            const itemPath = [...blockModel.path, String(value)]
-            const itemModel = {
-              value,
-              meta: bucket.meta,
-              count: bucket.doc_count,
-              path: itemPath,
-            }
-            blockModel.items.push(itemModel)
+        if (requestAgg.hasOwnProperty(AggreationsTypesEnum.TERMS)) {
+          blockModel = {
+            ...blockModel,
+            type: AggreationsTypesEnum.TERMS,
+            size: requestAgg[AggreationsTypesEnum.TERMS].size,
+            more: responseAgg.sum_other_doc_count > 0,
+            includeFilter: requestAgg.terms.include !== undefined,
+            excludeFilter: requestAgg.terms.exclude !== undefined,
           }
-        })
+
+          responseAgg.buckets.forEach((bucket) => {
+            if (bucket.key) {
+              const value = bucket.key_as_string || bucket.key
+              const itemPath = [...blockModel.path, String(value)]
+              const itemModel = {
+                value,
+                meta: bucket.meta,
+                count: bucket.doc_count,
+                path: itemPath,
+              }
+              blockModel.items.push(itemModel)
+            }
+          })
+        }
+        listModel.push(blockModel)
       }
-      listModel.push(blockModel)
     }
     return listModel
   }
