753ee0ed6d3335d72a082b150a478b9e053c5aa3
Florent gravin <florent.gravin@camptocamp.com>
2020-11-20 17:05:56 +0100

    test(thumbnail): add knobs to the story

--- libs/ui/src/lib/record-thumbnail/record-thumbnail.component.ts
+++ libs/ui/src/lib/record-thumbnail/record-thumbnail.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { RecordSummary } from '@lib/common'
 
 @Component({
   selector: 'ui-record-thumbnail',
--- libs/ui/src/lib/record-thumbnail/record-thumbnail.stories.ts
+++ libs/ui/src/lib/record-thumbnail/record-thumbnail.stories.ts
@@ -1,24 +1,19 @@
 import { withA11y } from '@storybook/addon-a11y'
-import { withKnobs } from '@storybook/addon-knobs'
-import { Meta, Story } from '@storybook/angular'
+import { text, withKnobs } from '@storybook/addon-knobs'
 import { RecordThumbnailComponent } from './record-thumbnail.component'
 
 export default {
   title: 'UI/Record preview',
-  component: RecordThumbnailComponent,
   decorators: [withKnobs, withA11y],
-} as Meta
-
-const Template: Story<RecordThumbnailComponent> = (args) => ({
-  component: RecordThumbnailComponent,
-  props: args,
-})
-
-export const RecordThumbnailComponentStory = Template.bind({})
-
-RecordThumbnailComponentStory.args = {
-  thumbnailUrl:
-    'https://sextant.ifremer.fr/var/storage/images/_aliases/listitem_thumbnail/medias-ifremer/medias-sextant/accueil/cartes-thematiques/adcp/1595636-3-fre-FR/ADCP.png',
 }
 
+export const RecordThumbnailComponentStory = () => ({
+  component: RecordThumbnailComponent,
+  props: {
+    thumbnailUrl: text(
+      'url',
+      'https://sextant.ifremer.fr/var/storage/images/_aliases/listitem_thumbnail/medias-ifremer/medias-sextant/accueil/cartes-thematiques/adcp/1595636-3-fre-FR/ADCP.png'
+    ),
+  },
+})
 RecordThumbnailComponentStory.storyName = 'Record thumbnail'
