9072c32f8d20cec2f9cab9abe5a654c5503d7f74
Florent gravin <florent.gravin@camptocamp.com>
2020-11-25 15:34:05 +0200

    test: fix jest & openlayers

--- apps/datafeeder/src/app/presentation/components/data-import-validation-map-panel/data-import-validation-map-panel.component.spec.ts
+++ apps/datafeeder/src/app/presentation/components/data-import-validation-map-panel/data-import-validation-map-panel.component.spec.ts
@@ -1,4 +1,9 @@
+if (typeof global.URL.createObjectURL !== 'function') {
+  global.URL.createObjectURL = jest.fn()
+}
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { I18nModule } from '@lib/common'
 
 import { DataImportValidationMapPanelComponent } from './data-import-validation-map-panel.component'
 import { NO_ERRORS_SCHEMA } from '@angular/core'
@@ -10,6 +15,7 @@ describe('MapViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [I18nModule],
       declarations: [DataImportValidationMapPanelComponent],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
@@ -18,6 +24,23 @@ describe('MapViewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DataImportValidationMapPanelComponent)
     component = fixture.componentInstance
+    component.headerLabel = 'title'
+    component.geoJson = {
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [100.0, 0.0],
+            [101.0, 0.0],
+            [101.0, 1.0],
+            [100.0, 1.0],
+            [100.0, 0.0],
+          ],
+        ],
+      },
+    }
+
     fixture.detectChanges()
   })
 
@@ -28,6 +51,6 @@ describe('MapViewComponent', () => {
   it('should display head title', () => {
     const el = fixture.debugElement.query(By.css('.header-label')).nativeElement
 
-    expect(el.textContent).toEqual(component.headerLabel)
+    expect(el.textContent).toEqual(' title ')
   })
 })
