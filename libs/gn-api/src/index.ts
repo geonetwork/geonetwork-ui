31989885aeb4c3cff0514254776878542ac6bf74
Florent gravin <florent.gravin@camptocamp.com>
2020-12-16 12:35:08 +0100

    'define one aliases per api

--- libs/gn-api/src/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of gn-api
- */
-
-export * from './lib/gn4/gn-api.module'
-export * from './lib/gn4/openapi/'
-export * from './lib/gn4/fixtures/'
---
+++ libs/gn-api/src/lib/gn4/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of gn-api
+ */
+
+export * from './gn-api.module'
+export * from './openapi/'
+export * from './fixtures/'
