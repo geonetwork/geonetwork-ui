c8683d9462e597a74a0c132e0b3e6c8087feceb7
Florent gravin <florent.gravin@camptocamp.com>
2020-12-17 09:59:46 +0100

    test: fix api modules aliases in jest

--- apps/datafeeder/src/app/presentation/components/upload-data/upload-data.component.spec.ts
+++ apps/datafeeder/src/app/presentation/components/upload-data/upload-data.component.spec.ts
@@ -1,8 +1,18 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { FileUploadApiService } from '@lib/datafeeder-api'
+import { TranslateModule } from '@ngx-translate/core'
+import { of } from 'rxjs'
 
 import { UploadDataComponent } from './upload-data.component'
-import { NO_ERRORS_SCHEMA } from '@angular/core'
-import { TranslateModule } from '@ngx-translate/core'
+
+const fileUploadApiServiceMock = {
+  uploadFiles: jest.fn(() =>
+    of({
+      jobId: 12,
+    })
+  ),
+}
 
 describe('UploadDataComponent', () => {
   let component: UploadDataComponent
@@ -13,6 +23,12 @@ describe('UploadDataComponent', () => {
       declarations: [UploadDataComponent],
       imports: [TranslateModule.forRoot()],
       schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        {
+          provide: FileUploadApiService,
+          useValue: fileUploadApiServiceMock,
+        },
+      ],
     }).compileComponents()
   }))
 
