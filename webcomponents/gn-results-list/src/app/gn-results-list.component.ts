72a6077b1b40715bb5c7f7636895aa7854c42649
Florent gravin <florent.gravin@camptocamp.com>
2020-12-17 12:07:08 +0100

    wc: make them watch input values
    based on Angular onChanges lifecycle, be carefull that wc execution is outside of an Angular zone
    meaning you need to run a detectChanges when expected

--- webcomponents/gn-results-list/src/app/gn-results-list.component.ts
+++ webcomponents/gn-results-list/src/app/gn-results-list.component.ts
@@ -1,12 +1,14 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Input,
+  SimpleChanges,
   ViewEncapsulation,
 } from '@angular/core'
 import { ResultsListLayout } from '@lib/common'
-import { SearchState, SetSearch } from '@lib/search'
-import { Store } from '@ngrx/store'
+import { getSearchResultsLoading, SearchState, SetSearch } from '@lib/search'
+import { select, Store } from '@ngrx/store'
 import { BaseComponent } from '../../../base.component'
 
 @Component({
@@ -21,14 +23,31 @@ export class GnResultsListComponent extends BaseComponent {
   @Input() size = 10
   @Input() filter = ''
 
-  constructor(private store: Store<SearchState>) {
+  constructor(
+    private store: Store<SearchState>,
+    private changeDetector: ChangeDetectorRef
+  ) {
     super()
   }
 
   ngOnInit(): void {
     super.ngOnInit()
+    setTimeout(() => {
+      // Be sure to update the source page when the state is updated
+      this.store.pipe(select(getSearchResultsLoading)).subscribe((v) => {
+        this.changeDetector.detectChanges()
+      })
+    })
+  }
+
+  private setSearch_() {
     this.store.dispatch(
       new SetSearch({ filters: { any: this.filter }, size: this.size })
     )
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    super.ngOnChanges(changes)
+    this.setSearch_()
   }
 }
