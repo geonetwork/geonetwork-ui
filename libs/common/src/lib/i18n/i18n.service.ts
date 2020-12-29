8f3c7adceeb21b4f1051ecb458d8b4f83255514d
Francois Prunayre <fx.prunayre@gmail.com>
2020-12-29 16:10:48 +0100

    i18n: Add loader for core translation + codelist
    * i18n / Load the main translations + codelist translations (mainly used for
    translating aggregation bucket values)
    * Aggs / Translate values (for all cl_*.key fields)
    * OpenAPI / Update to GN 4.0.2 and fix doc examples.

---
+++ libs/common/src/lib/i18n/i18n.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core'
+import { TranslateLoader } from '@ngx-translate/core'
+import { forkJoin, Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { SiteApiService, StandardsApiService } from '@lib/gn-api'
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TranslationService implements TranslateLoader {
+  constructor(
+    private http: HttpClient,
+    private standardApiService: StandardsApiService
+  ) {}
+
+  getTranslation(lang: string): Observable<any> {
+    const observables: Observable<any>[] = []
+    const translationFiles = [`/assets/i18n/${lang}.json`]
+
+    const core = this.http.get(translationFiles[0])
+    observables.push(core)
+    observables.push(
+      this.standardApiService.getCodelistsTranslations('iso19139', [
+        'gmd:MD_TopicCategoryCode',
+        'gmd:MD_ScopeCode',
+        'gmd:MD_MaintenanceFrequencyCode',
+        'gmd:MD_ProgressCode',
+        'gmd:DS_InitiativeTypeCode',
+        'gmd:MD_SpatialRepresentationTypeCode',
+      ])
+    )
+
+    return forkJoin(observables).pipe(
+      map((response) => {
+        return response.reduce((a, b) => {
+          return Object.assign(a, b)
+        })
+      })
+    )
+  }
+}
