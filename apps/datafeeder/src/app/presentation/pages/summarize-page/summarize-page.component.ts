ebc3ac5b90902f6dbb72954da6bccb102df06269
Antoine Abt <antoine@abt.im>
2021-02-03 14:11:32 +0100

    fix: make latest pages design coherent with previous, plug buttons

--- apps/datafeeder/src/app/presentation/pages/summarize-page/summarize-page.component.ts
+++ apps/datafeeder/src/app/presentation/pages/summarize-page/summarize-page.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-summarize-page',
   templateUrl: './summarize-page.component.html',
   styleUrls: ['./summarize-page.component.css'],
 })
-export class SummarizePageComponent implements OnInit {
-  constructor() {}
+export class SummarizePageComponent implements OnInit, OnDestroy {
+  private routeParamsSub: Subscription
+  private rootId: number
 
-  ngOnInit(): void {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.routeParamsSub = this.activatedRoute.params.subscribe(({ id }) => {
+      this.rootId = id
+    })
+  }
+
+  submit() {
+    this.router.navigate(['/', this.rootId, 'publish'])
+
+  }
+
+  previous() {
+    this.router.navigate(['/', this.rootId, 'step', 4])
+  }
+
+  ngOnDestroy() {
+    this.routeParamsSub.unsubscribe()
+  }
 }
