996dce724549743c53a1b17014b77ec08047ceda
Florent gravin <florent.gravin@camptocamp.com>
2021-02-24 09:24:49 +0100

    fix(result-list): unsubscribe subscriptions

--- libs/search/src/lib/results-list/results-list.container.component.ts
+++ libs/search/src/lib/results-list/results-list.container.component.ts
@@ -1,22 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
 import {
   InfiniteScrollModel,
   InfiniteScrollOptionsDefault,
   ResultsListLayout,
 } from '@lib/common'
+import { Subscription } from 'rxjs'
+import { distinctUntilChanged } from 'rxjs/operators'
 import { SearchFacade } from '../state/search.facade'
-import { pipe } from 'rxjs'
 
 @Component({
   selector: 'search-results-list-container',
   templateUrl: './results-list.container.component.html',
   styleUrls: ['./results-list.container.component.css'],
 })
-export class ResultsListContainerComponent implements OnInit {
+export class ResultsListContainerComponent implements OnInit, OnDestroy {
   @Input() layout: ResultsListLayout = ResultsListLayout.CARD
   @Input() scrollableOptions: InfiniteScrollModel = {}
 
   scrollableConfig: InfiniteScrollModel
+  subscription = new Subscription()
 
   constructor(public facade: SearchFacade) {}
 
@@ -26,12 +28,20 @@ export class ResultsListContainerComponent implements OnInit {
       ...this.scrollableOptions,
     }
     this.facade.setResultsLayout(this.layout)
-    this.facade.isEndOfResults$.subscribe((isTheEnd) => {
-      this.scrollableConfig.disabled = isTheEnd
-    })
+    this.subscription.add(
+      this.facade.isEndOfResults$
+        .pipe(distinctUntilChanged())
+        .subscribe((isTheEnd) => {
+          this.scrollableConfig.disabled = isTheEnd
+        })
+    )
   }
 
   onScrollDown() {
     this.facade.scroll()
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 }
