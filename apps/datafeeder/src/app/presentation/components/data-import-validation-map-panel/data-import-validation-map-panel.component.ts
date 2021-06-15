792cb08292ff0401f75af943f191aca21f91d961
Antoine Abt <antoine@abt.im>
2021-06-15 14:21:13 +0200

    chore: lint

--- apps/datafeeder/src/app/presentation/components/data-import-validation-map-panel/data-import-validation-map-panel.component.ts
+++ apps/datafeeder/src/app/presentation/components/data-import-validation-map-panel/data-import-validation-map-panel.component.ts
@@ -64,7 +64,7 @@ export class DataImportValidationMapPanelComponent
 
   addFeature() {
     if (!this.source) {
-      return;
+      return
     }
     this.source.clear()
     this.source.addFeatures(
