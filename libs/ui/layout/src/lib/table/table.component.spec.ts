9a80b7551491bec2d3ad79114dab43aa1bb3e3ab
Florent gravin <florent.gravin@camptocamp.com>
2021-06-21 11:08:52 +0200

    fix table test

--- libs/ui/layout/src/lib/table/table.component.spec.ts
+++ libs/ui/layout/src/lib/table/table.component.spec.ts
@@ -1,13 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { MatSortModule } from '@angular/material/sort'
+import { MatTableModule } from '@angular/material/table'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 import { TableComponent } from './table.component'
 
+const data = [
+  {
+    name: 'name 1',
+    id: 'id 1',
+    age: 15,
+  },
+  {
+    name: 'name 2',
+    id: 'id 2',
+    age: 10,
+  },
+  {
+    name: 'name 3',
+    id: 'id 3',
+    age: 55,
+  },
+]
 describe('TableComponent', () => {
   let component: TableComponent
   let fixture: ComponentFixture<TableComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, MatTableModule, MatSortModule],
       declarations: [TableComponent],
     }).compileComponents()
   })
@@ -15,6 +36,7 @@ describe('TableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableComponent)
     component = fixture.componentInstance
+    component.data = data
     fixture.detectChanges()
   })
 
