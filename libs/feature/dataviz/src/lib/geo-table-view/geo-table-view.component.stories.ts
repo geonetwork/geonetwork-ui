062c597bb39b3f866f1f395ad7e2939e3c2dab4d
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-08-13 13:49:53 +0200

    feat(demo): fix styles and geotable component in storybook

--- libs/feature/dataviz/src/lib/geo-table-view/geo-table-view.component.stories.ts
+++ libs/feature/dataviz/src/lib/geo-table-view/geo-table-view.component.stories.ts
@@ -4,13 +4,25 @@ import { UiMapModule } from '@geonetwork-ui/ui/map'
 import { FEATURE_COLLECTION_POINT_FIXTURE_4326 } from '@geonetwork-ui/util/shared'
 import { Meta, moduleMetadata, Story } from '@storybook/angular'
 import { GeoTableViewComponent } from './geo-table-view.component'
+import {
+  defaultMapOptions,
+  FEATURE_MAP_OPTIONS,
+  FeatureMapModule,
+} from '@geonetwork-ui/feature/map'
+import { UiLayoutModule } from '@geonetwork-ui/ui/layout'
 
 export default {
   title: 'Map/GeoTable',
   component: GeoTableViewComponent,
   decorators: [
     moduleMetadata({
-      imports: [HttpClientModule, UiMapModule, BrowserAnimationsModule],
+      imports: [
+        HttpClientModule,
+        UiMapModule,
+        UiLayoutModule,
+        FeatureMapModule,
+        BrowserAnimationsModule,
+      ],
     }),
   ],
 } as Meta<GeoTableViewComponent>
