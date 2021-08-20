ae3f101afae14154d3d39dd765ccba069b807d30
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-08-20 11:57:33 +0200

    chore: generate webcomponents app
    nx g @nrwl/angular:app --prefix wc
    
    Note that this reused the `webcomponents` dir at the root of the project

---
+++ apps/webcomponents/src/app/components/gn-aggregated-records/gn-aggregated-records.component.ts
@@ -0,0 +1,45 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Injector,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core'
+import { SearchFacade } from '@geonetwork-ui/feature/search'
+import { BaseComponent } from '../base.component'
+
+@Component({
+  selector: 'wc-gn-aggregated-records',
+  templateUrl: './gn-aggregated-records.html',
+  styleUrls: ['./gn-aggregated-records.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class GnAggregatedRecordsComponent
+  extends BaseComponent
+  implements OnInit
+{
+  @Input() aggregationField: string
+  @Input() aggregationMaxCount = 20
+  @Input() aggregationQueryString: string
+
+  activeFilter = null
+
+  constructor(injector: Injector) {
+    super(injector)
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit()
+  }
+
+  setFilter(value: string) {
+    this.activeFilter = `+${this.aggregationField}:"${value}"`
+    this.facade.updateFilters({ any: this.activeFilter })
+  }
+
+  clearFilter() {
+    this.activeFilter = null
+  }
+}
--- webcomponents/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { enableProdMode } from '@angular/core'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import { GnWcModule } from './app/gn-wc.module'
-
-enableProdMode()
-
-platformBrowserDynamic()
-  .bootstrapModule(GnWcModule)
-  .catch((err) => console.error(err))
