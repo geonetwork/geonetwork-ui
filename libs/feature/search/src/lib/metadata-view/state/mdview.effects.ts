1d359a478f2b2f2710e1fc14acfc81105f292de8
Florent gravin <florent.gravin@camptocamp.com>
2021-08-24 17:19:06 +0200

    feat: add effects to chain mdView full metadata loding

---
+++ libs/feature/search/src/lib/metadata-view/state/mdview.effects.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core'
+import { SearchApiService } from '@geonetwork-ui/data-access/gn4'
+import { ElasticsearchService } from '../../elasticsearch'
+import { ElasticsearchMapper } from '../../elasticsearch/mapper'
+import { EsSearchResponse } from '@geonetwork-ui/util/shared'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { of } from 'rxjs'
+import { catchError, map, switchMap } from 'rxjs/operators'
+import * as MdViewActions from './mdview.actions'
+
+@Injectable()
+export class MdViewEffects {
+  constructor(
+    private actions$: Actions,
+    private searchService: SearchApiService,
+    private esService: ElasticsearchService,
+    private esMapper: ElasticsearchMapper
+  ) {}
+
+  setPreview$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(MdViewActions.setPreview),
+      map(({ preview }) => MdViewActions.loadFull({ uuid: preview.uuid }))
+    )
+  )
+
+  loadFull$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(MdViewActions.loadFull),
+      switchMap(({ uuid }) =>
+        this.searchService.search(
+          'bucket',
+          JSON.stringify(this.esService.getMetadataByIdPayload(uuid))
+        )
+      ),
+      map((response: EsSearchResponse) => {
+        const records = this.esMapper.toRecords(response)
+        const full = records[0]
+        return MdViewActions.loadFullSuccess({ full })
+      }),
+      catchError((error) => of(MdViewActions.loadFullFailure({ error })))
+    )
+  )
+}
