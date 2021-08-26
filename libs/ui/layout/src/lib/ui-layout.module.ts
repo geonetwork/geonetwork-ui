bdcc49298326440fcf7a0bba1eb837a142b02d01
Florent gravin <florent.gravin@camptocamp.com>
2021-08-26 10:06:22 +0200

    feat: add dumb dataviz figure component

--- libs/ui/layout/src/lib/ui-layout.module.ts
+++ libs/ui/layout/src/lib/ui-layout.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { MatIconModule } from '@angular/material/icon'
 import { MatSortModule } from '@angular/material/sort'
 import { MatTableModule } from '@angular/material/table'
 import { TableComponent } from './table/table.component'
 import { MetadataPageComponent } from './metadata-page/metadata-page.component'
 import { ContentGhostComponent } from './content-ghost/content-ghost.component'
+import { FigureComponent } from './figure/figure.component'
 
 @NgModule({
-  imports: [CommonModule, MatTableModule, MatSortModule],
-  declarations: [TableComponent, MetadataPageComponent, ContentGhostComponent],
-  exports: [TableComponent, MetadataPageComponent, ContentGhostComponent],
+  imports: [CommonModule, MatTableModule, MatSortModule, MatIconModule],
+  declarations: [
+    TableComponent,
+    MetadataPageComponent,
+    ContentGhostComponent,
+    FigureComponent,
+  ],
+  exports: [
+    TableComponent,
+    MetadataPageComponent,
+    ContentGhostComponent,
+    FigureComponent,
+  ],
 })
 export class UiLayoutModule {}
