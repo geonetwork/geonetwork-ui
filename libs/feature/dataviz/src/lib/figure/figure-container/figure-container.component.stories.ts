9719007df2049e6cd6e0e5cfbd48ae2db42172d5
Florent gravin <florent.gravin@camptocamp.com>
2021-08-26 14:21:30 +0200

    feat: add figure-container component
    takes a dataset and an expression and compute the figure to pass to the dumb component

---
+++ libs/feature/dataviz/src/lib/figure/figure-container/figure-container.component.stories.ts
@@ -0,0 +1,52 @@
+import {
+  TABLE_ITEM_FIXTURE,
+  TABLE_ITEM_FIXTURE_HAB,
+  UiLayoutModule,
+} from '@geonetwork-ui/ui/layout'
+import {
+  moduleMetadata,
+  Story,
+  Meta,
+  componentWrapperDecorator,
+} from '@storybook/angular'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FigureContainerComponent } from './figure-container.component'
+
+export default {
+  title: 'Dataviz/FigureContainerComponent',
+  component: FigureContainerComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [UiLayoutModule, BrowserAnimationsModule],
+    }),
+    componentWrapperDecorator(
+      (story) => `<div style="max-width: 700px">${story}</div>`
+    ),
+  ],
+} as Meta<FigureContainerComponent>
+
+const Template: Story<FigureContainerComponent> = (
+  args: FigureContainerComponent
+) => ({
+  component: FigureContainerComponent,
+  props: args,
+})
+
+export const Sum = Template.bind({})
+Sum.args = {
+  title: 'Sum of inhabitants',
+  icon: 'maps_home_work',
+  unit: 'hab.',
+  expression: 'sum|pop',
+  dataset: TABLE_ITEM_FIXTURE_HAB,
+}
+
+export const Average = Template.bind({})
+Average.args = {
+  title: 'Average age of the population',
+  icon: 'group',
+  unit: 'years old',
+  expression: 'average|age',
+  digits: 3,
+  dataset: TABLE_ITEM_FIXTURE,
+}
---
+++ libs/feature/dataviz/src/lib/figure/figure-container/figure-container.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core'
+import { TableItemModel } from '@geonetwork-ui/ui/layout'
+import { FigureService } from '../figure-service.service'
+
+@Component({
+  selector: 'gn-ui-figure-container',
+  templateUrl: './figure-container.component.html',
+  styleUrls: ['./figure-container.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FigureContainerComponent implements OnChanges {
+  @Input() dataset: TableItemModel[]
+  @Input() expression: string
+  @Input() icon: string
+  @Input() title: string
+  @Input() unit: string
+  @Input() digits?: number = 2
+  figure: string
+
+  constructor(private service: FigureService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const figure = this.service
+      .compute(this.expression, this.dataset)
+      .toFixed(this.digits)
+    this.figure = parseFloat(figure).toString()
+  }
+}
