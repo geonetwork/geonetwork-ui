a68cf39bd2d4411d63cfd7611fb230dc5bebff45
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-08-27 12:05:31 +0200

    feat(es): add mapping for links & dates from index document

--- libs/feature/search/src/lib/elasticsearch/mapper/elasticsearch.field.mapper.ts
+++ libs/feature/search/src/lib/elasticsearch/mapper/elasticsearch.field.mapper.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core'
-import { MetadataUrlService, RecordSummary } from '@geonetwork-ui/util/shared'
+import {
+  MetadataUrlService,
+  MetadataRecord,
+  MetadataLink,
+} from '@geonetwork-ui/util/shared'
 
 type ESResponseSource = any
 
 type EsFieldMapperFn = (
-  output: RecordSummary,
+  output: MetadataRecord,
   source: ESResponseSource
-) => RecordSummary
+) => MetadataRecord
 @Injectable({
   providedIn: 'root',
 })
@@ -14,12 +18,12 @@ export class ElasticsearchFieldMapper {
   constructor(private metadataUrlService: MetadataUrlService) {}
 
   fields: Record<string, EsFieldMapperFn> = {
-    uuid: (output: RecordSummary, source: ESResponseSource) => {
+    uuid: (output: MetadataRecord, source: ESResponseSource) => {
       this.mapGenericField(output, source, 'uuid')
       output.metadataUrl = this.metadataUrlService.getUrl(source.uuid)
       return output
     },
-    resourceTitleObject: (output: RecordSummary, source: ESResponseSource) =>
+    resourceTitleObject: (output: MetadataRecord, source: ESResponseSource) =>
       this.mapTranslatedField(
         output,
         source,
@@ -27,7 +31,10 @@ export class ElasticsearchFieldMapper {
         'title',
         'no title'
       ),
-    resourceAbstractObject: (output: RecordSummary, source: ESResponseSource) =>
+    resourceAbstractObject: (
+      output: MetadataRecord,
+      source: ESResponseSource
+    ) =>
       this.mapTranslatedField(
         output,
         source,
@@ -35,47 +42,75 @@ export class ElasticsearchFieldMapper {
         'abstract',
         'no abstract'
       ),
-    overview: (output: RecordSummary, source: ESResponseSource) => {
+    overview: (output: MetadataRecord, source: ESResponseSource) => {
       const overview = this.getFirstValue(source.overview)
       output.thumbnailUrl = overview?.data || overview?.url || ''
       return output
     },
-    codelist_status_text: (output: RecordSummary, source: ESResponseSource) => {
+    codelist_status_text: (
+      output: MetadataRecord,
+      source: ESResponseSource
+    ) => {
       output.updateFrequency = this.getFirstValue(source.codelist_status_text)
       return output
     },
-    logo: (output: RecordSummary, source: ESResponseSource) => {
+    logo: (output: MetadataRecord, source: ESResponseSource) => {
       output.logoUrl = `/geonetwork${source.logo}`
       return output
     },
-    resourceDate: (output: RecordSummary, source: ESResponseSource) => {
+    resourceDate: (output: MetadataRecord, source: ESResponseSource) => {
       return output
     },
-    resourceIdentifier: (output: RecordSummary, source: ESResponseSource) => {
+    resourceIdentifier: (output: MetadataRecord, source: ESResponseSource) => {
       return output
     },
-    resourceLanguage: (output: RecordSummary, source: ESResponseSource) => {
+    resourceLanguage: (output: MetadataRecord, source: ESResponseSource) => {
       return output
     },
     resourceTemporalDateRange: (
-      output: RecordSummary,
+      output: MetadataRecord,
       source: ESResponseSource
     ) => {
       return output
     },
     resourceTemporalExtentDateRange: (
-      output: RecordSummary,
+      output: MetadataRecord,
       source: ESResponseSource
     ) => {
       return output
     },
-    resourceType: (output: RecordSummary, source: ESResponseSource) => {
+    resourceType: (output: MetadataRecord, source: ESResponseSource) => {
+      return output
+    },
+    creationDateForResource: (
+      output: MetadataRecord,
+      source: ESResponseSource
+    ) => {
+      this.mapDateField(
+        output,
+        source,
+        'creationDateForResource',
+        'dataCreatedOn'
+      )
+      return output
+    },
+    createDate: (output: MetadataRecord, source: ESResponseSource) => {
+      this.mapDateField(output, source, 'createDate', 'createdOn')
+      return output
+    },
+    changeDate: (output: MetadataRecord, source: ESResponseSource) => {
+      this.mapDateField(output, source, 'changeDate', 'updatedOn')
+      return output
+    },
+    link: (output: MetadataRecord, source: ESResponseSource) => {
+      this.mapLinks(output, source, true, 'dataLinks')
+      this.mapLinks(output, source, false, 'otherLinks')
       return output
     },
   }
 
   mapGenericField(
-    output: RecordSummary,
+    output: MetadataRecord,
     source: ESResponseSource,
     fieldName: string,
     outputPropName?: string
@@ -86,7 +121,7 @@ export class ElasticsearchFieldMapper {
   }
 
   mapTranslatedField(
-    output: RecordSummary,
+    output: MetadataRecord,
     source: ESResponseSource,
     fieldName: string,
     outputPropName?: string,
@@ -94,6 +129,40 @@ export class ElasticsearchFieldMapper {
   ) {
     outputPropName = outputPropName || fieldName
     output[outputPropName] = source[fieldName]?.default || defaultValue
+    return output
+  }
+
+  mapDateField(
+    output: MetadataRecord,
+    source: ESResponseSource,
+    fieldName: string,
+    outputPropName?: string
+  ) {
+    outputPropName = outputPropName || fieldName
+    output[outputPropName] = new Date(this.getFirstValue(source[fieldName]))
+    return output
+  }
+
+  mapLinks(
+    output: MetadataRecord,
+    source: ESResponseSource,
+    linksToData: boolean,
+    outputPropName?: string
+  ) {
+    const dataProtocols = ['OGC', 'FILE']
+    output[outputPropName] = source.link
+      .map((link) => ({
+        protocol: link.protocol,
+        url: link.url,
+        description: link.description,
+        name: link.name,
+      }))
+      .filter(
+        (link: MetadataLink) =>
+          ('protocol' in link &&
+            dataProtocols.some((p) => link.protocol.includes(p))) ===
+          linksToData
+      )
     return output
   }
 
