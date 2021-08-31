a9108ce03023933e801e1bdceea117d330ab960a
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-08-31 17:11:49 +0200

    feat: create data-fetcher lib (typescript only)
    nx g @nrwl/node:lib data-fetcher --directory util --buildable --publishable --importPath "@geonetwork-ui/data-fetcher" --simpleModuleName

---
+++ libs/util/data-fetcher/src/lib/data-fetcher.spec.ts
@@ -0,0 +1,7 @@
+import { dataFetcher } from './data-fetcher'
+
+describe('dataFetcher', () => {
+  it('should work', () => {
+    expect(dataFetcher()).toEqual('data-fetcher')
+  })
+})
---
+++ libs/util/data-fetcher/src/lib/data-fetcher.ts
@@ -0,0 +1,3 @@
+export function dataFetcher(): string {
+  return 'data-fetcher'
+}
