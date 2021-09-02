f85421e58cd29034d71e3102cc461d4d31aa8eb1
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-09-02 15:25:33 +0200

    feat(router): add search router module with predefined routes
    The router module will in turn dispatch actions to the other search-related
    states through effects

---
+++ libs/feature/search/src/lib/router/state/router.effects.spec.ts
@@ -0,0 +1,139 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { TestBed } from '@angular/core/testing'
+import { Router } from '@angular/router'
+import { provideMockActions } from '@ngrx/effects/testing'
+import { hot } from 'jasmine-marbles'
+import { Observable } from 'rxjs'
+import { Location } from '@angular/common'
+
+import * as fromActions from './router.actions'
+import { RouterGoActionPayload } from './router.actions'
+import * as fromEffects from './router.effects'
+import { Action } from '@ngrx/store'
+import { routerNavigationAction } from '@ngrx/router-store'
+import * as MdViewActions from '../../metadata-view/state/mdview.actions'
+import { MetadataRouteComponent, SearchRouteComponent } from '../constants'
+
+describe('RouterEffects', () => {
+  let router: Router
+  let location: Location
+  let effects: fromEffects.RouterEffects
+  let actions: Observable<Action>
+
+  const RouterMock = {
+    navigate: jest.fn(),
+  }
+  const LocationMock = {
+    back: jest.fn(),
+    forward: jest.fn(),
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        fromEffects.RouterEffects,
+        provideMockActions(() => actions),
+        {
+          provide: Router,
+          useValue: RouterMock,
+        },
+        {
+          provide: Location,
+          useValue: LocationMock,
+        },
+      ],
+    })
+
+    effects = TestBed.inject(fromEffects.RouterEffects)
+    router = TestBed.inject(Router)
+    location = TestBed.inject(Location)
+  })
+
+  describe('loadMetadata$', () => {
+    it('should dispatch a loadFullMetadata action', () => {
+      actions = hot('-a', {
+        a: routerNavigationAction({
+          payload: {
+            routerState: {
+              root: {
+                routeConfig: {
+                  component: MetadataRouteComponent,
+                },
+                params: {
+                  metadataUuid: 'abcdef',
+                },
+              },
+            },
+          },
+        } as any),
+      })
+      const expected = hot('-a', {
+        a: MdViewActions.loadFullMetadata({ uuid: 'abcdef' }),
+      })
+      expect(effects.loadMetadata$).toBeObservable(expected)
+    })
+  })
+
+  describe('closeMetadata$', () => {
+    it('should dispatch a loadFullMetadata action', () => {
+      actions = hot('-a', {
+        a: routerNavigationAction({
+          payload: {
+            routerState: {
+              root: {
+                routeConfig: {
+                  component: SearchRouteComponent,
+                },
+                params: {},
+              },
+            },
+          },
+        } as any),
+      })
+      const expected = hot('-a', {
+        a: MdViewActions.close(),
+      })
+      expect(effects.closeMetadata$).toBeObservable(expected)
+    })
+  })
+
+  describe('navigate$', () => {
+    it('should call router navigate', () => {
+      const payload: RouterGoActionPayload = {
+        path: '.',
+        query: {
+          id: 10,
+        },
+      }
+      actions = hot('--a', { a: fromActions.goAction(payload) })
+
+      effects.navigate$.subscribe(() => {
+        const { path, query: queryParams } = payload
+        expect(router.navigate).toHaveBeenCalledWith(path, {
+          queryParams,
+        })
+      })
+    })
+  })
+
+  describe('navigateBack$', () => {
+    it('should call location back', () => {
+      actions = hot('-a', { a: fromActions.backAction() })
+
+      effects.navigate$.subscribe(() => {
+        expect(location.back).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('navigateForward$', () => {
+    it('should call location forward', () => {
+      actions = hot('-a', { a: fromActions.forwardAction() })
+
+      effects.navigate$.subscribe(() => {
+        expect(location.forward).toHaveBeenCalled()
+      })
+    })
+  })
+})
---
+++ libs/feature/search/src/lib/router/state/router.effects.ts
@@ -0,0 +1,70 @@
+import { Location } from '@angular/common'
+import { Injectable } from '@angular/core'
+import { ActivatedRouteSnapshot, Router } from '@angular/router'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { tap } from 'rxjs/operators'
+
+import * as RouterActions from './router.actions'
+import * as MdViewActions from '../../metadata-view/state/mdview.actions'
+import { navigation } from '@nrwl/angular'
+import { MetadataRouteComponent, SearchRouteComponent } from '../constants'
+
+@Injectable()
+export class RouterEffects {
+  constructor(
+    private _actions$: Actions,
+    private _router: Router,
+    private _location: Location
+  ) {}
+
+  navigate$ = createEffect(
+    () =>
+      this._actions$.pipe(
+        ofType(RouterActions.goAction),
+        tap(({ path, query: queryParams }) => {
+          this._router.navigate([path], { queryParams })
+        })
+      ),
+    { dispatch: false }
+  )
+
+  loadMetadata$ = createEffect(() =>
+    this._actions$.pipe(
+      navigation(MetadataRouteComponent, {
+        run: (activatedRouteSnapshot: ActivatedRouteSnapshot) =>
+          MdViewActions.loadFullMetadata({
+            uuid: activatedRouteSnapshot.params.metadataUuid,
+          }),
+        onError(a: ActivatedRouteSnapshot, e) {
+          console.error('Navigation failed', e)
+        },
+      })
+    )
+  )
+
+  closeMetadata$ = createEffect(() =>
+    this._actions$.pipe(
+      navigation(SearchRouteComponent, {
+        run: () => MdViewActions.close(),
+      })
+    )
+  )
+
+  navigateBack$ = createEffect(
+    () =>
+      this._actions$.pipe(
+        ofType(RouterActions.backAction),
+        tap(() => this._location.back())
+      ),
+    { dispatch: false }
+  )
+
+  navigateForward$ = createEffect(
+    () =>
+      this._actions$.pipe(
+        ofType(RouterActions.forwardAction),
+        tap(() => this._location.forward())
+      ),
+    { dispatch: false }
+  )
+}
