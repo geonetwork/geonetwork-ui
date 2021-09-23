97425ef7f2c31e7fbeba3d114d9fa89ac7128eb2
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-09-23 14:37:50 +0200

    move ContentGhostComponent to ui-elements lib

--- libs/ui/elements/src/lib/ui-elements.module.ts
+++ libs/ui/elements/src/lib/ui-elements.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MetadataPageComponent } from './metadata-page/metadata-page.component'
+import { ContentGhostComponent } from './content-ghost/content-ghost.component'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MetadataPageComponent],
-  exports: [MetadataPageComponent],
+  declarations: [MetadataPageComponent, ContentGhostComponent],
+  exports: [MetadataPageComponent, ContentGhostComponent],
 })
 export class UiElementsModule {}
--- libs/ui/layout/src/lib/ui-layout.module.ts
+++ libs/ui/layout/src/lib/ui-layout.module.ts
@@ -4,12 +4,11 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatSortModule } from '@angular/material/sort'
 import { MatTableModule } from '@angular/material/table'
 import { TableComponent } from './table/table.component'
-import { ContentGhostComponent } from './content-ghost/content-ghost.component'
 import { FigureComponent } from './figure/figure.component'
 
 @NgModule({
   imports: [CommonModule, MatTableModule, MatSortModule, MatIconModule],
-  declarations: [TableComponent, ContentGhostComponent, FigureComponent],
-  exports: [TableComponent, ContentGhostComponent, FigureComponent],
+  declarations: [TableComponent, FigureComponent],
+  exports: [TableComponent, FigureComponent],
 })
 export class UiLayoutModule {}
