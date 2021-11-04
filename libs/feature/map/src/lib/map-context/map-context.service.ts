519feaefeb374a5bcf546fec5e12d0bccb750ae0
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-11-04 16:31:26 +0100

    feat: add extent to map context model and fit map to it

--- libs/feature/map/src/lib/map-context/map-context.service.ts
+++ libs/feature/map/src/lib/map-context/map-context.service.ts
@@ -29,6 +29,9 @@ export class MapContextService {
     }
     map.getLayers().clear()
     mapContext.layers.forEach((layer) => map.addLayer(this.createLayer(layer)))
+    if (mapContext.extent) {
+      map.getView().fit(mapContext.extent)
+    }
     return map
   }
 
