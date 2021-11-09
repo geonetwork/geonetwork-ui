7aa7b9a3144a1f3387d54dc0b0de39f0e89b52bc
Florent gravin <florent.gravin@camptocamp.com>
2021-11-09 12:16:45 +0100

    feat(resultsList): add an injectable configuration to define the layouts it's a map of configurations, each config specify a Component to be rendered and classes for the outerhtml

---
+++ libs/ui/search/src/lib/results-list/results-layout.config.ts
@@ -0,0 +1,39 @@
+import { InjectionToken, Type } from '@angular/core'
+import { ResultsListLayout } from '@geonetwork-ui/util/shared'
+import { RecordPreviewCardComponent } from '../record-preview-card/record-preview-card.component'
+import { RecordPreviewListComponent } from '../record-preview-list/record-preview-list.component'
+import { RecordPreviewTextComponent } from '../record-preview-text/record-preview-text.component'
+import { RecordPreviewTitleComponent } from '../record-preview-title/record-preview-title.component'
+import { RecordPreviewComponent } from '../record-preview/record-preview.component'
+
+const DEFAULT_ITEM_CLS = 'pb-4'
+const DEFAULT_CONTAINER_CLS = 'gap-4 p-4'
+
+export class ResultsLayoutConfigItem {
+  constructor(
+    public component: Type<RecordPreviewComponent>,
+    public itemClass: string = DEFAULT_ITEM_CLS,
+    public containerClass: string = DEFAULT_CONTAINER_CLS
+  ) {}
+}
+export type ResultsLayoutConfigModel = Record<string, ResultsLayoutConfigItem>
+
+export const RESULTS_LAYOUT_CONFIG =
+  new InjectionToken<ResultsLayoutConfigModel>('results-layout.config')
+
+export const DEFAULT_RESULTS_LAYOUT_CONFIG: ResultsLayoutConfigModel = {
+  [ResultsListLayout.LIST]: new ResultsLayoutConfigItem(
+    RecordPreviewListComponent
+  ),
+  [ResultsListLayout.CARD]: new ResultsLayoutConfigItem(
+    RecordPreviewCardComponent,
+    '',
+    'grid md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'
+  ),
+  [ResultsListLayout.TEXT]: new ResultsLayoutConfigItem(
+    RecordPreviewTextComponent
+  ),
+  [ResultsListLayout.TITLE]: new ResultsLayoutConfigItem(
+    RecordPreviewTitleComponent
+  ),
+}
