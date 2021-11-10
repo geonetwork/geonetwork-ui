82dccad9e93b1d349742e514e9af0aaa80432189
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-11-10 10:36:26 +0100

    feat(link-utils): add jpg to displayed file types

--- libs/feature/search/src/lib/utils/links/link-utils.spec.ts
+++ libs/feature/search/src/lib/utils/links/link-utils.spec.ts
@@ -4,26 +4,88 @@ import {
   checkFileFormat,
   getLinksWithEsriRestFormats,
 } from './link-utils'
+import { LINK_FIXTURES } from './link.fixtures'
 
 describe('link utils', () => {
   describe('#getDownloadFormat', () => {
-    describe('for a FILE link', () => {
-      it('returns download format', () => {
+    describe('for a csv FILE link', () => {
+      it('returns csv format', () => {
         expect(
-          getDownloadFormat(
-            {
-              description: 'Test file',
-              name: 'file.geojson',
-              protocol: 'WWW:DOWNLOAD',
-              url: 'http://example.com',
-            },
-            DownloadFormatType.FILE
-          )
-        ).toEqual('geojson')
+          getDownloadFormat(LINK_FIXTURES.dataCsv, DownloadFormatType.FILE)
+        ).toEqual('csv')
       })
     }),
-      describe('for a WFS link', () => {
-        it('returns download format', () => {
+      describe('for a geojson FILE link', () => {
+        it('returns geojson format', () => {
+          expect(
+            getDownloadFormat(
+              LINK_FIXTURES.geodataJson,
+              DownloadFormatType.FILE
+            )
+          ).toEqual('geojson')
+        })
+      }),
+      describe('for a json FILE link', () => {
+        it('returns json format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.dataJson, DownloadFormatType.FILE)
+          ).toEqual('json')
+        })
+      }),
+      describe('for a shapefile link', () => {
+        it('returns shp format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.geodataShp, DownloadFormatType.FILE)
+          ).toEqual('shp')
+        })
+      }),
+      describe('for a kml FILE link', () => {
+        it('returns kml format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.geodataKml, DownloadFormatType.FILE)
+          ).toEqual('kml')
+        })
+      }),
+      describe('for a geopackage FILE link', () => {
+        it('returns gpkg format', () => {
+          expect(
+            getDownloadFormat(
+              LINK_FIXTURES.geodataGpkg,
+              DownloadFormatType.FILE
+            )
+          ).toEqual('gpkg')
+        })
+      }),
+      describe('for an excel FILE link', () => {
+        it('returns excel format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.dataXlsx, DownloadFormatType.FILE)
+          ).toEqual('excel')
+        })
+      }),
+      describe('for a pdf FILE link', () => {
+        it('returns pdf format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.dataPdf, DownloadFormatType.FILE)
+          ).toEqual('pdf')
+        })
+      }),
+      describe('for a jpg FILE link', () => {
+        it('returns jpg format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.dataJpg, DownloadFormatType.FILE)
+          ).toEqual('jpg')
+        })
+      }),
+      describe('for a zip FILE link', () => {
+        it('returns zip format', () => {
+          expect(
+            getDownloadFormat(LINK_FIXTURES.dataZip, DownloadFormatType.FILE)
+          ).toEqual('zip')
+        })
+      }),
+      describe('for a WFS link with geojson format', () => {
+        it('returns WFS:geojson format', () => {
           expect(
             getDownloadFormat(
               {
---
+++ libs/feature/search/src/lib/utils/links/link.fixtures.ts
@@ -0,0 +1,108 @@
+export const LINK_FIXTURES = {
+  readmeLink: {
+    protocol: 'WWW:LINK',
+    description: 'Readme page',
+    url: 'http://envlit.ifremer.fr/resultats/quadrige',
+  },
+  doiLink: {
+    protocol: 'WWW:DOI',
+    description: 'DOI for the resource',
+    url: 'http://doi.org/123-456-678',
+  },
+  dataCsv: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in CSV format',
+    name: 'abc.csv',
+    url: 'http://my.server/files/abc.csv',
+  },
+  dataPdf: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in PDF format',
+    name: 'abc.pdf',
+    url: 'https://my.server/files/abc.pdf',
+  },
+  dataJpg: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in JPG format',
+    name: 'abc.jpg',
+    url: 'https://my.server/files/abc.jpg',
+  },
+  dataZip: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in ZIP format',
+    name: 'abc.zip',
+    url: 'https://my.server/files/abc.zip',
+  },
+  dataXls: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in XLS format',
+    name: 'abc.xls',
+    url: 'https://my.server/files/abc.xls',
+  },
+  dataXlsx: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in XLSX format',
+    name: 'abc.XLSX',
+    url: 'https://my.server/files/abc.XLSX',
+  },
+  dataJson: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Data in JSON format',
+    name: 'abc.json',
+    url: 'https://my.server/files/abc.json',
+  },
+  geodataJson: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Geographic data in GeoJSON format',
+    name: 'dataset.geojson',
+    url: 'http://my.server/files/geographic/dataset.geojson',
+  },
+  geodataKml: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Geographic data in KML format',
+    name: 'dataset.kml',
+    url: 'http://my.server/files/geographic/dataset.kml',
+  },
+  geodataGpkg: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Geographic data in geopackage format',
+    name: 'dataset.gpkg',
+    url: 'http://my.server/files/geographic/dataset.gpkg',
+  },
+  geodataShp: {
+    protocol: 'WWW:DOWNLOAD',
+    description: 'Geographic data in shapefile format',
+    name: 'dataset.shp',
+    url: 'http://my.server/files/geographic/dataset.zip',
+  },
+  geodataWms: {
+    protocol: 'OGC:WMS',
+    name: 'mylayer',
+    url: 'https://my.ogc.server/wms',
+  },
+  geodataWfs: {
+    protocol: 'OGC:WFS',
+    name: 'mylayer',
+    url: 'https://my.ogc.server/wfs',
+  },
+  geodataWms2: {
+    protocol: 'OGC:WMS',
+    name: 'myotherlayer',
+    url: 'https://my.ogc.server/wms',
+  },
+  geodataWfs2: {
+    protocol: 'OGC:WFS',
+    name: 'myotherlayer',
+    url: 'https://my.ogc.server/wfs',
+  },
+  geodataRest: {
+    protocol: 'ESRI:REST',
+    name: 'myrestlayer',
+    url: 'https://my.esri.server/FeatureServer',
+  },
+  maplayerRest: {
+    protocol: 'ESRI:REST',
+    name: 'myotherrestlayer',
+    url: 'https://my.esri.server/MapServer',
+  },
+}
