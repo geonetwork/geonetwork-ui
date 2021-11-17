1b9fe308f51422841e2f240e1ab3492726ea5b1b
Florent gravin <florent.gravin@camptocamp.com>
2021-11-17 10:35:33 +0100

    refactor(router): remove the navigate to /serach on RequestMoreResult action
    was to eager to led to side effects

--- libs/feature/router/src/lib/default/state/router.effects.ts
+++ libs/feature/router/src/lib/default/state/router.effects.ts
@@ -2,17 +2,12 @@ import { Location } from '@angular/common'
 import { Inject, Injectable } from '@angular/core'
 import { ActivatedRouteSnapshot, Router } from '@angular/router'
 import { MdViewActions } from '@geonetwork-ui/feature/record'
-import {
-  REQUEST_MORE_RESULTS,
-  RequestMoreResults,
-} from '@geonetwork-ui/feature/search'
 import { Actions, createEffect, ofType } from '@ngrx/effects'
 import { navigation } from '@nrwl/angular'
-import { filter, map, tap } from 'rxjs/operators'
+import { tap } from 'rxjs/operators'
 import { MetadataRouteComponent, SearchRouteComponent } from '../constants'
 import { ROUTER_CONFIG, RouterConfigModel } from '../router.module'
 import * as RouterActions from './router.actions'
-import { goAction } from './router.actions'
 
 @Injectable()
 export class RouterEffects {
@@ -61,25 +56,6 @@ export class RouterEffects {
       navigation(SearchRouteComponent, {
         run: () => MdViewActions.close(),
       })
-    )
-  )
-
-  /**
-   * This effect will navigate to the search page when a new
-   * search is launched
-   */
-  search$ = createEffect(() =>
-    this._actions$.pipe(
-      ofType(REQUEST_MORE_RESULTS),
-      filter(
-        (action: RequestMoreResults) =>
-          action.id === this.routerConfig.searchStateId
-      ),
-      map((action) =>
-        goAction({
-          path: 'search',
-        })
-      )
     )
   )
 
