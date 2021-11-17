793a907a12762803661c80247f358a27787699a5
Florent gravin <florent.gravin@camptocamp.com>
2021-11-17 18:13:45 +0100

    fix(autocomplete): handle selection on enter key press
    Material Autocomplete does not provide any good solution, the acceptable solution for now is:
    cancel the enterPress after a selection change (which can be from click or enter), then enterPress
    is enabled again after the user start typing new text, so he can press enter to trigger the search

--- libs/ui/inputs/src/lib/autocomplete/autocomplete.component.ts
+++ libs/ui/inputs/src/lib/autocomplete/autocomplete.component.ts
@@ -1,24 +1,31 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ElementRef,
   EventEmitter,
   Input,
+  OnDestroy,
   OnInit,
   Output,
   ViewChild,
 } from '@angular/core'
-import { Observable } from 'rxjs'
+import { FormControl } from '@angular/forms'
+import {
+  MatAutocomplete,
+  MatAutocompleteSelectedEvent,
+  MatAutocompleteTrigger,
+} from '@angular/material/autocomplete'
+import { Observable, ReplaySubject, Subscription } from 'rxjs'
 import {
   debounceTime,
   distinctUntilChanged,
   filter,
   finalize,
   switchMap,
+  take,
   tap,
 } from 'rxjs/operators'
-import { MatAutocompleteTrigger } from '@angular/material/autocomplete'
-import { FormControl } from '@angular/forms'
 
 export type AutcompleteItem = unknown
 
@@ -28,17 +35,21 @@ export type AutcompleteItem = unknown
   styleUrls: ['./autocomplete.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AutocompleteComponent implements OnInit {
+export class AutocompleteComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() placeholder: string
   @Input() action: (value: string) => Observable<AutcompleteItem[]>
   @Output() itemSelected = new EventEmitter<AutcompleteItem>()
   @Output() inputSubmited = new EventEmitter<string>()
   @ViewChild(MatAutocompleteTrigger) triggerRef: MatAutocompleteTrigger
+  @ViewChild(MatAutocomplete) autocomplete: MatAutocomplete
   @ViewChild('searchInput') inputRef: ElementRef<HTMLInputElement>
+  selectionSubject = new ReplaySubject<MatAutocompleteSelectedEvent>(1)
 
   searching: boolean
   suggestions$: Observable<AutcompleteItem[]>
   control = new FormControl()
+  subscription = new Subscription()
+  cancelEnter = true
 
   @Input() itemToStringFn: (AutcompleteItem) => string = (item) => item
   @Input() displayWithFn: (AutcompleteItem) => string = (item) => item
@@ -52,18 +63,38 @@ export class AutocompleteComponent implements OnInit {
       switchMap((value) => this.action(value)),
       finalize(() => (this.searching = false))
     )
+    this.subscription = this.control.valueChanges.subscribe((any) => {
+      if (any !== '') {
+        this.cancelEnter = false
+      }
+    })
+  }
+
+  ngAfterViewInit(): void {
+    this.autocomplete.optionSelected.subscribe(this.selectionSubject)
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 
   clear(): void {
     this.inputRef.nativeElement.value = ''
+    this.selectionSubject
+      .pipe(take(1))
+      .subscribe((selection) => selection && selection.option.deselect())
     this.inputRef.nativeElement.focus()
     this.triggerRef.closePanel()
   }
 
-  handleEnter(value: string) {
-    this.inputSubmited.emit(value)
+  handleEnter(any: string) {
+    if (!this.cancelEnter) {
+      this.inputSubmited.emit(any)
+      this.triggerRef.closePanel()
+    }
   }
-  handleSelection(item: AutcompleteItem) {
-    this.itemSelected.emit(item)
+  handleSelection(event: MatAutocompleteSelectedEvent) {
+    this.cancelEnter = true
+    this.itemSelected.emit(event.option.value)
   }
 }
