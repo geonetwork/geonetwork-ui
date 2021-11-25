4d291adcb58a7158901afdba4edda0f0ca3866e8
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-11-25 17:42:14 +0100

    feat(datahub): remove breadcrumb

--- apps/datahub/src/app/app.component.ts
+++ apps/datahub/src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component, ViewChild } from '@angular/core'
+import { Component } from '@angular/core'
 import { MdViewFacade } from '@geonetwork-ui/feature/record'
-import { RouterFacade } from '@geonetwork-ui/feature/router'
-import { ColorService, MetadataRecord } from '@geonetwork-ui/util/shared'
-import { combineLatest, of } from 'rxjs'
-import { map, switchMap } from 'rxjs/operators'
-import { MainSearchComponent } from './main-search/main-search.component'
+import { ColorService } from '@geonetwork-ui/util/shared'
+import { combineLatest } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'datahub-root',
@@ -19,26 +17,7 @@ export class AppComponent {
   isRecordOpened$ = this.isPresentOrLoading$.pipe(
     map(([present, loading]) => present || loading)
   )
-
-  breadcrumb$ = this.isPresentOrLoading$.pipe(
-    switchMap(([present, loading]) => {
-      if (present)
-        return this.mdViewFacade.metadata$.pipe(
-          map((md) => `Dataset > ${md.title}`)
-        )
-      if (loading) return of('Loading...')
-      return of('Search')
-    })
-  )
-  constructor(
-    private mdViewFacade: MdViewFacade,
-    private searchRouter: RouterFacade
-  ) {
+  constructor(private mdViewFacade: MdViewFacade) {
     ColorService.applyCssVariables('#0f4395', '#c2e9dc', '#212029', '#fdfbff')
-  }
-
-  onBCDatahubClick() {
-    this.searchRouter.goToSearch()
-    this.searchComponent.resetSearch()
   }
 }
