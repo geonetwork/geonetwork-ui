5ee359f14cbdf4626ef097853f66b3a9dc5cb005
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-12-01 15:39:23 +0100

    build(datahub): adapt dockerfile & entrypoint to work with new config file
    some settings in the config file can be overwritten at runtime, thus allowing
    and quick and easy configuration for people who don't want to mess with the
    actual config file (and it keeps backwards compatiblity as well!).

--- tools/docker/docker-entrypoint.sh
+++ tools/docker/docker-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-envsubst < /usr/share/nginx/html/${APP_NAME}/assets/env.template.js > /usr/share/nginx/html/${APP_NAME}/assets/env.js
+CONFIG_FILE=assets/configuration/default.toml
+
+# Modify the GN4 url and proxy path based on env variables (if defined)
+if [ ! -z "${GN4_API_URL}" ]
+then
+  sed -i "s%geonetwork4_api_url = \".*\"%geonetwork4_api_url = \"${GN4_API_URL}\"%" /usr/share/nginx/html/${APP_NAME}/${CONFIG_FILE}
+  echo "[INFO] Replaced GN4 url in conf with: ${GN4_API_URL}"
+fi
+if [ ! -z "${PROXY_PATH}" ]
+then
+  sed -i "s%proxy_path = \".*\"%proxy_path = \"${PROXY_PATH}\"%" /usr/share/nginx/html/${APP_NAME}/${CONFIG_FILE}
+  echo "[INFO] Replaced proxy path in conf with: ${PROXY_PATH}"
+fi
 
 exec "$@"
