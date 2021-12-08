a655d4c4c3d8d75c46965729968f5a1d7dbfac2b
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-12-08 12:51:12 +0100

    feat(datahub): if a proxy path is defined, disable worker usage in ogc-client
    Hopefully this will help working around issues with the Referer header not
    being present in outgoing HTTP requests

--- apps/datahub/src/main.ts
+++ apps/datahub/src/main.ts
@@ -3,13 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
-import { loadAppConfig } from '@geonetwork-ui/util/app-config'
+import { loadAppConfig, getGlobalConfig } from '@geonetwork-ui/util/app-config'
+import { enableFallbackWithoutWorker } from '@camptocamp/ogc-client'
 
 if (environment.production) {
   enableProdMode()
 }
 
 loadAppConfig().then(() => {
+  if (getGlobalConfig().PROXY_PATH) {
+    // disable worker in ogc-client to allow using a proxy with a Referer check
+    enableFallbackWithoutWorker()
+  }
+
   platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch((err) => console.error(err))
