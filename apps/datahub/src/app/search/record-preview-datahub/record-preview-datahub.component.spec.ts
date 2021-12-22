90a4a520b3be9e48285f014e585cc29bebb534d2
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-12-22 16:33:00 +0100

    refactor(record-preview): use record-thumbnail component to display placeholder

--- apps/datahub/src/app/search/record-preview-datahub/record-preview-datahub.component.spec.ts
+++ apps/datahub/src/app/search/record-preview-datahub/record-preview-datahub.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { UtilSharedModule } from '@geonetwork-ui/util/shared'
 
@@ -11,6 +12,7 @@ describe('RecordPreviewDatahubComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [RecordPreviewDatahubComponent],
       imports: [UtilSharedModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents()
   })
 
