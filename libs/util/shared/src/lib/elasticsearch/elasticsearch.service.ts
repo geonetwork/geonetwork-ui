674060a36ed5e618b568bb059601e0a2a1792d4a
Florent gravin <florent.gravin@camptocamp.com>
2022-02-01 19:05:07 +0100

    feat(search): escape special char from any field

--- libs/util/shared/src/lib/elasticsearch/elasticsearch.service.ts
+++ libs/util/shared/src/lib/elasticsearch/elasticsearch.service.ts
@@ -305,4 +305,11 @@ export class ElasticsearchService {
     }
     return queryString
   }
+
+  private escapeSpecialCharacters(querystring) {
+    return querystring.replace(
+      /(\+|-|&&|\|\||!|\{|\}|\[|\]\^|\~|\?|:|\\{1}|\(|\))/g,
+      '\\$1'
+    )
+  }
 }
