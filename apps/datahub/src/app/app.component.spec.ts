951ab822845057648ab145008ef732ab31b7ef7b
Florent gravin <florent.gravin@camptocamp.com>
2022-02-08 13:47:06 +0100

    refactor(router): use ng-outlet for routing components

--- apps/datahub/src/app/app.component.spec.ts
+++ apps/datahub/src/app/app.component.spec.ts
@@ -1,27 +1,12 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { TestBed } from '@angular/core/testing'
-import { MdViewFacade } from '@geonetwork-ui/feature/record'
 import { AppComponent } from './app.component'
-import { of } from 'rxjs'
-
-class MdViewFacadeMock {
-  close = jest.fn()
-  isPresent$ = of()
-  isLoading$ = of()
-  metadata$ = of()
-}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [
-        {
-          provide: MdViewFacade,
-          useClass: MdViewFacadeMock,
-        },
-      ],
     }).compileComponents()
   })
 
--- apps/datahub/src/app/app.component.ts
+++ apps/datahub/src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core'
-import { MdViewFacade } from '@geonetwork-ui/feature/record'
-import { combineLatest } from 'rxjs'
-import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'datahub-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  isPresentOrLoading$ = combineLatest([
-    this.mdViewFacade.isPresent$,
-    this.mdViewFacade.isLoading$,
-  ])
-  isRecordOpened$ = this.isPresentOrLoading$.pipe(
-    map(([present, loading]) => present || loading)
-  )
-  constructor(private mdViewFacade: MdViewFacade) {}
-}
+export class AppComponent {}
