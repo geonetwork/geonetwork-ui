4951f7dea86ded6e57dd0fee9b93ac12a1e4c5a7
Florent gravin <florent.gravin@camptocamp.com>
2022-02-09 12:17:58 +0100

    test(jest): update transformIgnorePatterns to ignore .mjs file

--- apps/search/jest.config.js
+++ apps/search/jest.config.js
@@ -18,5 +18,4 @@ module.exports = {
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
 }
--- libs/feature/router/jest.config.js
+++ libs/feature/router/jest.config.js
@@ -12,7 +12,6 @@ module.exports = {
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
