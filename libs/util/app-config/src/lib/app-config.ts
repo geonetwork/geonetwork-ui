611f0e6ae3059aeda639cee5547c06bf71ffc9e4
Olivier Guyot <olivier.guyot@camptocamp.com>
2022-02-23 21:21:17 +0100

    refactor(app-config): put some parsing code in a separate file, add more tests
    The logic for parsing sections is now reused and more thoroughly tested.
    
    The app config tests have been kept almost identical

--- libs/util/app-config/src/lib/app-config.ts
+++ libs/util/app-config/src/lib/app-config.ts
@@ -1,4 +1,9 @@
 import * as TOML from '@ltd/j-toml'
+import {
+  parseConfigSection,
+  parseMultiConfigSection,
+  parseTranslationsConfigSection,
+} from './parse-utils'
 
 const MISSING_CONFIG_ERROR = `Application configuration was not initialized correctly.
 This error might show up in case of an invalid/malformed configuration file. 
@@ -62,22 +67,6 @@ export function getCustomTranslations(langCode: string): CustomTranslations {
   return langCode in customTranslations ? customTranslations[langCode] : {}
 }
 
-function checkKeys(
-  input: Record<string, string>,
-  mandatory: string[],
-  optional: string[]
-) {
-  const keys = Object.keys(input)
-  const missing = mandatory.filter((key) => keys.indexOf(key) === -1)
-  const unrecognized = keys.filter(
-    (key) => mandatory.indexOf(key) === -1 && optional.indexOf(key) === -1
-  )
-  return {
-    missing,
-    unrecognized,
-  }
-}
-
 let appConfigLoaded = false
 
 export function loadAppConfig() {
@@ -95,79 +84,62 @@ export function loadAppConfig() {
           `An error occurred when parsing the configuration file: ${e.message}`
         )
       }
-      const {
-        global,
-        map,
-        map_layer,
-        theme,
-        translations: translationsNested,
-      } = parsed
       const errors = []
       const warnings = []
 
-      const globalCheck = checkKeys(
-        global || {},
+      const parsedGlobalSection = parseConfigSection(
+        parsed,
+        'global',
         ['geonetwork4_api_url'],
-        ['proxy_path']
+        ['proxy_path'],
+        warnings,
+        errors
       )
-      if (globalCheck.missing.length) {
-        errors.push(`In the [global] section: ${globalCheck.missing.join(', ')}
-`)
-      } else if (globalCheck.unrecognized.length) {
-        warnings.push(
-          `In the [global] section: ${globalCheck.unrecognized.join(', ')}`
-        )
-      }
+      globalConfig =
+        parsedGlobalSection === null
+          ? null
+          : ({
+              GN4_API_URL: parsedGlobalSection.geonetwork4_api_url,
+              PROXY_PATH: parsedGlobalSection.proxy_path,
+            } as GlobalConfig)
 
-      const mapCheck = checkKeys(
-        map || {},
+      const parsedLayersSections = parseMultiConfigSection(
+        parsed,
+        'map_layer',
+        ['type', 'url'],
+        ['name'],
+        warnings,
+        errors
+      )
+      const parsedMapSection = parseConfigSection(
+        parsed,
+        'map',
         [],
-        ['max_zoom', 'max_extent', 'baselayer', 'do_not_use_default_basemap']
+        ['max_zoom', 'max_extent', 'baselayer', 'do_not_use_default_basemap'],
+        warnings,
+        errors
       )
-      if (mapCheck.missing.length) {
-        errors.push(`In the [map] section: ${mapCheck.missing.join(', ')}`)
-      } else if (mapCheck.unrecognized.length) {
-        warnings.push(
-          `In the [map] section: ${mapCheck.unrecognized.join(', ')}`
-        )
-      }
+      mapConfig =
+        parsedMapSection === null || parsedLayersSections === null
+          ? null
+          : ({
+              MAX_ZOOM: parsedMapSection.max_zoom,
+              MAX_EXTENT: parsedMapSection.max_extent,
+              DO_NOT_USE_DEFAULT_BASEMAP:
+                !!parsedMapSection.do_not_use_default_basemap,
+              MAP_LAYERS: parsedLayersSections.map(
+                (map_layer) =>
+                  ({
+                    TYPE: map_layer.type,
+                    URL: map_layer.url,
+                    NAME: map_layer.name,
+                  } as LayerConfig)
+              ),
+            } as MapConfig)
 
-      let layersCheck = { missing: [], unrecognized: [] }
-      if (map_layer) {
-        map_layer.forEach((layer) => {
-          const { missing, unrecognized } = checkKeys(
-            layer || {},
-            ['type', 'url'],
-            ['name']
-          )
-          layersCheck = {
-            missing: [
-              ...layersCheck.missing,
-              ...(missing.length ? [missing] : []),
-            ],
-            unrecognized: [
-              ...layersCheck.unrecognized,
-              ...(unrecognized.length ? [unrecognized] : []),
-            ],
-          }
-        })
-        if (layersCheck.missing.length) {
-          errors.push(
-            `In one of the [map_layer] definitions: ${layersCheck.missing.join(
-              ', '
-            )}`
-          )
-        } else if (layersCheck.unrecognized.length) {
-          warnings.push(
-            `In one of the [map_layer] definitions: ${layersCheck.unrecognized.join(
-              ', '
-            )}`
-          )
-        }
-      }
-
-      const themeCheck = checkKeys(
-        theme || {},
+      const parsedThemeSection = parseConfigSection(
+        parsed,
+        'theme',
         [
           'primary_color',
           'secondary_color',
@@ -175,70 +147,36 @@ export function loadAppConfig() {
           'background_color',
           'header_background',
         ],
-        ['main_font', 'title_font', 'fonts_stylesheet_url']
+        ['main_font', 'title_font', 'fonts_stylesheet_url'],
+        warnings,
+        errors
       )
-      if (themeCheck.missing.length) {
-        errors.push(`In the [theme] section: ${themeCheck.missing.join(', ')}`)
-      } else if (themeCheck.unrecognized.length) {
-        warnings.push(
-          `In the [theme] section: ${themeCheck.unrecognized.join(', ')}`
-        )
-      }
+      themeConfig =
+        parsedThemeSection === null
+          ? null
+          : ({
+              PRIMARY_COLOR: parsedThemeSection.primary_color,
+              SECONDARY_COLOR: parsedThemeSection.secondary_color,
+              MAIN_COLOR: parsedThemeSection.main_color,
+              BACKGROUND_COLOR: parsedThemeSection.background_color,
+              HEADER_BACKGROUND: parsedThemeSection.header_background,
+              TITLE_FONT: parsedThemeSection.title_font,
+              MAIN_FONT: parsedThemeSection.main_font,
+              FONTS_STYLESHEET_URL: parsedThemeSection.fonts_stylesheet_url,
+            } as ThemeConfig)
 
-      if (errors.length)
+      customTranslations = parseTranslationsConfigSection(
+        parsed,
+        'translations'
+      ) as CustomTranslationsAllLanguages
+
+      if (errors.length) {
         throw new Error(`One or more mandatory settings were missing from the configuration file.
 ${errors.join('\n')}`)
-      else if (warnings.length)
+      } else if (warnings.length) {
         console.warn(`One or more unexpected settings were encountered in the configuration file.
 ${warnings.join('\n')}`)
-
-      // will flatten nested objects using dotted properties
-      const flatten = (base, obj, isFirst) =>
-        Object.keys(obj).reduce((prev, curr) => {
-          const path = base ? `${base}.${curr}` : curr
-          const val = obj[curr]
-          if (isFirst) return { ...prev, [path]: flatten('', val, false) }
-          else if (typeof val === 'object')
-            return { ...prev, ...flatten(path, val, false) }
-          else return { ...prev, [path]: val }
-        }, {})
-
-      // flatten translations by language
-      const translations = translationsNested
-        ? flatten('', translationsNested, true)
-        : {}
-
-      globalConfig = {
-        GN4_API_URL: global.geonetwork4_api_url,
-        PROXY_PATH: global.proxy_path,
       }
-      const layersConfig: LayerConfig[] = []
-      if (map_layer) {
-        map_layer.forEach((layerConfig) => {
-          layersConfig.push({
-            TYPE: layerConfig.type,
-            URL: layerConfig.url,
-            NAME: layerConfig.name,
-          })
-        })
-      }
-      mapConfig = {
-        MAX_ZOOM: map.max_zoom,
-        MAX_EXTENT: map.max_extent,
-        DO_NOT_USE_DEFAULT_BASEMAP: !!map.do_not_use_default_basemap,
-        MAP_LAYERS: layersConfig,
-      }
-      themeConfig = {
-        PRIMARY_COLOR: theme.primary_color,
-        SECONDARY_COLOR: theme.secondary_color,
-        MAIN_COLOR: theme.main_color,
-        BACKGROUND_COLOR: theme.background_color,
-        HEADER_BACKGROUND: theme.header_background,
-        TITLE_FONT: theme.title_font,
-        MAIN_FONT: theme.main_font,
-        FONTS_STYLESHEET_URL: theme.fonts_stylesheet_url,
-      }
-      customTranslations = translations || {}
 
       appConfigLoaded = true
     })
---
+++ libs/util/app-config/src/lib/parse-utils.spec.ts
@@ -0,0 +1,226 @@
+import {
+  parseConfigSection,
+  parseTranslationsConfigSection,
+  parseMultiConfigSection,
+} from './parse-utils'
+import fetchMock from 'fetch-mock-jest'
+
+describe('parse utils', () => {
+  let result
+  let warnings
+  let errors
+
+  beforeEach(() => {
+    jest.spyOn(global.console, 'warn')
+    fetchMock.reset()
+    warnings = []
+    errors = []
+  })
+
+  describe('parseConfigSection', () => {
+    describe('object with unrecognized keys', () => {
+      beforeEach(() => {
+        result = parseConfigSection(
+          {
+            test: {
+              mandatory1: 'abcd',
+              mandatory2: ['a', 'b', 4],
+              optional1: 123,
+              anotherOne: true,
+              anotherTwo: 'aaa',
+            },
+          },
+          'test',
+          ['mandatory1', 'mandatory2'],
+          ['optional1'],
+          warnings,
+          errors
+        )
+      })
+      it('returns an object with the expected keys', () => {
+        expect(result).toEqual({
+          mandatory1: 'abcd',
+          mandatory2: ['a', 'b', 4],
+          optional1: 123,
+        })
+      })
+      it('outputs a warning for unrecognized keys', () => {
+        expect(warnings).toEqual([
+          expect.stringMatching(/(?=.*anotherOne)(?=.*anotherTwo)/s),
+        ])
+      })
+      it('outputs no error', () => {
+        expect(errors).toEqual([])
+      })
+    })
+
+    describe('object missing mandatory keys', () => {
+      beforeEach(() => {
+        result = parseConfigSection(
+          {
+            test: {
+              mandatory1: 'abcd',
+            },
+          },
+          'test',
+          ['mandatory1', 'mandatory2'],
+          ['optional1'],
+          warnings,
+          errors
+        )
+      })
+      it('returns null', () => {
+        expect(result).toBe(null)
+      })
+      it('outputs an error with relevant message', () => {
+        expect(errors).toEqual([expect.stringContaining('mandatory2')])
+      })
+    })
+
+    describe('object with nested properties', () => {
+      beforeEach(() => {
+        result = parseConfigSection(
+          {
+            test: {
+              mandatory1: {
+                path: {
+                  local: '/aaa/bbb',
+                  global: 'https://hello',
+                },
+              },
+            },
+          },
+          'test',
+          ['mandatory1.path.local'],
+          ['mandatory1.path.global'],
+          warnings,
+          errors
+        )
+      })
+      it('returns an object with flattened properties', () => {
+        expect(result).toEqual({
+          ['mandatory1.path.local']: '/aaa/bbb',
+          ['mandatory1.path.global']: 'https://hello',
+        })
+      })
+    })
+  })
+
+  describe('parseTranslationsConfigSection', () => {
+    describe('object with nested properties', () => {
+      beforeEach(() => {
+        result = parseTranslationsConfigSection(
+          {
+            translations: {
+              en: {
+                term: {
+                  one: 'aaa',
+                  two: 'bbb',
+                },
+              },
+              de: {
+                term: {
+                  one: 'ccc',
+                  two: 'ddd',
+                },
+              },
+            },
+          },
+          'translations'
+        )
+      })
+      it('returns an object with flattened properties (except first level)', () => {
+        expect(result).toEqual({
+          en: {
+            'term.one': 'aaa',
+            'term.two': 'bbb',
+          },
+          de: {
+            'term.one': 'ccc',
+            'term.two': 'ddd',
+          },
+        })
+      })
+    })
+  })
+
+  describe('parseMultiConfigSection', () => {
+    describe('objects with unrecognized keys', () => {
+      beforeEach(() => {
+        result = parseMultiConfigSection(
+          {
+            test: [
+              {
+                mandatory1: 'abcd',
+                optional1: 123,
+                anotherOne: true,
+              },
+              {
+                mandatory1: 'efgh',
+                optional1: 456,
+                anotherTwo: true,
+              },
+            ],
+          },
+          'test',
+          ['mandatory1'],
+          ['optional1'],
+          warnings,
+          errors
+        )
+      })
+      it('returns an array of objects with the expected keys', () => {
+        expect(result).toEqual([
+          {
+            mandatory1: 'abcd',
+            optional1: 123,
+          },
+          {
+            mandatory1: 'efgh',
+            optional1: 456,
+          },
+        ])
+      })
+      it('outputs warnings for unrecognized keys', () => {
+        expect(warnings).toEqual([
+          expect.stringContaining('anotherOne'),
+          expect.stringContaining('anotherTwo'),
+        ])
+      })
+    })
+    describe('objects missing mandatory keys', () => {
+      beforeEach(() => {
+        result = parseMultiConfigSection(
+          {
+            test: [
+              {
+                mandatory1: 'abcd',
+                optional1: 123,
+              },
+              {
+                optional1: 123,
+              },
+              {
+                optional1: 456,
+              },
+            ],
+          },
+          'test',
+          ['mandatory1'],
+          ['optional1'],
+          warnings,
+          errors
+        )
+      })
+      it('returns null', () => {
+        expect(result).toBe(null)
+      })
+      it('outputs errors for missing keys', () => {
+        expect(errors).toEqual([
+          expect.stringContaining('mandatory1'),
+          expect.stringContaining('mandatory1'),
+        ])
+      })
+    })
+  })
+})
