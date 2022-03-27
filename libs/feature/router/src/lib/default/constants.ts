69214cd8522ee00bb0bce16fe04aa8a21421d669
Florent gravin <florent.gravin@camptocamp.com>
2022-03-27 22:49:06 +0200

    feat(router): add mappers between search state and route params

--- libs/feature/router/src/lib/default/constants.ts
+++ libs/feature/router/src/lib/default/constants.ts
@@ -2,3 +2,13 @@ export const ROUTER_STATE_KEY = 'router'
 
 export const ROUTER_ROUTE_SEARCH = 'search'
 export const ROUTER_ROUTE_DATASET = 'dataset'
+
+export enum ROUTE_PARAMS {
+  ANY = 'q',
+  PUBLISHER = 'publisher',
+}
+export type SearchRouteParams = Partial<Record<ROUTE_PARAMS, string>>
+export const ROUTE_PARAMS_MAPPING: SearchRouteParams = {
+  [ROUTE_PARAMS.ANY]: 'any',
+  [ROUTE_PARAMS.PUBLISHER]: 'Org',
+}
---
+++ libs/feature/router/src/lib/default/router.mapper.spec.ts
@@ -0,0 +1,38 @@
+import { routeParamsToState, stateToRouteParams } from './router.mapper'
+
+describe('RouterMapper', () => {
+  describe('stateToRouteParams', () => {
+    let state
+    beforeEach(() => {
+      state = {
+        any: 'scot',
+        Org: {
+          'org sample': true,
+        },
+      }
+    })
+    it('transform to route object', () => {
+      expect(stateToRouteParams(state)).toEqual({
+        publisher: 'org sample',
+        q: 'scot',
+      })
+    })
+  })
+  describe('routeParamsToState', () => {
+    let routeParams
+    beforeEach(() => {
+      routeParams = {
+        publisher: 'org sample',
+        q: 'scot',
+      }
+    })
+    it('transform to route object', () => {
+      expect(routeParamsToState(routeParams)).toEqual({
+        any: 'scot',
+        Org: {
+          'org sample': true,
+        },
+      })
+    })
+  })
+})
