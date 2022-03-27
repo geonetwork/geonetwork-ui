274959354c85c82aa580a68756d99923bfa00721
Florent gravin <florent.gravin@camptocamp.com>
2022-03-27 22:52:53 +0200

    feat(search): inject SearchService from SearchStateContainer directive
    the service is not inject at application level because it depends on SearchFacade which is a DOM context service

--- libs/feature/search/src/lib/state/container/search-state.container.directive.ts
+++ libs/feature/search/src/lib/state/container/search-state.container.directive.ts
@@ -1,9 +1,10 @@
 import { Directive, Host, Input, OnInit } from '@angular/core'
+import { SearchService } from '../../utils/service/search.service'
 import { SearchFacade } from '../search.facade'
 
 @Directive({
   selector: '[gnUiSearchStateContainer]',
-  providers: [SearchFacade],
+  providers: [SearchFacade, SearchService],
 })
 export class SearchStateContainerDirective implements OnInit {
   @Input('gnUiSearchStateContainer') searchId: string
