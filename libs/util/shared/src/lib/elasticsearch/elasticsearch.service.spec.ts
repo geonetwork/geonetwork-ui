02acc5cf45a17483708dbf705602f072f5443e9e
Florent gravin <florent.gravin@camptocamp.com>
2022-03-31 19:37:09 +0200

    feat(datahub): display contact org in search results
    instead of source

--- libs/util/shared/src/lib/elasticsearch/elasticsearch.service.spec.ts
+++ libs/util/shared/src/lib/elasticsearch/elasticsearch.service.spec.ts
@@ -284,7 +284,7 @@ describe('ElasticsearchService', () => {
           'logo',
           'codelist_status_text',
           'linkProtocol',
-          'sourceCatalogue',
+          'contact.organisation',
         ],
         query: {
           bool: {
