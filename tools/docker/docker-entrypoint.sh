7ccc8cd4c00bd3d93ade29ca879eee5a07505fac
Olivier Guyot <olivier.guyot@camptocamp.com>
2022-04-05 14:41:43 +0200

    feat(docker): allow providing custom assets to the datahub docker image
    Images in the custom assets will be added as preload links as well.
    
    Also reworked a bit the docker entry point to be more consistent: each step is
    now outputting a line in the log, logs come before the actual operation (cp,
    sed...) and added a line to declare that everything completed successfully

--- tools/docker/docker-entrypoint.sh
+++ tools/docker/docker-entrypoint.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
 
+APP_FILES_PATH=/usr/share/nginx/html/${APP_NAME}/
+
 CONFIG_FILE_PATH=assets/configuration/
 CONFIG_FILE_NAME=default.toml
 CONFIG_OVERRIDE_FILE_PATH=${CONFIG_DIRECTORY_OVERRIDE:-/conf}/${CONFIG_FILE_NAME}
 
-# copy the conf file from $CONFIG_DIRECTORY_OVERRIDE (defaults to /conf) if present
+ASSETS_PATH=assets/
+CUSTOM_ASSETS_PATH=${ASSETS_DIRECTORY_OVERRIDE:-/assets}/
+
+## 1. COPY CONFIG FILE
+
+# check if conf file from $CONFIG_DIRECTORY_OVERRIDE (defaults to /conf) is present
 if [ -f "${CONFIG_OVERRIDE_FILE_PATH}" ]
 then
-  cp ${CONFIG_OVERRIDE_FILE_PATH} /usr/share/nginx/html/${APP_NAME}/${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
-  echo "[INFO] Copied custom configuration file located at ${CONFIG_OVERRIDE_FILE_PATH}"
+  # copy file straight to the assets
+  echo "[INFO] Copying custom configuration file located at ${CONFIG_OVERRIDE_FILE_PATH}..."
+  cp ${CONFIG_OVERRIDE_FILE_PATH} ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
 else
+  # no conf file; use env variables to tweak app config
   echo "[INFO] No custom configuration file found at ${CONFIG_OVERRIDE_FILE_PATH}"
   # Modify the GN4 url and proxy path based on env variables (if defined)
   if [ ! -z "${GN4_API_URL}" ]
   then
-    sed -i "s%geonetwork4_api_url = \".*\"%geonetwork4_api_url = \"${GN4_API_URL}\"%" /usr/share/nginx/html/${APP_NAME}/${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
-    echo "[INFO] Replaced GN4 url in conf with: ${GN4_API_URL}"
+    echo "[INFO] Replacing GN4 url in conf with: ${GN4_API_URL}..."
+    sed -i "s%geonetwork4_api_url = \".*\"%geonetwork4_api_url = \"${GN4_API_URL}\"%" ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
   fi
   if [ ! -z "${PROXY_PATH}" ]
   then
-    sed -i "s%proxy_path = \".*\"%proxy_path = \"${PROXY_PATH}\"%" /usr/share/nginx/html/${APP_NAME}/${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
-    echo "[INFO] Replaced proxy path in conf with: ${PROXY_PATH}"
+    echo "[INFO] Replacing proxy path in conf with: ${PROXY_PATH}..."
+    sed -i "s%proxy_path = \".*\"%proxy_path = \"${PROXY_PATH}\"%" ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
   fi
 fi
 
+## 2. COPY ASSETS
+
+# check whether the $CUSTOM_ASSETS_PATH directory is present and not empty
+if [ -d "${CUSTOM_ASSETS_PATH}" ] && [ "$(ls -A ${CUSTOM_ASSETS_PATH})" ]; then
+  files_count=$(find ${CUSTOM_ASSETS_PATH} -type f | wc -l)
+  # copy assets right away
+  echo "[INFO] Copying ${files_count} custom assets found in ${CUSTOM_ASSETS_PATH}..."
+  cp ${CUSTOM_ASSETS_PATH}. -r ${APP_FILES_PATH}${ASSETS_PATH}
+  # add a preload link for each asset that is an image
+  cd ${CUSTOM_ASSETS_PATH}
+  images=$(find . -type f -a \( -iname "*.png" -o  -iname "*.svg" -o -iname "*.webp" -o -iname "*.jpg" -o -iname "*.jpeg" \))
+  for image in ${images}
+  do
+    echo "[INFO] Adding preload link for ${image}..."
+    sed -i "s@<!--%PRELOAD_LINKS%-->@<!--%PRELOAD_LINKS%-->\n<link rel=\"preload\" href=\"assets/${image}\" as=\"image\" importance=\"high\" />@" \
+      ${APP_FILES_PATH}index.html
+  done
+else
+  echo "[INFO] No custom assets found at ${CUSTOM_ASSETS_PATH}"
+fi
+
+echo "[INFO] docker-entrypoint.sh ended successfully."
 
 exec "$@"
