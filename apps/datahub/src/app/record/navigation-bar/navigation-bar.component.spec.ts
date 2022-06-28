c869b80fb902cbfd4864be7c84e5a6ff4324daa9
Florent gravin <florent.gravin@camptocamp.com>
2022-06-28 17:27:45 +0200

    fix(navigation-bar): test

--- apps/datahub/src/app/record/navigation-bar/navigation-bar.component.spec.ts
+++ apps/datahub/src/app/record/navigation-bar/navigation-bar.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { NavigationBarComponent } from './navigation-bar.component'
@@ -9,6 +10,7 @@ describe('NavigationBarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NavigationBarComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
   })
 
