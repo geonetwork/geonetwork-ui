527e86bcf970dc7ad224afc42a9d1ac92a3733aa
Abdelmalek <ab.khelouat@gmail.com>
2022-07-04 07:51:20 +0200

    test(datahub): add pagination component tests

--- libs/ui/elements/src/lib/pagination/pagination.component.spec.ts
+++ libs/ui/elements/src/lib/pagination/pagination.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 
 import { PaginationComponent } from './pagination.component'
 
@@ -13,10 +14,26 @@ describe('PaginationComponent', () => {
 
     fixture = TestBed.createComponent(PaginationComponent)
     component = fixture.componentInstance
+    component.currentPage = 10
+    component.nPages = 10
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should navigation_next be disabled', () => {
+    fixture.detectChanges()
+    const isDisabled = fixture.debugElement.query(By.css('#navigate_next'))
+      .nativeElement.disabled
+    expect(isDisabled).toBeTruthy()
+  })
+
+  it('should navigate_previous be enabled', () => {
+    fixture.detectChanges()
+    const isDisabled = fixture.debugElement.query(By.css('#navigate_previous'))
+      .nativeElement.disabled
+    expect(isDisabled).toBeFalsy()
   })
 })
