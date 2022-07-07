c4a1e66a3bc49214b11a5cae0b5a1ff47c501cca
Olivier Guyot <olivier.guyot@camptocamp.com>
2022-07-07 16:31:52 +0200

    ci: add a workflow for generating artifacts for dev branches and releases
    The workflow will:
    * build all apps as docker images and ZIP archives when a release is
      done in Github (zip files are attached to the release)
    * build docker images for affected apps on any commit, tagged with the
      branch name
    * delete docker images on Dockerhub when a branch is deleted
    
    This currently applies to the datafeeder and datahub apps.

---
+++ tools/make-archive.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# This script creates an archive out of a directory in the `dist` folder,
+# and appends it with a tag or branch name
+
+# input should be a unique app name or a list of names separated by a comma
+# e.g. tools/make-archive.sh datahub,datafeeder
+appNames=$1
+gitTag=$(git describe --exact-match --tags 2>/dev/null | sed "s/^v//") # remove "v" in front of version if any
+gitBranch=$(git symbolic-ref --short HEAD)
+
+# make sure the archives dist is there and empty
+mkdir -p dist/archives
+rm -f dist/archives/*
+
+# set field separator to allow looping
+IFS=","
+for appName in $appNames
+do
+  distPath=dist/apps/${appName}
+  zipPath=dist/archives/${appName}-${gitTag:-${gitBranch}}.zip
+
+  # make sure the built files are there
+  if [[ -d "${distPath}" ]]
+  then
+    zip $zipPath -r ${distPath}
+  else
+    echo "Error: the files for the app named '${appName}' were not found in dist directory, exiting."
+    exit 1
+  fi
+done
+
+
