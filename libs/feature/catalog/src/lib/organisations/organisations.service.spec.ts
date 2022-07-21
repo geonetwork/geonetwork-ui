8b8ac736d2a6d5ba1fd31841560e22ebb36975f4
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-07-21 16:20:40 +0200

    chore(format): npm run format

--- libs/feature/catalog/src/lib/organisations/organisations.service.spec.ts
+++ libs/feature/catalog/src/lib/organisations/organisations.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { OrganisationsService } from './organisations.service';
+import { OrganisationsService } from './organisations.service'
 
 describe('OrganisationsService', () => {
-  let service: OrganisationsService;
+  let service: OrganisationsService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrganisationsService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(OrganisationsService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
