3f41f197e8dda7485db94454dc2047766018e278
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-08-10 16:22:12 +0200

    feat(organisations): implement sort of organisations

--- libs/feature/catalog/src/lib/organisations/organisations.component.spec.ts
+++ libs/feature/catalog/src/lib/organisations/organisations.component.spec.ts
@@ -7,6 +7,7 @@ import {
   Output,
 } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { readFirst } from '@nrwl/angular/testing'
 import { By } from '@angular/platform-browser'
 import { Organisation, ORGANISATIONS_FIXTURE } from '@geonetwork-ui/util/shared'
 import { of } from 'rxjs'
@@ -17,6 +18,13 @@ import {
 } from './organisations.component'
 import { OrganisationsService } from './organisations.service'
 
+@Component({
+  selector: 'gn-ui-organisations-sort',
+  template: '<div></div>',
+})
+class OrganisationsSortMockComponent {
+  @Output() sortBy = new EventEmitter<string>()
+}
 @Component({
   selector: 'gn-ui-organisation-preview',
   template: '<div></div>',
@@ -48,6 +56,7 @@ describe('OrganisationsComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         OrganisationsComponent,
+        OrganisationsSortMockComponent,
         OrganisationPreviewMockComponent,
         PaginationMockComponent,
       ],
@@ -74,9 +83,11 @@ describe('OrganisationsComponent', () => {
   })
 
   describe('on component init', () => {
+    let sortComponent: OrganisationsSortMockComponent
     let orgPreviewComponents: OrganisationPreviewMockComponent[]
     let paginationComponent: PaginationMockComponent
     let setCurrentPageSpy
+    let setSortBySpy
     it('should call getOrganisationsWithGroups', () => {
       expect(
         organisationsServiceMock.getOrganisationsWithGroups
@@ -88,15 +99,15 @@ describe('OrganisationsComponent', () => {
           By.directive(OrganisationPreviewMockComponent)
         )
       })
-      it('should pass first organisation to first ui preview component', () => {
-        expect(orgPreviewComponents[0].componentInstance.organisation).toEqual(
-          ORGANISATIONS_FIXTURE[0]
-        )
-      })
-      it('should pass last organisation on page to last ui preview component', () => {
+      it('should pass first organisation (sorted by name-asc) to first ui preview component', () => {
         expect(
-          orgPreviewComponents[ITEMS_ON_PAGE - 1].componentInstance.organisation
-        ).toEqual(ORGANISATIONS_FIXTURE[ITEMS_ON_PAGE - 1])
+          orgPreviewComponents[0].componentInstance.organisation.name
+        ).toEqual('A Data Org')
+      })
+      it('should pass 6th organisation (sorted by name-asc) on page to 6th ui preview component', () => {
+        expect(
+          orgPreviewComponents[5].componentInstance.organisation.name
+        ).toEqual('F Data Org')
       })
     })
     describe('pass params to ui pagination component', () => {
@@ -130,17 +141,46 @@ describe('OrganisationsComponent', () => {
         it('should set currentPage in ui component to correct value', () => {
           expect(paginationComponent.componentInstance.currentPage).toEqual(2)
         })
-        it('should pass first organisation of second page to first ui preview component', () => {
+        it('should pass first organisation of second page (sorted by name-asc) to first ui preview component', () => {
           expect(
-            orgPreviewComponents[0].componentInstance.organisation
-          ).toEqual(ORGANISATIONS_FIXTURE[ITEMS_ON_PAGE])
+            orgPreviewComponents[0].componentInstance.organisation.name
+          ).toEqual('G Data Org')
         })
-        it('should pass last organisation of second page to last ui preview component', () => {
+        it('should pass last organisation of second page (sorted by name-asc) to last ui preview component', () => {
           expect(
             orgPreviewComponents[orgPreviewComponents.length - 1]
-              .componentInstance.organisation
-          ).toEqual(ORGANISATIONS_FIXTURE[ORGANISATIONS_FIXTURE.length - 1])
+              .componentInstance.organisation.name
+          ).toEqual('J Data Org')
         })
+      })
+    })
+    describe('sort by recordCount', () => {
+      beforeEach(() => {
+        sortComponent = de.query(By.directive(OrganisationsSortMockComponent))
+        setSortBySpy = jest.spyOn(component, 'setSortBy')
+        sortComponent.triggerEventHandler('sortBy', 'recordCount-desc')
+        fixture.detectChanges()
+        orgPreviewComponents = de.queryAll(
+          By.directive(OrganisationPreviewMockComponent)
+        )
+        fixture.detectChanges()
+      })
+      it('should call setSortBy', () => {
+        expect(setSortBySpy).toHaveBeenCalledWith('recordCount-desc')
+      })
+      it('should have organsiation with max recordCount at first position in observable', async () => {
+        const organisations = await readFirst(component.organisations$)
+        expect(organisations[0]).toEqual(ORGANISATIONS_FIXTURE[5])
+      })
+      it('should pass organsiation with max recordCount to first preview component', () => {
+        expect(orgPreviewComponents[0].componentInstance.organisation).toEqual(
+          ORGANISATIONS_FIXTURE[5]
+        )
+      })
+      it('should pass organsiation with 6th highest recordCount to 6th preview component', () => {
+        expect(orgPreviewComponents[5].componentInstance.organisation).toEqual(
+          ORGANISATIONS_FIXTURE[3]
+        )
       })
     })
   })
