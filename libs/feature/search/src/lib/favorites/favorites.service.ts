ac16ad144d39b2d761fe351bd16a168e72c957ba
Olivier Guyot <olivier.guyot@camptocamp.com>
2022-09-05 10:26:08 +0200

    feat(search): clarify the observable logic in favorite service

--- libs/feature/search/src/lib/favorites/favorites.service.ts
+++ libs/feature/search/src/lib/favorites/favorites.service.ts
@@ -9,6 +9,7 @@ import {
   switchMap,
   take,
   tap,
+  withLatestFrom,
 } from 'rxjs/operators'
 
 const SELECTION_ID = 0 // hardcoded to always point on the first selection
@@ -17,15 +18,17 @@ const SELECTION_ID = 0 // hardcoded to always point on the first selection
   providedIn: 'root',
 })
 export class FavoritesService {
-  private myId$ = this.authService
+  private myUserId$ = this.authService
     .authReady()
     .pipe(map((userInfo) => (userInfo ? parseInt(userInfo.id) : null)))
 
-  private myFavoritesUuidFromApi$ = this.myId$.pipe(
-    switchMap((userId) =>
-      userId !== null
-        ? this.userSelectionsService.getSelectionRecords(SELECTION_ID, userId)
-        : of([] as string[])
+  // this observable loads the current list of favorites from the API
+  private myFavoritesUuidFromApi$ = this.myUserId$.pipe(
+    switchMap(
+      (userId) =>
+        userId !== null
+          ? this.userSelectionsService.getSelectionRecords(SELECTION_ID, userId)
+          : of([] as string[]) // emit an empty array if the user is not authentified
     ),
     catchError((e) =>
       throwError(
@@ -38,10 +41,15 @@ export class FavoritesService {
 
   private modifiedFavorites$ = new Subject<string[]>()
 
+  // favorites are loaded once from the API (from myFavoritesUuidFromApi$);
+  // subsequent emissions are caused by modifications of the favorite list
+  // on the client side (coming from modifiedFavorites$)
   myFavoritesUuid$ = merge(
     this.myFavoritesUuidFromApi$,
     this.modifiedFavorites$
-  ).pipe(shareReplay(1))
+  ).pipe(
+    shareReplay(1) // new subscriptions should not trigger a new API request!
+  )
 
   constructor(
     private userSelectionsService: UserselectionsApiService,
@@ -49,12 +57,13 @@ export class FavoritesService {
   ) {}
 
   addToFavorites(uuids: string[]): Observable<void> {
-    return combineLatest([this.myId$, this.myFavoritesUuid$]).pipe(
+    return this.myFavoritesUuid$.pipe(
       take(1),
-      tap(([userId]) => {
+      withLatestFrom(this.myUserId$),
+      tap(([, userId]) => {
         if (userId === null) throw new Error('not authenticated')
       }),
-      switchMap(([userId, favorites]) =>
+      switchMap(([favorites, userId]) =>
         this.userSelectionsService
           .addToUserSelection(SELECTION_ID, userId, uuids)
           .pipe(tap(() => this.emitAddedFavorites(favorites, uuids)))
@@ -71,12 +80,13 @@ export class FavoritesService {
   }
 
   removeFromFavorites(uuids: string[]): Observable<void> {
-    return combineLatest([this.myId$, this.myFavoritesUuid$]).pipe(
+    return this.myFavoritesUuid$.pipe(
       take(1),
-      tap(([userId]) => {
+      withLatestFrom(this.myUserId$),
+      tap(([, userId]) => {
         if (userId === null) throw new Error('not authenticated')
       }),
-      switchMap(([userId, favorites]) =>
+      switchMap(([favorites, userId]) =>
         this.userSelectionsService
           .deleteFromUserSelection(SELECTION_ID, userId, uuids)
           .pipe(tap(() => this.emitRemovedFavorites(favorites, uuids)))
