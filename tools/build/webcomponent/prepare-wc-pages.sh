795fe74892da179f0c4bcfa023e5ba362932fd8e
Florent gravin <florent.gravin@camptocamp.com>
2022-09-30 12:05:15 +0200

    fix(wc): fix build and scripts

--- tools/build/webcomponent/prepare-wc-pages.sh
+++ tools/build/webcomponent/prepare-wc-pages.sh
@@ -2,12 +2,11 @@
 
 DIST_DEMO_PATH=dist/demo/
 DIST_WC_PATH=${DIST_DEMO_PATH}webcomponents/
-APP_NAME=${1}
 
 echo '-- Build Geonetwork Web Components'
-ng build --prod --output-hashing=none --output-path=${DIST_WC_PATH} gn-wc
+nx run webcomponents:build --output-hashing=none --output-path=${DIST_WC_PATH}
 
-echo '-- Publish html page for Web Component' ${APP_NAME}
+echo '-- Publish html page for Web Component'
 mkdir -p $DIST_WC_PATH
 cat ${DIST_WC_PATH}{runtime,polyfills,main}.js > $DIST_WC_PATH'gn-wc.js'
 rm -f ${DIST_WC_PATH}main.js
@@ -15,7 +14,7 @@ rm -f ${DIST_WC_PATH}polyfills.js
 rm -f ${DIST_WC_PATH}runtime.js
 rm -f ${DIST_WC_PATH}styles.css
 
-for c in webcomponents/src/app/components/gn-* ; do
+for c in apps/webcomponents/src/app/components/gn-* ; do
   echo "-- Copy HTML sample for:" `basename $c`
   cp $c/*".sample.html" $DIST_WC_PATH
 done
