3df38e7963f533cc7b075d0c85c8d1ddc8d8697d
Florent gravin <florent.gravin@camptocamp.com>
2022-09-30 13:38:56 +0200

    feat(wc): fix translation, icon and add DATAHUB preview

--- apps/webcomponents/src/app/webcomponents.module.ts
+++ apps/webcomponents/src/app/webcomponents.module.ts
@@ -1,25 +1,32 @@
-import { DoBootstrap, Injector, NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Injector, NgModule } from '@angular/core'
 import { createCustomElement } from '@angular/elements'
+import { MatIconModule } from '@angular/material/icon'
+import { BrowserModule } from '@angular/platform-browser'
+import { Configuration } from '@geonetwork-ui/data-access/gn4'
+import { FeatureSearchModule } from '@geonetwork-ui/feature/search'
+import { UiElementsModule } from '@geonetwork-ui/ui/elements'
+import { UiInputsModule } from '@geonetwork-ui/ui/inputs'
 import {
-  TRANSLATE_GEONETWORK_CONFIG,
+  DEFAULT_RESULTS_LAYOUT_CONFIG,
+  RESULTS_LAYOUT_CONFIG,
+  UiSearchModule,
+} from '@geonetwork-ui/ui/search'
+import {
+  TRANSLATE_DEFAULT_CONFIG,
   UtilI18nModule,
 } from '@geonetwork-ui/util/i18n'
-import { Configuration } from '@geonetwork-ui/data-access/gn4'
-import {
-  FeatureSearchModule,
-  SearchFacade,
-} from '@geonetwork-ui/feature/search'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { TranslateModule } from '@ngx-translate/core'
+import { DATAHUB_RESULTS_LAYOUT_CONFIG } from '../../../datahub/src/app/app.config'
+import { RecordPreviewDatahubComponent } from '../../../datahub/src/app/home/search/record-preview-datahub/record-preview-datahub.component'
+import { AppComponent } from './app.component'
 import { apiConfiguration, BaseComponent } from './components/base.component'
 import { GnAggregatedRecordsComponent } from './components/gn-aggregated-records/gn-aggregated-records.component'
 import { GnFacetsComponent } from './components/gn-facets/gn-facets.component'
 import { GnResultsListComponent } from './components/gn-results-list/gn-results-list.component'
-import { UiInputsModule } from '@geonetwork-ui/ui/inputs'
-import { UiSearchModule } from '@geonetwork-ui/ui/search'
-import { CommonModule } from '@angular/common'
 
 const CUSTOM_ELEMENTS: [new (...args) => BaseComponent, string][] = [
   [GnFacetsComponent, 'gn-facets'],
@@ -35,31 +42,43 @@ const CUSTOM_ELEMENTS: [new (...args) => BaseComponent, string][] = [
     GnAggregatedRecordsComponent,
   ],
   declarations: [
+    AppComponent,
     BaseComponent,
     GnFacetsComponent,
     GnResultsListComponent,
     GnAggregatedRecordsComponent,
+    RecordPreviewDatahubComponent,
   ],
   imports: [
     CommonModule,
+    BrowserModule,
     UiInputsModule,
     UiSearchModule,
+    UiElementsModule,
     FeatureSearchModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
     EffectsModule.forRoot(),
     UtilI18nModule,
-    TranslateModule.forRoot(TRANSLATE_GEONETWORK_CONFIG),
-    StoreDevtoolsModule.instrument(),
+    TranslateModule.forRoot(TRANSLATE_DEFAULT_CONFIG),
+    MatIconModule,
   ],
   providers: [
+    {
+      provide: RESULTS_LAYOUT_CONFIG,
+      useValue: {
+        ...DEFAULT_RESULTS_LAYOUT_CONFIG,
+        ...DATAHUB_RESULTS_LAYOUT_CONFIG,
+      },
+    },
     {
       provide: Configuration,
       useValue: apiConfiguration,
     },
-    SearchFacade,
   ],
+  // bootstrap: [AppComponent],
 })
-export class WebcomponentsModule implements DoBootstrap {
+export class WebcomponentsModule {
   constructor(private injector: Injector) {
     CUSTOM_ELEMENTS.forEach((ceDefinition) => {
       const angularComponent = ceDefinition[0]
