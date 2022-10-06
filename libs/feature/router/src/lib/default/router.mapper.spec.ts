922836264ba46ddcf3216ad0d1ee81870e4937b0
Abdelmalek <ab.khelouat@gmail.com>
2022-10-06 16:34:28 +0200

    feat(router): enable route state mapping with multiple parameters

--- libs/feature/router/src/lib/default/router.mapper.spec.ts
+++ libs/feature/router/src/lib/default/router.mapper.spec.ts
@@ -3,36 +3,78 @@ import { routeParamsToState, stateToRouteParams } from './router.mapper'
 describe('RouterMapper', () => {
   describe('stateToRouteParams', () => {
     let state
-    beforeEach(() => {
-      state = {
-        any: 'scot',
-        OrgForResource: {
-          'org sample': true,
-        },
-        keyword: {},
-      }
+    describe('params with unique value', () => {
+      beforeEach(() => {
+        state = {
+          any: 'scot',
+          OrgForResource: {
+            'org sample': true,
+          },
+          keyword: {},
+        }
+      })
+      it('transform to route object', () => {
+        expect(stateToRouteParams(state)).toEqual({
+          publisher: ['org sample'],
+          q: 'scot',
+        })
+      })
     })
-    it('transform to route object', () => {
-      expect(stateToRouteParams(state)).toEqual({
-        publisher: 'org sample',
-        q: 'scot',
+    describe('params with multiple values', () => {
+      beforeEach(() => {
+        state = {
+          any: 'scot',
+          OrgForResource: {
+            'org 1': true,
+            'org (%2)': true,
+            'not org': false,
+            '123[]<>;:!': true,
+          },
+          keyword: {},
+        }
+      })
+      it('transform to route object', () => {
+        expect(stateToRouteParams(state)).toEqual({
+          publisher: ['org 1', 'org (%2)', '123[]<>;:!'],
+          q: 'scot',
+        })
       })
     })
   })
   describe('routeParamsToState', () => {
     let routeParams
-    beforeEach(() => {
-      routeParams = {
-        publisher: 'org sample',
-        q: 'scot',
-      }
+    describe('params with unique value', () => {
+      beforeEach(() => {
+        routeParams = {
+          publisher: 'org sample',
+          q: 'scot',
+        }
+      })
+      it('transform to route object', () => {
+        expect(routeParamsToState(routeParams)).toEqual({
+          any: 'scot',
+          OrgForResource: {
+            'org sample': true,
+          },
+        })
+      })
     })
-    it('transform to route object', () => {
-      expect(routeParamsToState(routeParams)).toEqual({
-        any: 'scot',
-        OrgForResource: {
-          'org sample': true,
-        },
+    describe('params with multiple values', () => {
+      beforeEach(() => {
+        routeParams = {
+          publisher: ['org 1', 'org (%2)', '123[]<>;:!'],
+          q: 'scot',
+        }
+      })
+      it('transform to route object', () => {
+        expect(routeParamsToState(routeParams)).toEqual({
+          any: 'scot',
+          OrgForResource: {
+            'org 1': true,
+            'org (%2)': true,
+            '123[]<>;:!': true,
+          },
+        })
       })
     })
   })
