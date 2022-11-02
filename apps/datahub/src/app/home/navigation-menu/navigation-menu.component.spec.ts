d21cc8b92929cf5d6e6cd1d9eafb3c27a8b017c2
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-11-02 12:21:41 +0100

    feat(navigation-menu): update active label by listening to route changes

--- apps/datahub/src/app/home/navigation-menu/navigation-menu.component.spec.ts
+++ apps/datahub/src/app/home/navigation-menu/navigation-menu.component.spec.ts
@@ -1,7 +1,24 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {
+  RouterFacade,
+  ROUTER_ROUTE_SEARCH,
+} from '@geonetwork-ui/feature/router'
+import { TranslateModule } from '@ngx-translate/core'
+import { readFirst } from '@nrwl/angular/testing'
+import { BehaviorSubject } from 'rxjs'
+import {
+  ROUTER_ROUTE_NEWS,
+  ROUTER_ROUTE_ORGANISATIONS,
+} from '../../router/constants'
 
 import { NavigationMenuComponent } from './navigation-menu.component'
+
+const routerFacadeMock = {
+  currentRoute$: new BehaviorSubject({
+    url: [{ path: ROUTER_ROUTE_NEWS }],
+  }),
+}
 
 describe('NavigationMenuComponent', () => {
   let component: NavigationMenuComponent
@@ -9,7 +26,14 @@ describe('NavigationMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
       declarations: [NavigationMenuComponent],
+      providers: [
+        {
+          provide: RouterFacade,
+          useValue: routerFacadeMock,
+        },
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
   })
@@ -22,5 +46,45 @@ describe('NavigationMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  describe('should display activeLabel$', () => {
+    it('displays initially activeLabel for news', async () => {
+      const activeLabel = await readFirst(component.activeLabel$)
+      expect(activeLabel).toEqual('datahub.header.news')
+    })
+    describe('navigate to search route', () => {
+      beforeEach(() => {
+        routerFacadeMock.currentRoute$.next({
+          url: [{ path: ROUTER_ROUTE_SEARCH }],
+        })
+      })
+      it('displays activeLabel for search', async () => {
+        const activeLabel = await readFirst(component.activeLabel$)
+        expect(activeLabel).toEqual('datahub.header.datasets')
+      })
+    })
+    describe('navigate to organisations route', () => {
+      beforeEach(() => {
+        routerFacadeMock.currentRoute$.next({
+          url: [{ path: ROUTER_ROUTE_ORGANISATIONS }],
+        })
+      })
+      it('displays activeLabel for organisations', async () => {
+        const activeLabel = await readFirst(component.activeLabel$)
+        expect(activeLabel).toEqual('datahub.header.organisations')
+      })
+    })
+    describe('navigate to a route with missing label', () => {
+      beforeEach(() => {
+        routerFacadeMock.currentRoute$.next({
+          url: [{ path: 'ROUTE_WITHOUT_LABEL' }],
+        })
+      })
+      it('displays empty string as activeLabel', async () => {
+        const activeLabel = await readFirst(component.activeLabel$)
+        expect(activeLabel).toEqual('')
+      })
+    })
   })
 })
