b6f64ab31cc8fdd899c0f02edf340b2fd370a3fc
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-11-10 20:45:29 +0100

    chore(i18n): update translations

--- libs/ui/map/src/lib/ui-map.module.ts
+++ libs/ui/map/src/lib/ui-map.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MapComponent } from './components/map/map.component'
 import { FeatureDetailComponent } from './components/feature-detail/feature-detail.component'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   declarations: [MapComponent, FeatureDetailComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, TranslateModule.forRoot()],
   exports: [MapComponent, FeatureDetailComponent],
 })
 export class UiMapModule {}
