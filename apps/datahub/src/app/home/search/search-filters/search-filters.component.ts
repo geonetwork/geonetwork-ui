117b0d06bac7fb9e09452f5ec12c8bd3dbf51ece
Olivier Guyot <olivier.guyot@camptocamp.com>
2022-11-18 18:15:55 +0100

    remove group factory from datahub

--- apps/datahub/src/app/home/search/search-filters/search-filters.component.ts
+++ apps/datahub/src/app/home/search/search-filters/search-filters.component.ts
@@ -34,28 +34,6 @@ export class SearchFiltersComponent {
     return `${formatTypes[getLinkType('', bucketKey)]}`
   }
 
-  formatGroupFactory(selectOptions: any) {
-    const groupedOptions = groupBy(selectOptions, 'label')
-    const groupedOptionsSum = []
-    if (groupedOptions) {
-      Object.entries(groupedOptions).forEach((group: any) => {
-        const option = group[1].reduce((group: any, item: any) => ({
-          ...group,
-          value:
-            group.value instanceof Array
-              ? [...group.value, item.value]
-              : [group.value],
-          count: group.count + item.count,
-        }))
-        groupedOptionsSum.push({
-          ...option,
-          label: `${option.label} (${option.count})`,
-        })
-      })
-    }
-    return groupedOptionsSum
-  }
-
   open() {
     this.isOpen = true
   }
@@ -69,16 +47,4 @@ export class SearchFiltersComponent {
       OrgForResource: {},
     })
   }
-}
-
-function groupBy(list, key) {
-  return list
-    ? list.reduce(
-        (groups, item) => ({
-          ...groups,
-          [item[key]]: [...(groups[item[key]] || []), item],
-        }),
-        {}
-      )
-    : list
 }
