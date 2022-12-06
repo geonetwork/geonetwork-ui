04dac379437cb18978676ddfb0a0aded9d8cbd7d
Florent gravin <florent.gravin@camptocamp.com>
2022-12-06 20:06:11 +0100

    feat(editor): load yaml config file on boostrap

--- apps/metadata-editor/src/main.ts
+++ apps/metadata-editor/src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { loadAppConfig } from '@geonetwork-ui/util/app-config'
 
 import { AppModule } from './app/app.module'
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err))
+loadAppConfig().then(() => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err))
+})
