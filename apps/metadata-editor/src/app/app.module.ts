edd91e0f31f7c2f37e5afd17235562aaaa9e21d3
Florent gravin <florent.gravin@camptocamp.com>
2022-12-09 21:09:55 +0100

    refactor(editor): move dashboard page to a dedicated module
    so we can instantiate a feature state

--- apps/metadata-editor/src/app/app.module.ts
+++ apps/metadata-editor/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
+import { FeatureEditorModule } from '@geonetwork-ui/feature/editor'
 import { FeatureSearchModule } from '@geonetwork-ui/feature/search'
 import { UiElementsModule } from '@geonetwork-ui/ui/elements'
+import { UiInputsModule } from '@geonetwork-ui/ui/inputs'
 import { UiSearchModule } from '@geonetwork-ui/ui/search'
 import {
   TRANSLATE_DEFAULT_CONFIG,
@@ -14,10 +16,7 @@ import { AppCommonModule } from './app.common.module'
 import { AppComponent } from './app.component'
 import { appRoutes } from './app.routes'
 import { CreatePageComponent } from './create/create-page.component'
-import { DashboardMenuComponent } from './dashboard/dashboard-menu/dashboard-menu.component'
-import { DashboardPageComponent } from './dashboard/dashboard-page.component'
-import { SearchHeaderComponent } from './dashboard/search-header/search-header.component'
-import { SidebarComponent } from './dashboard/sidebar/sidebar.component'
+import { DashboardModule } from './dashboard/dashboard.module'
 import { EditPageComponent } from './edit/edit-page.component'
 import { SignInPageComponent } from './sign-in/sign-in-page.component'
 import { UiInputsModule } from '@geonetwork-ui/ui/inputs'
@@ -27,17 +26,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardPageComponent,
     EditPageComponent,
     CreatePageComponent,
     SignInPageComponent,
-    EditPageComponent,
-    SidebarComponent,
-    SearchHeaderComponent,
-    DashboardMenuComponent,
   ],
   imports: [
     BrowserModule,
+    DashboardModule,
     FeatureSearchModule,
     UiElementsModule,
     UiSearchModule,
