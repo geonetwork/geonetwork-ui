a3dd943422e15e6602b301d5a669bc7eedeb1edc
Florent gravin <florent.gravin@camptocamp.com>
2022-12-12 20:51:13 +0100

    test(editor): fix sidebar component

--- apps/metadata-editor/src/app/dashboard/sidebar/sidebar.component.spec.ts
+++ apps/metadata-editor/src/app/dashboard/sidebar/sidebar.component.spec.ts
@@ -1,10 +1,18 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { SidebarComponent } from './sidebar.component'
+import { By } from '@angular/platform-browser'
 import { AuthService } from '@geonetwork-ui/feature/auth'
-import { Component } from '@angular/core'
+import { UserModel } from '@geonetwork-ui/util/shared'
+import { USER_FIXTURE } from '@geonetwork-ui/util/shared/fixtures'
 import { LetModule } from '@ngrx/component'
+import { BehaviorSubject } from 'rxjs'
 
-class AuthServiceMock {}
+import { SidebarComponent } from './sidebar.component'
 
 @Component({
   selector: 'md-editor-dashboard-menu',
@@ -12,21 +20,38 @@ class AuthServiceMock {}
 })
 class DashboardMenuMockComponent {}
 
+@Component({
+  selector: 'gn-ui-user-preview',
+  template: '<div></div>',
+})
+export class UserPreviewComponentMock {
+  @Input() user: UserModel
+}
+
+const user = USER_FIXTURE()
+class AuthServiceMock {
+  user$ = new BehaviorSubject(user)
+}
+
 describe('SidebarComponent', () => {
   let component: SidebarComponent
   let fixture: ComponentFixture<SidebarComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SidebarComponent, DashboardMenuMockComponent],
-      imports: [LetModule],
-      providers: [
-        {
-          provide: AuthService,
-          useClass: AuthServiceMock,
-        },
+      declarations: [
+        SidebarComponent,
+        UserPreviewComponentMock,
+        DashboardMenuMockComponent,
       ],
-    }).compileComponents()
+      imports: [LetModule],
+      providers: [{ provide: AuthService, useClass: AuthServiceMock }],
+      schemas: [NO_ERRORS_SCHEMA],
+    })
+      .overrideComponent(SidebarComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents()
 
     fixture = TestBed.createComponent(SidebarComponent)
     component = fixture.componentInstance
@@ -35,5 +60,13 @@ describe('SidebarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+  describe('when a user is logged', () => {
+    it('displays the user preview', () => {
+      const avatar = fixture.debugElement.query(
+        By.directive(UserPreviewComponentMock)
+      )
+      expect(avatar.componentInstance.user).toBe(user)
+    })
   })
 })
