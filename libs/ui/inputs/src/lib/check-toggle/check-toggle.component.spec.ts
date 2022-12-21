df1e9e96afc3dcedeed16bea6446e9f6998fbf28
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-12-21 15:21:48 +0100

    feat(check-toggle): add check-toggle UI component

---
+++ libs/ui/inputs/src/lib/check-toggle/check-toggle.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { CheckToggleComponent } from './check-toggle.component'
+
+describe('CheckToggleComponent', () => {
+  let component: CheckToggleComponent
+  let fixture: ComponentFixture<CheckToggleComponent>
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CheckToggleComponent],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(CheckToggleComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
