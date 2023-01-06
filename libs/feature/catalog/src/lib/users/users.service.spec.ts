6f16eb344ceb67e90f43c6157cb94fee2a107bd3
Florent gravin <florent.gravin@camptocamp.com>
2023-01-06 14:53:13 +0100

    feat(users): create a user service
    in catalog library to share the users

---
+++ libs/feature/catalog/src/lib/users/users.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing'
+import { UsersApiService } from '@geonetwork-ui/data-access/gn4'
+import { USERS_FIXTURE } from '@geonetwork-ui/util/shared/fixtures'
+import { of } from 'rxjs'
+
+import { UsersService } from './users.service'
+
+class UsersApiServiceMock {
+  getUsers = jest.fn(() => of(USERS_FIXTURE()))
+}
+
+describe('UsersService', () => {
+  let service: UsersService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: UsersApiService,
+          useClass: UsersApiServiceMock,
+        },
+      ],
+    })
+    TestBed.inject(UsersApiService)
+    service = TestBed.inject(UsersService)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy()
+  })
+})
