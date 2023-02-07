3e21fc417a2091c52da8bce27ad1b7c1d1e8bc2f
Florent gravin <florent.gravin@camptocamp.com>
2023-02-07 16:02:50 +0100

    feat(wc-search): add option to forward to datahub
    on search submission: open search page
    on suggestion selection: open record

--- apps/webcomponents/src/app/components/gn-search-input/gn-search-input.component.ts
+++ apps/webcomponents/src/app/components/gn-search-input/gn-search-input.component.ts
@@ -1,9 +1,11 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  Input,
   ViewEncapsulation,
 } from '@angular/core'
 import { SearchFacade, SearchService } from '@geonetwork-ui/feature/search'
+import { MetadataRecord } from '@geonetwork-ui/util/shared'
 import { BaseComponent } from '../base.component'
 
 @Component({
@@ -14,4 +16,20 @@ import { BaseComponent } from '../base.component'
   encapsulation: ViewEncapsulation.ShadowDom,
   providers: [SearchFacade, SearchService],
 })
-export class GnSearchInputComponent extends BaseComponent {}
+export class GnSearchInputComponent extends BaseComponent {
+  @Input() datahubUrl: string
+
+  search(any: string) {
+    if (this.datahubUrl) {
+      const landingPage = `${this.datahubUrl}/home/search/?q=${any}`
+      window.open(landingPage, '_self').focus()
+    }
+  }
+
+  select(record: MetadataRecord) {
+    if (this.datahubUrl) {
+      const landingPage = `${this.datahubUrl}/dataset/${record.uuid}`
+      window.open(landingPage, '_self').focus()
+    }
+  }
+}
