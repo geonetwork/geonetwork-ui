a9b45f8d5a039541151609ef015271aa709c1cda
Florent gravin <florent.gravin@camptocamp.com>
2023-03-02 13:40:44 +0100

    chore(platform): setup typeorm connexion

--- apps/data-platform/src/app/app.module.ts
+++ apps/data-platform/src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DatavizModule } from './dataviz/dataviz.module'
 
 @Module({
-  imports: [],
+  imports: [
+    DatavizModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'geonetwork',
+      password: 'geonetwork',
+      database: 'gn-data-platform',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      migrations: ['dist/orm/migration/*.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
