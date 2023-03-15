4c7c166586dc0433a2cec5743cfd74684a12d0f5
Florent gravin <florent.gravin@camptocamp.com>
2023-03-15 20:52:52 +0100

    test(util-types): secure jest config

--- libs/util/types/jest.config.ts
+++ libs/util/types/jest.config.ts
@@ -2,9 +2,6 @@
 export default {
   displayName: 'util-types',
   preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/util/types',
 }
