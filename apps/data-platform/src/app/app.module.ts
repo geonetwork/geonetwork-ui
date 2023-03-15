5b72e3a218a13a0c8585851089e531b0e47e7698
Florent gravin <florent.gravin@camptocamp.com>
2023-03-15 12:44:13 +0100

    feat(platform): add ENV configuration
    used to set up database connection

--- apps/data-platform/src/app/app.module.ts
+++ apps/data-platform/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AppController } from './app.controller'
@@ -8,13 +9,17 @@ import { DatavizModule } from './dataviz/dataviz.module'
 @Module({
   imports: [
     DatavizModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'geonetwork',
-      password: 'geonetwork',
-      database: 'gn-data-platform',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      schema: process.env.DB_SCHEMA,
       autoLoadEntities: true,
       synchronize: true,
       logging: true,
