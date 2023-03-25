9a7018a584c730f66a3439874212ad2017875616
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-03-25 22:56:43 +0100

    feat(search): use fields service in filter-dropdown component
    The filter-dropdown is now much simpler and relies on the fields service
    to handle the logic related to aggregations and reading values from filters
    Also removed the aggregations service which was not needed anymore

--- libs/feature/search/src/lib/filter-dropdown/filter-dropdown.component.ts
+++ libs/feature/search/src/lib/filter-dropdown/filter-dropdown.component.ts
@@ -5,15 +5,12 @@ import {
   OnInit,
 } from '@angular/core'
 import { Choice } from '@geonetwork-ui/ui/inputs'
-import {
-  AggregationsOrderEnum,
-  AggregationsTypesEnum,
-} from '@geonetwork-ui/util/shared'
 import { Observable } from 'rxjs'
 import { filter, map, startWith } from 'rxjs/operators'
 import { SearchFacade } from '../state/search.facade'
-import { AggregationsService } from '../utils/service/aggregations.service'
 import { SearchService } from '../utils/service/search.service'
+import { FieldsService } from '../utils/service/fields.service'
+import { FieldAvailableValue } from '../utils/service/fields'
 
 @Component({
   selector: 'gn-ui-filter-dropdown',
@@ -24,49 +21,38 @@ import { SearchService } from '../utils/service/search.service'
 export class FilterDropdownComponent implements OnInit {
   @Input() fieldName: string
   @Input() title: string
-  @Input() order: AggregationsOrderEnum = AggregationsOrderEnum.ASC
-  @Input() aggregationType: AggregationsTypesEnum = AggregationsTypesEnum.TERMS
 
   choices$: Observable<Choice[]>
   selected$ = this.searchFacade.searchFilters$.pipe(
-    map((filters) => Object.keys(filters[this.fieldName] ?? {})),
+    map((filters) =>
+      this.fieldsService.getValuesForFilters(this.fieldName, filters)
+    ),
     filter((selected) => !!selected)
   )
 
-  onSelectedValues(values: unknown[]) {
-    this.searchService.updateFilters({
-      [this.fieldName]: values.reduce<Record<string, boolean>>((acc, val) => {
-        return { ...acc, [val.toString()]: true }
-      }, {}),
-    })
+  onSelectedValues(values: (string | number)[]) {
+    const filters = this.fieldsService.getFiltersForValues(
+      this.fieldName,
+      values
+    )
+    this.searchService.updateFilters(filters)
   }
 
   constructor(
     private searchFacade: SearchFacade,
     private searchService: SearchService,
-    private aggregationsService: AggregationsService
+    private fieldsService: FieldsService
   ) {}
 
   ngOnInit() {
-    this.choices$ = (
-      this.aggregationType === AggregationsTypesEnum.HISTOGRAM
-        ? this.aggregationsService.getHistogramAggregation(
-            this.fieldName,
-            this.order
-          )
-        : this.aggregationsService.getFullSearchTermAggregation(
-            this.fieldName,
-            this.order
-          )
-    ).pipe(
-      map((agg) =>
-        agg.buckets.map((bucket) => ({
-          label: `${bucket.key} (${bucket.doc_count})`,
-          value: bucket.key.toString(),
+    this.choices$ = this.fieldsService.getAvailableValues(this.fieldName).pipe(
+      startWith([] as FieldAvailableValue[]),
+      map((values) =>
+        values.map((v) => ({
+          ...v,
+          value: v.value.toString(), // converting to string for the dropdown
         }))
-      ),
-      filter((choices) => !!choices),
-      startWith([])
+      )
     )
   }
 }
