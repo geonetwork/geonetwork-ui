b4fd76a62a5ac4ee3db071bb9e425231240354ce
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-04-09 18:20:47 +0200

    chore: run npx storybook@latest upgrade --config-dir apps/demo/.storybook
    As per https://storybook.js.org/docs/react/migration-guide#automatic-upgrade

--- apps/demo/.storybook/main.js
+++ apps/demo/.storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
-  core: { builder: 'webpack5' },
-  stories: [
-    '../../../libs/**/*.stories.mdx',
-    '../../../libs/**/*.stories.@(js|jsx|ts|tsx)',
-    '../../**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-essentials'],
-}
+  stories: ['../../../libs/**/*.stories.mdx', '../../../libs/**/*.stories.@(js|jsx|ts|tsx)', '../../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
