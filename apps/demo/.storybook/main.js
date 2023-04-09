fc4b4822a2130522440f0dc717e7942164a521a2
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-04-09 19:15:49 +0200

    chore(storybook): remove autodocs

--- apps/demo/.storybook/main.js
+++ apps/demo/.storybook/main.js
@@ -1,14 +1,15 @@
 module.exports = {
-  stories: ['../../../libs/**/*.stories.mdx', '../../../libs/**/*.stories.@(js|jsx|ts|tsx)', '../../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../../../libs/**/*.stories.mdx',
+    '../../../libs/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/angular',
-    options: {}
+    options: {},
   },
-  docs: {
-    autodocs: true
-  }
-};
+}
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
