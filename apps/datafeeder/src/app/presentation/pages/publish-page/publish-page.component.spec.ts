0a5c53bf815891d6db2561023c18ce56bea387e0
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-04-13 23:43:11 +0200

    test(datafeeder): refactor the test to use class mocks

--- apps/datafeeder/src/app/presentation/pages/publish-page/publish-page.component.spec.ts
+++ apps/datafeeder/src/app/presentation/pages/publish-page/publish-page.component.spec.ts
@@ -2,10 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { UtilI18nModule } from '@geonetwork-ui/util/i18n'
 import { TranslateModule } from '@ngx-translate/core'
 import { DatafeederFacade } from '../../../store/datafeeder.facade'
-
 import { PublishPageComponent } from './publish-page.component'
 import { NO_ERRORS_SCHEMA } from '@angular/core'
-import { Router, ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import {
   DataPublishingApiService,
   PublishJobStatusApiModel,
@@ -15,30 +14,34 @@ import { of } from 'rxjs'
 import { TestScheduler } from 'rxjs/testing'
 import { UiInputsModule } from '@geonetwork-ui/ui/inputs'
 
+const JOB_ID = '1234'
+
 const jobMock: PublishJobStatusApiModel = {
-  jobId: '1234',
+  jobId: JOB_ID,
   status: PublishStatusEnumApiModel.Done,
   progress: 1,
 }
 
-const publishServiceMock = {
-  getPublishingStatus: jest.fn(() => of(jobMock)),
+class PublishServiceMock {
+  getPublishingStatus = jest.fn(() => of(jobMock))
 }
-
-const activatedRouteMock = {
-  params: of({ id: 1 }),
+class FacadeMock {
+  setUpload = jest.fn()
 }
-
-const routerMock = {
-  navigate: jest.fn(),
+class ActivatedRouteMock {
+  params = of({ id: JOB_ID })
 }
-const facadeMock = {
-  setPublication: jest.fn(),
+class RouterMock {
+  navigate = jest.fn()
 }
 
 describe('SumUpPageComponent', () => {
   let component: PublishPageComponent
   let fixture: ComponentFixture<PublishPageComponent>
+  let facade: DatafeederFacade
+  let dataPublishingService: DataPublishingApiService
+  let activatedRoute: ActivatedRoute
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -48,13 +51,17 @@ describe('SumUpPageComponent', () => {
       providers: [
         {
           provide: DatafeederFacade,
-          useValue: facadeMock,
+          useClass: FacadeMock,
         },
-        { provide: DataPublishingApiService, useValue: publishServiceMock },
-        { provide: Router, useValue: routerMock },
-        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: DataPublishingApiService, useClass: PublishServiceMock },
+        { provide: Router, useClass: RouterMock },
+        { provide: ActivatedRoute, useClass: ActivatedRouteMock },
       ],
     }).compileComponents()
+    facade = TestBed.inject(DatafeederFacade)
+    dataPublishingService = TestBed.inject(DataPublishingApiService)
+    activatedRoute = TestBed.inject(ActivatedRoute)
+    router = TestBed.inject(Router)
   })
 
   beforeEach(() => {
@@ -78,8 +85,10 @@ describe('SumUpPageComponent', () => {
       }
       expectObservable(component.statusFetch$).toBe(expected, values)
     })
-    expect(publishServiceMock.getPublishingStatus).toHaveBeenCalledWith(1)
-    expect(facadeMock.setPublication).toHaveBeenCalledWith(jobMock)
+    expect(dataPublishingService.getPublishingStatus).toHaveBeenCalledWith(
+      JOB_ID
+    )
+    expect(facade.setPublication).toHaveBeenCalledWith(jobMock)
     expect(component.progress).toBe(100)
   })
 
@@ -91,8 +100,8 @@ describe('SumUpPageComponent', () => {
     })
 
     it('route to publishok page', () => {
-      expect(routerMock.navigate).toHaveBeenCalledWith(['/', 1, 'publishok'], {
-        relativeTo: activatedRouteMock,
+      expect(router.navigate).toHaveBeenCalledWith(['/', JOB_ID, 'publishok'], {
+        relativeTo: activatedRoute,
         queryParams: {},
       })
     })
