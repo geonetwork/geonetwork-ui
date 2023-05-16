ce59147322cdf053230029babbf2e566af1aa3d6
Tobias Kohr <tobias.kohr@camptocamp.com>
2023-05-16 17:27:54 +0200

    feat(record-md): generate permalinkUrl and display it for chart in copy-text-component

--- libs/feature/record/src/lib/record-metadata/record-metadata.component.ts
+++ libs/feature/record/src/lib/record-metadata/record-metadata.component.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  InjectionToken,
+  Optional,
+} from '@angular/core'
 import { SourcesService } from '@geonetwork-ui/feature/catalog'
 import { SearchService } from '@geonetwork-ui/feature/search'
 import { ErrorType } from '@geonetwork-ui/ui/elements'
-import { combineLatest } from 'rxjs'
-import { filter, map, mergeMap, pluck } from 'rxjs/operators'
+import { BehaviorSubject, combineLatest } from 'rxjs'
+import { filter, first, map, mergeMap, pluck } from 'rxjs/operators'
 import { MdViewFacade } from '../state/mdview.facade'
+import { Configuration } from '@geonetwork-ui/data-access/gn4'
 
 export const WEB_COMPONENT_EMBEDDER_URL = new InjectionToken<string>(
   'webComponentEmbedderUrl'
@@ -53,14 +60,30 @@ export class RecordMetadataComponent {
   )
 
   errorTypes = ErrorType
+  selectedTabIndex$ = new BehaviorSubject(0)
+  permalinkUrl$ = new BehaviorSubject<URL>(null)
 
   constructor(
     public facade: MdViewFacade,
     private searchService: SearchService,
-    private sourceService: SourcesService
-  ) {}
+    private sourceService: SourcesService,
+    @Inject(Configuration) private config: Configuration,
+    @Optional()
+    @Inject(WEB_COMPONENT_EMBEDDER_URL)
+    protected wcEmbedderBaseUrl: string
+  ) {
+    this.displayMap$
+      .pipe(
+        map((displayMap) => (displayMap ? 0 : 1)),
+        first()
+      )
+      .subscribe((value) => {
+        this.selectedTabIndex$.next(value)
+      })
+  }
 
-  onTabIndexChange(): void {
+  onTabIndexChange(index: number): void {
+    this.selectedTabIndex$.next(index)
     setTimeout(() => {
       window.dispatchEvent(new Event('resize'))
     }, 0)
@@ -73,5 +96,17 @@ export class RecordMetadataComponent {
     this.searchService.updateFilters({
       OrgForResource: { [contactOrgName]: true },
     })
+  }
+
+  setPermalinkUrl(event: URLSearchParams) {
+    if (event) {
+      const url = new URL(`${this.wcEmbedderBaseUrl}`, window.location.origin)
+      url.search = `?a=api-url=${this.config.basePath}
+&a=primary-color=%230f4395
+&a=secondary-color=%238bc832
+&a=main-color=%23555
+&a=background-color=%23fdfbff${event.toString()}`
+      this.permalinkUrl$.next(url)
+    }
   }
 }
