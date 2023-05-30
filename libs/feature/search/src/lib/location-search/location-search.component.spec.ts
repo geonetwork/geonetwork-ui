bac254de640664ffbe904bd7d0c7054d5c33949e
Angelika Kinas <angelika.kinas@camptocamp.com>
2023-05-30 16:28:13 +0200

    fix(test): Fix failing test by adding mock component

--- libs/feature/search/src/lib/location-search/location-search.component.spec.ts
+++ libs/feature/search/src/lib/location-search/location-search.component.spec.ts
@@ -1,6 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { AutocompleteItem } from '@geonetwork-ui/ui/inputs'
+import { TranslateModule } from '@ngx-translate/core'
+import { Observable } from 'rxjs'
 import { LocationSearchComponent } from './location-search.component'
+
+@Component({
+  selector: 'gn-ui-autocomplete',
+  template: `<div></div>`,
+})
+class MockAutoCompleteComponent {
+  @Input() placeholder: string
+  @Input() action: (value: string) => Observable<AutocompleteItem[]>
+  @Input() value?: AutocompleteItem
+  @Input() clearOnSelection = false
+  @Input() icon = 'search'
+  @Output() itemSelected = new EventEmitter<AutocompleteItem>()
+  @Output() inputSubmitted = new EventEmitter<string>()
+}
 
 describe('LocationSearchComponent', () => {
   let component: LocationSearchComponent
@@ -8,7 +25,8 @@ describe('LocationSearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LocationSearchComponent],
+      declarations: [LocationSearchComponent, MockAutoCompleteComponent],
+      imports: [TranslateModule.forRoot()],
     }).compileComponents()
 
     fixture = TestBed.createComponent(LocationSearchComponent)
