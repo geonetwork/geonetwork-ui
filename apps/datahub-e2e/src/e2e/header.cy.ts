575f1bce9ee2732087735eda7223e45726a636ea
Camille Moinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2023-06-08 15:27:04 +0200

    integration tests for header

--- apps/datahub-e2e/src/e2e/header.cy.ts
+++ apps/datahub-e2e/src/e2e/header.cy.ts
@@ -1,20 +1,100 @@
 describe('header', () => {
   beforeEach(() => cy.visit('/'))
 
-  it('should display the title', () => {
-    cy.get('[data-cy="dh-title"]')
-  })
-  it('should display the search with autocomplete result', () => {
-    cy.get('gn-ui-fuzzy-search').type('velo')
-    cy.get('mat-option').should('have.text', ' Accroches vélos MEL ')
-  })
-  it('should display the search results on click', () => {
-    cy.get('gn-ui-fuzzy-search').type('velo')
-    cy.get('mat-icon').contains('search').click()
-    cy.get('gn-ui-record-preview-row').should('have.length', 1)
-    cy.get('gn-ui-record-preview-row')
-      .find('div')
-      .first()
-      .should('have.attr', 'title', 'Accroches vélos MEL')
+    describe('general display', () => {
+      it('should display the title', () => {
+        cy.get('[data-cy="dh-title"]')
+      })
+      it('should display the header image', () => {
+        cy.get('header').should('have.css', 'background').and('include', 'header_bg.webp')
+      })
+      it('should display the search bar, button and placeholder', () => {
+        cy.get('gn-ui-fuzzy-search')
+        cy.get('gn-ui-autocomplete').should('have.length.gt', 0)
+        cy.get('mat-icon').contains('search')
+    })
+
+    describe('search actions', () => {
+      it('should display the search with autocomplete result', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo')
+        cy.get('mat-option').should('have.text', ' Accroches vélos MEL ')
+      })
+      it('should display the search results on click on icon', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo')
+        cy.get('mat-icon').contains('search').click()
+        cy.get('gn-ui-record-preview-row').should('have.length', 1)
+        cy.get('gn-ui-record-preview-row')
+          .find('div')
+          .first()
+          .should('have.attr', 'title', 'Accroches vélos MEL')
+      })
+      it('should display the search results on enter touch', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo{enter}')
+        cy.get('gn-ui-record-preview-row').should('have.length', 1)
+        cy.get('gn-ui-record-preview-row')
+          .find('div')
+          .first()
+          .should('have.attr', 'title', 'Accroches vélos MEL')
+      })
+      it('should go to dataset page when click on the autocomplete result', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo')
+        cy.get('mat-option').should('have.text', ' Accroches vélos MEL ')
+        cy.get('mat-option').click()
+        cy.url().should('include', '/dataset/')
+      })
+    })
+
+    describe('reset search actions', () => {
+      it('should create a cancel icon when typing', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo')
+        cy.get('mat-icon').contains('close')
+      })
+      it('should delete text input on click on cancel button', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo')          
+        cy.get('mat-icon').contains('close').click()
+        cy.get('gn-ui-autocomplete').find('div')
+        .find('input').should('have.value', '')
+      })
+      it('should reset search results on click on cancel button', () => {
+        cy.get('gn-ui-fuzzy-search').type('velo')
+        cy.get('mat-icon').contains('close').click()
+        cy.get('gn-ui-record-preview-row').should('have.length.gt', 1)
+      })
+    })
+
+    describe('filter search actions', () => {
+      it('should create two filter buttons upon loading page', () => {
+        cy.get('gn-ui-fuzzy-search')
+          .next()
+          .find('button')
+          .should('have.length', 2)
+      })
+
+      beforeEach(() => {
+        cy.visit('/home/search')
+        cy.get('gn-ui-record-preview-row').as('initialList')
+        cy.visit('/home/news')
+      })
+
+      it('should filter results by latest date', () => {
+        cy.get('gn-ui-fuzzy-search')
+          .next()
+          .find('button')
+          .first()
+          .click()   
+        cy.get('gn-ui-record-preview-row').should('not.eq', '@initialList')
+        cy.get('select#sort-by- option:selected').should('have.value', '-createDate')
+      })
+      it('should filter results by popularity', () => {
+        cy.get('gn-ui-fuzzy-search')
+          .next()
+          .find('button')
+          .eq(1)
+          .click()   
+        cy.get('gn-ui-record-preview-row').should('not.eq', '@initialList')
+        cy.get('select#sort-by- option:selected').should('have.value', '-userSavedCount')
+      })
+    })
+
   })
 })
