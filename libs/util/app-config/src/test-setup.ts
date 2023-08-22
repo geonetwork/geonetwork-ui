e9e68f7c213f4df576aaeb83e438dd0fd679cdd2
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-08-22 22:14:30 +0200

    test: fix error with app-config tests

--- libs/util/app-config/src/test-setup.ts
+++ libs/util/app-config/src/test-setup.ts
@@ -1,11 +1,11 @@
-import fetch from 'fetch-mock-jest'
 import 'jest-preset-angular/setup-jest'
+import '../../../../jest.setup'
 import { getTestBed } from '@angular/core/testing'
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing'
-import '../../../../jest.setup'
+import fetch from 'fetch-mock-jest'
 
 global.fetch = fetch as never
 
