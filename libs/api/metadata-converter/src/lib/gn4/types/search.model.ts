a09a3fb11c112134f372b3fcb526ee06c656459a
Florent gravin <florent.gravin@camptocamp.com>
2023-08-25 17:49:31 +0200

    refactor(es): clean useless interfaces

--- libs/api/metadata-converter/src/lib/gn4/types/search.model.ts
+++ libs/api/metadata-converter/src/lib/gn4/types/search.model.ts
@@ -7,15 +7,6 @@ export interface SearchFilters {
 
 type SearchFiltersFieldsLeaf = Record<string, boolean>
 
-export interface Organisation {
-  name: string
-  description?: string
-  logoUrl?: string
-  recordCount?: number
-  email?: string
-  emails?: string[]
-}
-
 export interface MetadataContact {
   name?: string
   organisation?: string
@@ -24,34 +15,6 @@ export interface MetadataContact {
   logoUrl?: string
   address?: string
   phone?: string
-}
-
-export interface MetadataRecord {
-  id: string
-  uuid: string
-  title: string
-  metadataUrl: string
-  abstract?: string
-  thumbnailUrl?: string
-  hasDownloads?: boolean
-  hasMaps?: boolean
-  updateStatus?: string
-  updateFrequency?: string
-  links?: MetadataLink[]
-  updatedOn?: Date
-  createdOn?: Date
-  dataUpdatedOn?: Date
-  dataCreatedOn?: Date
-  lineage?: string
-  keywords?: string[]
-  contact?: MetadataContact
-  resourceContacts?: MetadataContact[]
-  catalogUuid?: string
-  constraints?: string[]
-  favoriteCount?: number
-  isOpenData?: boolean
-  ownerInfo?: string
-  isPublishedToAll?: boolean
 }
 
 export enum MetadataLinkType {
@@ -64,23 +27,10 @@ export enum MetadataLinkType {
   OTHER,
 }
 
-export interface MetadataLink {
-  url: string
-  type: MetadataLinkType
-  // either a file name, a layer name or any other resource identifier
-  name?: string
-  protocol?: string
-  mimeType?: string
-  description?: string
-  label?: string
-}
-
 export interface RecordMetric {
   value: string
   recordCount: number
 }
-
-export const RESULTS_PAGE_SIZE = 10
 
 interface HitsObject {
   max_score?: number
