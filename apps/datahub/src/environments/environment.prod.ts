d61eae38164c04aa13ed8f68ee83014b1c2c4259
Tobias Kohr <tobias.kohr@camptocamp.com>
2023-11-07 10:12:31 +0100

    fix(version): set same env.version for prod as for dev
    this should fix the datahub latest build to point to the main web component dist branch instead of a version that does not exist yet

--- apps/datahub/src/environments/environment.prod.ts
+++ apps/datahub/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 import packageJson from '../../../../package.json'
 export const environment = {
   production: true,
-  version: `v${packageJson.version.split('-')[0]}`,
+  version:
+    packageJson.version.split('-')[1] === 'dev'
+      ? 'main'
+      : `v${packageJson.version}`,
 }
