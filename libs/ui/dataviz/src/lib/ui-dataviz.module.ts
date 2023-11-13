ec8c13d952c72ef06e30d05c7de36efdee4334dc
Angelika Kinas <angelika.kinas@camptocamp.com>
2023-11-13 15:41:49 +0100

    feat(DH): Improve plurals in text

--- libs/ui/dataviz/src/lib/ui-dataviz.module.ts
+++ libs/ui/dataviz/src/lib/ui-dataviz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FigureComponent } from './figure/figure.component'
 import { MatIconModule } from '@angular/material/icon'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, TranslateModule.forChild()],
   declarations: [FigureComponent],
   exports: [FigureComponent],
 })
