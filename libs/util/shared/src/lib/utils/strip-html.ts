086762582e2521f44dcd6ccd4cba8d013490f85d
Tobias Kohr <tobias.kohr@camptocamp.com>
2023-11-17 11:06:15 +0100

    fix(utils): handle undefined in html util functions
    this prevents displaying abstract 'undefined' as string in record-preview (although abstract should usually not be undefined)

--- libs/util/shared/src/lib/utils/strip-html.ts
+++ libs/util/shared/src/lib/utils/strip-html.ts
@@ -1,4 +1,5 @@
 export const stripHtml = function (html: string): string {
+  if (!html) return undefined
   const doc = new DOMParser().parseFromString(html, 'text/html')
   return doc.body.textContent || ''
 }
