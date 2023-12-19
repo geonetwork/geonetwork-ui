eef415df1c1865b558f3350e90d26220577819e0
Angelika Kinas <angelika.kinas@camptocamp.com>
2023-12-19 16:32:50 +0100

    feat(ui): Add translations for read more, read less

--- libs/ui/elements/src/lib/ui-elements.module.ts
+++ libs/ui/elements/src/lib/ui-elements.module.ts
@@ -26,7 +26,7 @@ import { FormsModule } from '@angular/forms'
 import { AvatarComponent } from './avatar/avatar.component'
 import { UserPreviewComponent } from './user-preview/user-preview.component'
 import { GnUiLinkifyDirective } from './metadata-info/linkify.directive'
-import { PaginationButtonsComponent } from './pagination-buttons/pagination-buttons.component';
+import { PaginationButtonsComponent } from './pagination-buttons/pagination-buttons.component'
 import { MaxLinesComponent } from './max-lines/max-lines.component'
 
 @NgModule({
