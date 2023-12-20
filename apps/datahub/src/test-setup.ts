2c3436c24cb42614867de0a1774123b916616b6d
Angelika Kinas <angelika.kinas@camptocamp.com>
2023-12-20 14:19:00 +0100

    feat(dh): Add resize observer to test-setup for datahub

--- apps/datahub/src/test-setup.ts
+++ apps/datahub/src/test-setup.ts
@@ -1,2 +1,9 @@
 import 'jest-preset-angular/setup-jest'
 import '../../../jest.setup'
+
+class ResizeObserverMock {
+  observe = jest.fn()
+  unobserve = jest.fn()
+}
+
+;(window as any).ResizeObserver = ResizeObserverMock
