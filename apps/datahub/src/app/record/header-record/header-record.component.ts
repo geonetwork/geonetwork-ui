6e8fe614b21a479baef27121f05dd57c45003446
Camille Moinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2024-01-12 15:38:46 +0100

    feat(dh): add geographical data type

--- apps/datahub/src/app/record/header-record/header-record.component.ts
+++ apps/datahub/src/app/record/header-record/header-record.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { SearchService } from '@geonetwork-ui/feature/search'
 import { getThemeConfig } from '@geonetwork-ui/util/app-config'
 import { CatalogRecord } from '@geonetwork-ui/common/domain/model/record'
+import { MdViewFacade } from '@geonetwork-ui/feature/record'
+import { combineLatest, map } from 'rxjs'
 
 @Component({
   selector: 'datahub-header-record',
@@ -16,7 +18,20 @@ export class HeaderRecordComponent {
     `center /cover url('assets/img/header_bg.webp')`
   foregroundColor = getThemeConfig().HEADER_FOREGROUND_COLOR || '#ffffff'
 
-  constructor(private searchService: SearchService) {}
+  constructor(
+    private searchService: SearchService,
+    public facade: MdViewFacade
+  ) {}
+
+  displayMap$ = combineLatest([
+    this.facade.mapApiLinks$,
+    this.facade.geoDataLinks$,
+  ]).pipe(
+    map(
+      ([mapLinks, geoDataLinks]) =>
+        mapLinks?.length > 0 || geoDataLinks?.length > 0
+    )
+  )
 
   get lastUpdate() {
     return this.metadata.recordUpdated.toLocaleString().split(',')[0]
