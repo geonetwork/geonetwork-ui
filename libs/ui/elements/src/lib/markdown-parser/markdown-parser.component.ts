33659321ceb68cae983f156eca046adff73ea3d7
cde-barros <Christopher.De-Barros@ign.fr>
2024-01-16 11:35:09 +0100

    Css is now encapsulated and storybook added for the component

--- libs/ui/elements/src/lib/markdown-parser/markdown-parser.component.ts
+++ libs/ui/elements/src/lib/markdown-parser/markdown-parser.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { marked } from 'marked'
 
 @Component({
   selector: 'gn-ui-markdown-parser',
   templateUrl: './markdown-parser.component.html',
   styleUrls: ['./markdown-parser.component.css'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class MarkdownParserComponent {
   @Input() textContent: string
