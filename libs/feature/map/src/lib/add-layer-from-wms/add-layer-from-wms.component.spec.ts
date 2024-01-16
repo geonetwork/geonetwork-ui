56240ff8f0af3cb10fb10f0b90823a71d27e26fb
Ronit Jadhav <ronitjadhav17@gmail.com>
2024-01-16 15:34:51 +0100

    resolved test errors

--- libs/feature/map/src/lib/add-layer-from-wms/add-layer-from-wms.component.spec.ts
+++ libs/feature/map/src/lib/add-layer-from-wms/add-layer-from-wms.component.spec.ts
@@ -1,15 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { AddLayerFromWmsComponent } from './add-layer-from-wms.component'
+import { MapFacade } from '../+state/map.facade'
+import { Store } from '@ngrx/store'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+
+class MapFacadeMock {
+  addLayer = jest.fn()
+}
 
 describe('AddLayerFromWmsComponent', () => {
   let component: AddLayerFromWmsComponent
   let fixture: ComponentFixture<AddLayerFromWmsComponent>
+  let mapFacade: MapFacade
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddLayerFromWmsComponent],
+      providers: [
+        {
+          provide: MapFacade,
+          useClass: MapFacadeMock,
+        }
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
 
+    mapFacade = TestBed.inject(MapFacade)
     fixture = TestBed.createComponent(AddLayerFromWmsComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
@@ -18,4 +34,5 @@ describe('AddLayerFromWmsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
 })
