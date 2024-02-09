eaf2a1f56936e342e79939998ba186e12977d0e1
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2024-02-09 13:41:47 +0100

    feat(ui): add an in place editable label

---
+++ libs/ui/inputs/src/lib/editable-label/editable-label.directive.stories.ts
@@ -0,0 +1,76 @@
+import { Component, Input } from '@angular/core'
+import { action } from '@storybook/addon-actions'
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular'
+import { EditableLabelDirective } from './editable-label.directive'
+
+@Component({
+  selector: 'gn-ui-editable-label-story',
+  template: `<h3
+    class="text-3xl font-bold"
+    [gnUiEditableLabel]="editable"
+    (editableLabelChanged)="handleEditableLabelChanged($event)"
+  >
+    {{ label }}
+  </h3>`,
+})
+class EditableLabelStoryComponent {
+  @Input() editable?: boolean
+  @Input() label: string
+
+  handleEditableLabelChanged = action('editableLabelChanged')
+}
+
+export default {
+  title: 'Inputs/EditableLabel',
+  component: EditableLabelStoryComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [EditableLabelDirective],
+      imports: [],
+    }),
+  ],
+} as Meta<EditableLabelStoryComponent>
+
+export const Editable: StoryObj<EditableLabelStoryComponent> = {
+  args: {
+    editable: true,
+    label: 'This is an in place editable label.',
+  },
+  render: (args) => ({
+    props: { ...args, editableLabelChanged: action('editableLabelChanged') },
+  }),
+}
+
+export const NonEditable: StoryObj<EditableLabelStoryComponent> = {
+  args: {
+    editable: false,
+    label: 'This is a non editable label.',
+  },
+  render: (args) => ({
+    props: { ...args, editableLabelChanged: action('editableLabelChanged') },
+  }),
+}
+
+export const EditableWithNewLinesAndSpaces: StoryObj<EditableLabelStoryComponent> =
+  {
+    args: {
+      editable: true,
+      label: `   This is a multi-line
+    editable label.  `,
+    },
+    render: (args) => ({
+      props: { ...args, editableLabelChanged: action('editableLabelChanged') },
+    }),
+  }
+
+export const NonEditableWithNewLinesAndSpaces: StoryObj<EditableLabelStoryComponent> =
+  {
+    args: {
+      editable: false,
+      label: `   This is a multi-line
+    non editable label.  `,
+    },
+    render: (args) => ({
+      props: { ...args, editableLabelChanged: action('editableLabelChanged') },
+    }),
+  }
---
+++ libs/ui/inputs/src/lib/editable-label/editable-label.directive.ts
@@ -0,0 +1,47 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+  EventEmitter,
+  Output,
+  Input,
+} from '@angular/core'
+
+@Directive({
+  selector: '[gnUiEditableLabel]',
+})
+export class EditableLabelDirective implements AfterViewInit {
+  @Output() editableLabelChanged = new EventEmitter<string>()
+  @Input() gnUiEditableLabel?: boolean
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    if (this.gnUiEditableLabel !== false) {
+      const appendedInput = this.renderer.createElement('input')
+
+      this.renderer.setStyle(appendedInput, 'background', 'inherit')
+      this.renderer.setStyle(appendedInput, 'color', 'inherit')
+      this.renderer.setStyle(appendedInput, 'font', 'inherit')
+      this.renderer.setStyle(appendedInput, 'border', 'inherit')
+      this.renderer.setStyle(appendedInput, 'width', '100%')
+      this.renderer.setStyle(appendedInput, 'padding', 'inherit')
+      this.renderer.setStyle(appendedInput, 'margin', '0')
+      this.renderer.setStyle(appendedInput, 'height', 'inherit')
+      this.renderer.setStyle(appendedInput, 'line-height', 'inherit')
+      this.renderer.setStyle(appendedInput, 'text-decoration', 'inherit')
+
+      const hostContent = this.el.nativeElement.textContent || ''
+      const formattedContent = hostContent.replace(/\s+/g, ' ').trim()
+      this.renderer.setProperty(appendedInput, 'value', formattedContent)
+      this.renderer.setProperty(this.el.nativeElement, 'innerHTML', '')
+
+      this.renderer.listen(appendedInput, 'input', (event) => {
+        this.editableLabelChanged.emit(event.target.value)
+      })
+
+      this.renderer.appendChild(this.el.nativeElement, appendedInput)
+    }
+  }
+}
