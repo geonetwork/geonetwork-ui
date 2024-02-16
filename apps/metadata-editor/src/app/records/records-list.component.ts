caed3a33dedadfed6eece0cffd44c037f067726f
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-02-16 11:56:30 +0100

    feat(editor): adapt records-list component to new results-table

--- apps/metadata-editor/src/app/records/records-list.component.ts
+++ apps/metadata-editor/src/app/records/records-list.component.ts
@@ -3,13 +3,14 @@ import { Component, Input } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
 import { Router } from '@angular/router'
 import { CatalogRecord } from '@geonetwork-ui/common/domain/model/record'
-import { SearchFacade, SearchService } from '@geonetwork-ui/feature/search'
+import {
+  ResultsTableComponent,
+  SearchFacade,
+  SearchService,
+} from '@geonetwork-ui/feature/search'
 import { UiSearchModule } from '@geonetwork-ui/ui/search'
 import { UiElementsModule } from '@geonetwork-ui/ui/elements'
-import { SortByField } from '@geonetwork-ui/common/domain/model/search'
 import { TranslateModule } from '@ngx-translate/core'
-import { SelectionService } from '@geonetwork-ui/api/repository'
-import { Subject } from 'rxjs'
 
 const includes = [
   'uuid',
@@ -34,27 +35,20 @@ const includes = [
     UiSearchModule,
     UiElementsModule,
     TranslateModule,
+    ResultsTableComponent,
   ],
 })
 export class RecordsListComponent {
   @Input() title: string
   @Input() logo: string
-  @Input() recordCount: number
-  @Input() userCount: number
-  @Input() users
   @Input() linkToDatahub?: string
 
   constructor(
     private router: Router,
     public searchFacade: SearchFacade,
-    public searchService: SearchService,
-    private selectionService: SelectionService
+    public searchService: SearchService
   ) {
     this.searchFacade.setPageSize(15).setConfigRequestFields(includes)
-  }
-
-  getRecords() {
-    return this.users ? this.users : this.searchFacade.results$
   }
 
   paginate(page: number) {
@@ -66,25 +60,5 @@ export class RecordsListComponent {
 
   editRecord(record: CatalogRecord) {
     this.router.navigate(['/edit', record.uniqueIdentifier])
-  }
-
-  setSortBy(newSortBy: SortByField) {
-    this.searchService.setSortBy(newSortBy)
-  }
-
-  showUsers() {
-    this.router.navigate(['/users/my-org'])
-  }
-
-  getSelectedRecords() {
-    return this.selectionService.selectedRecordsIdentifiers$
-  }
-
-  handleRecordsSelection(records: CatalogRecord[]) {
-    this.selectionService.selectRecords(records).subscribe()
-  }
-
-  handleRecordsDeselection(records: CatalogRecord[]) {
-    this.selectionService.deselectRecords(records).subscribe()
   }
 }
