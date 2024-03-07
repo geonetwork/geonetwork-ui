290ce53c8980696a953c0c8d3ab0f7777afc4750
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-03-07 13:29:24 +0100

    ci: upload cypress screenshots

--- apps/metadata-editor-e2e/cypress.config.js
+++ apps/metadata-editor-e2e/cypress.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'cypress'
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
 import { configureCommonPlugins } from '../../tools/e2e/plugins'
 import { cypressBrowserPermissionsPlugin } from 'cypress-browser-permissions'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__dirname),
+    ...nxE2EPreset(fileURLToPath(import.meta.url)),
     video: false,
     downloadsFolder: 'cypress/downloads',
     screenshotsFolder: 'cypress/screenshots',
