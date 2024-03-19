6e9ac076ea01b460626b701529790a45ab60f794
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-03-19 12:54:30 +0100

    feat(ui): improvements to buttons
    * use correct colors for spinners in buttons
    * correctly center button content
    * introduce css variables to customize the buttons parameters
    * rename tailwind component classes to make them more specific
    
    the light and outline buttons have a white bg by default but that can
    be changed using --gn-ui-button-background

--- libs/ui/inputs/src/lib/button/button.component.stories.ts
+++ libs/ui/inputs/src/lib/button/button.component.stories.ts
@@ -6,6 +6,7 @@ import {
   UtilI18nModule,
 } from '@geonetwork-ui/util/i18n'
 import { MatIconModule } from '@angular/material/icon'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 export default {
   title: 'Inputs/ButtonComponent',
@@ -16,6 +17,7 @@ export default {
         UtilI18nModule,
         TranslateModule.forRoot(TRANSLATE_DEFAULT_CONFIG),
         MatIconModule,
+        MatProgressSpinnerModule,
       ],
     }),
   ],
@@ -38,21 +40,47 @@ export const Primary: StoryObj<ButtonComponentWithContent> = {
   },
   render: (args) => ({
     props: args,
-    template: `<div class='flex flex-row gap-5'>
+    template: `<div class='flex flex-row gap-5 flex-wrap'>
   <gn-ui-button [type]="type" [disabled]="disabled" [extraClass]="extraClass">
     {{ content }}
   </gn-ui-button>
   <gn-ui-button [type]="type" [disabled]="disabled" [extraClass]="extraClass">
     with an icon&nbsp;<mat-icon class="material-symbols-outlined">downloading</mat-icon>
   </gn-ui-button>
-  <gn-ui-button class="text-[1.5em]" [type]="type" [disabled]="disabled" [extraClass]="extraClass">
-    <mat-icon class='material-symbols-outlined'>globe_asia</mat-icon>&nbsp;bigger
+  <gn-ui-button [type]="type" [disabled]="disabled"
+    [style.--gn-ui-button-font-size]='"1.5em"'
+    [extraClass]="extraClass">
+    <mat-icon class='material-symbols-outlined'>globe_asia</mat-icon>&nbsp;bigger (with variable)
   </gn-ui-button>
-  <gn-ui-button class="text-[0.7em]" [type]="type" [disabled]="disabled" [extraClass]="extraClass">
-    <mat-icon class='material-symbols-outlined'>pest_control</mat-icon>&nbsp;smaller
+  <gn-ui-button [type]="type" [disabled]="disabled"
+    [style.font-size]='"0.7em"'
+    [extraClass]="extraClass">
+    <mat-icon class='material-symbols-outlined'>pest_control</mat-icon>&nbsp;smaller (with css)
   </gn-ui-button>
-  <gn-ui-button [type]="type" [disabled]="disabled" [extraClass]="extraClass + ' !px-[3em] !py-[0.5em]'">
+  <gn-ui-button [type]="type" [disabled]="disabled"
+    [style.--gn-ui-button-padding]='"0.5em 3em"'
+    [extraClass]="extraClass">
     different&nbsp;<mat-icon class="material-symbols-outlined">waves</mat-icon>&nbsp;shape
+  </gn-ui-button>
+  <gn-ui-button [type]="type" [disabled]="disabled"
+    [style.--gn-ui-button-rounded]='"10px"'
+    [extraClass]="extraClass">
+    different corners
+  </gn-ui-button>
+  <div class='h-[150px] w-[150px] resize overflow-auto border border-gray-200 p-3'>
+    <gn-ui-button [type]="type" [disabled]="disabled"
+      [style.--gn-ui-button-width]='"100%"' [style.--gn-ui-button-height]='"100%"'
+      [extraClass]="extraClass">
+      <mat-icon class="material-symbols-outlined">fit_screen</mat-icon>&nbsp;resize it
+    </gn-ui-button>
+  </div>
+  <gn-ui-button [type]="type" [disabled]="disabled" [extraClass]="extraClass">
+    <mat-spinner [diameter]="32"></mat-spinner>
+  </gn-ui-button>
+  <gn-ui-button [type]="type" [disabled]="disabled"
+    [style.--gn-ui-button-background]='"rgba(168,255,235,1)"'
+    [extraClass]="extraClass">
+    different background
   </gn-ui-button>
 </div>`,
   }),
