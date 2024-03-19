8a750fe26824b7c4940e13c4f2eafcd5fae5d69a
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-03-19 16:00:12 +0100

    feat(edit): rework field config, allow expressions

---
+++ libs/feature/editor/src/lib/expressions.ts
@@ -0,0 +1,16 @@
+import { EditorFieldState, EditorFieldValue } from './models/fields.model'
+
+export type ExpressionEvaluator = (field: EditorFieldState) => EditorFieldValue
+
+export function evaluate(expression: string): ExpressionEvaluator {
+  if (expression.match(/^\${.*}$/)) {
+    return evaluate(expression.slice(2, -1))
+  }
+  const operator = expression.split('(')[0]
+  switch (operator) {
+    case 'dateNow':
+      return () => new Date()
+    default:
+      throw new Error(`Unknown operator: ${operator}`)
+  }
+}
