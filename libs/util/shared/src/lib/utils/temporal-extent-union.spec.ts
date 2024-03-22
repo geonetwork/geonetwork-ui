064f283517f0472e504bd6a73db5e530d26c86a8
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-03-22 17:15:56 +0100

    feat(util): create temporal extent union lib

---
+++ libs/util/shared/src/lib/utils/temporal-extent-union.spec.ts
@@ -0,0 +1,62 @@
+import { getTemporalRangeUnion } from './temporal-extent-union'
+
+describe('getTemporalRangeUnion', () => {
+  it('should return the union of temporal ranges', () => {
+    const ranges = [
+      { start: new Date('2022-01-01'), end: new Date('2022-01-10') },
+      { start: new Date('2022-01-05'), end: new Date('2022-01-15') },
+      { start: new Date('2022-01-12'), end: new Date('2022-01-20') },
+    ]
+
+    const expectedUnion = {
+      start: '1/1/2022',
+      end: '1/20/2022',
+    }
+
+    const union = getTemporalRangeUnion(ranges)
+
+    expect(union).toEqual(expectedUnion)
+  })
+
+  it('should return null if no ranges are provided', () => {
+    const ranges = []
+
+    const union = getTemporalRangeUnion(ranges)
+
+    expect(union).toBe(undefined)
+  })
+
+  it('should return an undefined end date if one range does not have it', () => {
+    const ranges = [
+      { start: new Date('2022-01-01') },
+      { start: new Date('2022-01-05'), end: new Date('2022-01-15') },
+      { start: new Date('2022-01-12'), end: new Date('2022-01-20') },
+    ]
+
+    const expectedUnion = {
+      start: '1/1/2022',
+      end: undefined,
+    }
+
+    const union = getTemporalRangeUnion(ranges)
+
+    expect(union).toEqual(expectedUnion)
+  })
+
+  it('should return an undefined start date if one range does not have it', () => {
+    const ranges = [
+      { start: new Date('2022-01-01'), end: new Date('2022-01-10') },
+      { end: new Date('2022-01-15') },
+      { start: new Date('2022-01-12'), end: new Date('2022-01-20') },
+    ]
+
+    const expectedUnion = {
+      start: undefined,
+      end: '1/20/2022',
+    }
+
+    const union = getTemporalRangeUnion(ranges)
+
+    expect(union).toEqual(expectedUnion)
+  })
+})
