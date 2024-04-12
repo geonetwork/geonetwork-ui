fd0139e9aabd8061c13c87847515916475b463c0
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-04-12 14:26:17 +0200

    feat(widgets): add notification component

---
+++ libs/ui/elements/src/lib/notification/notification.component.stories.ts
@@ -0,0 +1,65 @@
+import {
+  componentWrapperDecorator,
+  Meta,
+  moduleMetadata,
+  StoryObj,
+} from '@storybook/angular'
+import { NotificationComponent } from './notification.component'
+import { MatIconModule } from '@angular/material/icon'
+
+export default {
+  title: 'Elements/NotificationComponent',
+  component: NotificationComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatIconModule],
+    }),
+    componentWrapperDecorator(
+      (story) => `
+<div class="border border-gray-300 p-2" style="width: 600px; resize: horizontal; overflow: auto">
+  ${story}
+</div>`
+    ),
+  ],
+  argTypes: {
+    notificationClose: {
+      action: 'notificationClose',
+    },
+    type: {
+      control: false,
+    },
+  },
+} as Meta<NotificationComponent>
+
+export const Primary: StoryObj<NotificationComponent> = {
+  args: {
+    title: '',
+    text: "This is a message relevant for the current context! Close me once you're done reading it.",
+    closeMessage: 'Understood',
+  },
+  render: (args) => ({
+    props: args,
+    template: `<div class='flex flex-col gap-5'>
+  <gn-ui-notification type='success'
+      [title]="title || 'Good news, everyone!'"
+      [text]='text'
+      [closeMessage]='closeMessage'
+      (notificationClose)='notificationClose($event)'></gn-ui-notification>
+  <gn-ui-notification type='warning'
+      [title]="title || 'Uh oh!'"
+      [text]='text'
+      [closeMessage]='closeMessage'
+      (notificationClose)='notificationClose($event)'></gn-ui-notification>
+  <gn-ui-notification type='error'
+      [title]="title || 'Can\\'t connect'"
+      [text]='text'
+      [closeMessage]='closeMessage'
+      (notificationClose)='notificationClose($event)'></gn-ui-notification>
+  <gn-ui-notification type='info'
+      [title]="title || 'Hey, did you know...'"
+      [text]='text'
+      [closeMessage]='closeMessage'
+      (notificationClose)='notificationClose($event)'></gn-ui-notification>
+</div>`,
+  }),
+}
---
+++ libs/ui/elements/src/lib/notification/notification.component.ts
@@ -0,0 +1,31 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { MatIconModule } from '@angular/material/icon'
+import { ButtonComponent } from '@geonetwork-ui/ui/inputs'
+
+@Component({
+  selector: 'gn-ui-notification',
+  standalone: true,
+  imports: [CommonModule, MatIconModule, ButtonComponent],
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotificationComponent {
+  @Input() type: 'info' | 'warning' | 'error' | 'success' = 'info'
+  @Input() title: string
+  @Input() text: string
+  @Input() closeMessage?: string
+  @Output() notificationClose = new EventEmitter<void>()
+
+  handleClose(event?: Event) {
+    event?.preventDefault()
+    this.notificationClose.emit()
+  }
+}
