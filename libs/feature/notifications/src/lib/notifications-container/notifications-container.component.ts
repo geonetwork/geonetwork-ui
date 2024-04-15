dc8bfa78eed46ce3db7a54a055a6abd494726eb8
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-04-15 09:52:18 +0200

    feat(notifications): add animation for notifications

--- libs/feature/notifications/src/lib/notifications-container/notifications-container.component.ts
+++ libs/feature/notifications/src/lib/notifications-container/notifications-container.component.ts
@@ -2,6 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { NotificationsService } from '../notifications.service'
 import { NotificationComponent } from '@geonetwork-ui/ui/elements'
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations'
 
 @Component({
   selector: 'gn-ui-notifications-container',
@@ -10,6 +17,23 @@ import { NotificationComponent } from '@geonetwork-ui/ui/elements'
   templateUrl: './notifications-container.component.html',
   styleUrls: ['./notifications-container.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('enterExit', [
+      transition(':enter', [
+        animate(
+          '150ms',
+          keyframes([
+            style({ transform: 'scale(1)', opacity: 0 }),
+            style({ transform: 'scale(1.03)', opacity: 0.5 }),
+            style({ transform: 'scale(1)', opacity: 1 }),
+          ])
+        ),
+      ]),
+      transition(':leave', [
+        animate('200ms', style({ transform: 'translateX(50px)', opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class NotificationsContainerComponent {
   constructor(protected notificationsService: NotificationsService) {}
