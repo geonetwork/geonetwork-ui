9f24be9c9e103017be8584ad5f74d20b76f82d73
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2024-04-25 13:52:51 +0200

    feat(ui-inputs): change date-range-picker to standalone

--- libs/ui/inputs/src/lib/date-range-picker/date-range-picker.component.ts
+++ libs/ui/inputs/src/lib/date-range-picker/date-range-picker.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core'
-import { MatDatepickerInputEvent } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
+import {
+  MatDatepickerInputEvent,
+  MatDatepickerModule,
+} from '@angular/material/datepicker'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'gn-ui-date-range-picker',
   templateUrl: './date-range-picker.component.html',
   styleUrls: ['./date-range-picker.component.css'],
+  standalone: true,
+  imports: [MatIconModule, MatNativeDateModule, MatDatepickerModule],
 })
 export class DateRangePickerComponent {
   startDate: Date
