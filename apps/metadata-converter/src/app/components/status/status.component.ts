91015080bbbcc34b2ca5a5fefa4a72eb824e72df
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-07-04 14:37:41 +0200

    feat(md-converter): handle DCAT-AP, transform between formats

--- apps/metadata-converter/src/app/components/status/status.component.ts
+++ apps/metadata-converter/src/app/components/status/status.component.ts
@@ -1,7 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { CatalogRecord } from '@geonetwork-ui/common/domain/model/record'
-import { findConverterForDocument } from '@geonetwork-ui/api/metadata-converter'
-import { Iso191153Converter } from '@geonetwork-ui/api/metadata-converter'
+import {
+  BaseConverter,
+  findConverterForDocument,
+  Iso191153Converter,
+} from '@geonetwork-ui/api/metadata-converter'
+import { FORMATS, getFormatName } from '../../md-formats'
 
 @Component({
   selector: 'gn-ui-status',
@@ -9,35 +13,24 @@ import { Iso191153Converter } from '@geonetwork-ui/api/metadata-converter'
   styleUrls: ['./status.component.css'],
 })
 export class StatusComponent {
+  _currentRecord: CatalogRecord
   @Input() set recordNative(value: CatalogRecord) {
-    const start = performance.now()
-    this.status = 'Converting to ISO9139...'
-    this.newMetadata.emit('')
-    this.recordToXml(value)
-      .then((output) => {
-        this.newMetadata.emit(output)
-        const time = Math.round(performance.now() - start)
-        this.status = `Converting to ISO9139... Done (${time} ms).`
-      })
-      .catch((e) => {
-        this.status = `Converting to ISO9139... Failed: ${
-          e instanceof Error ? e.message : e
-        }`
-        console.error(e)
-      })
+    this._currentRecord = value
+    this.convertRecordToXml(value)
   }
   @Input() set currentMetadata(value: string) {
     const start = performance.now()
-    this.status = 'Converting to native format...'
+    this.status = 'Converting to CatalogRecord...'
     this.xmlToRecord(value)
       .then((output) => {
+        this._currentRecord = output
         this.newRecordNative.emit(output)
         this.newMetadata.emit(value)
         const time = Math.round(performance.now() - start)
-        this.status = `Converting to native format... Done (${time} ms).`
+        this.status = `Converting to CatalogRecord... Done (${time} ms).`
       })
       .catch((e) => {
-        this.status = `Converting to native format... Failed: ${
+        this.status = `Converting to CatalogRecord... Failed: ${
           e instanceof Error ? e.message : e
         }`
         console.error(e)
@@ -47,6 +40,8 @@ export class StatusComponent {
 
   @Output() newRecordNative = new EventEmitter<CatalogRecord>()
   @Output() newMetadata = new EventEmitter<string>()
+
+  currentConverter: BaseConverter<string> = null
 
   status = 'Standing by.'
 
@@ -64,15 +59,60 @@ export class StatusComponent {
     this.status = `Reading file... Failed`
   }
 
-  private recordToXml(record: CatalogRecord) {
-    const converter = this.referenceMetadata
-      ? findConverterForDocument(this.referenceMetadata)
-      : new Iso191153Converter()
-    return converter.writeRecord(record, this.referenceMetadata)
+  public changeFormat(format: string) {
+    const converterClass = FORMATS[format]
+    if (!converterClass) {
+      throw new Error(`Metadata format ${format} not supported`)
+    }
+    this.referenceMetadata = ''
+    this.convertRecordToXml(this._currentRecord, new converterClass())
+  }
+
+  private convertRecordToXml(
+    record: CatalogRecord,
+    currentConverter?: BaseConverter<string>
+  ) {
+    const start = performance.now()
+    const converter = currentConverter ?? this.currentConverter
+    const converterName = getFormatName(converter)
+    this.status = `Converting to ${converterName}...`
+    this.newMetadata.emit('')
+    this.recordToXml(record, currentConverter)
+      .then((output) => {
+        this.newMetadata.emit(output)
+        const time = Math.round(performance.now() - start)
+        this.status = `Converting to ${converterName}... Done (${time} ms).`
+      })
+      .catch((e) => {
+        this.status = `Converting to ${converterName}... Failed: ${
+          e instanceof Error ? e.message : e
+        }`
+        console.error(e)
+      })
+  }
+
+  private recordToXml(
+    record: CatalogRecord,
+    currentConverter?: BaseConverter<string>
+  ) {
+    try {
+      this.currentConverter = currentConverter
+        ? currentConverter
+        : this.referenceMetadata
+        ? findConverterForDocument(this.referenceMetadata)
+        : new Iso191153Converter()
+      return this.currentConverter.writeRecord(record, this.referenceMetadata)
+    } catch (e) {
+      return Promise.reject(e)
+    }
   }
 
   private xmlToRecord(metadata: string) {
-    const converter = findConverterForDocument(metadata)
-    return converter.readRecord(metadata)
+    try {
+      this.currentConverter = findConverterForDocument(metadata)
+      return this.currentConverter.readRecord(metadata)
+    } catch (e) {
+      return Promise.reject(e)
+    }
   }
 }
