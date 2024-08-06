70f05d033ff55cfa87cd09421c154e9288a9615b
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2024-08-06 15:42:29 +0200

    e2e(editor): delete record and draft

---
+++ apps/metadata-editor-e2e/src/e2e/delete.cy.ts
@@ -0,0 +1,62 @@
+describe('delete', () => {
+  beforeEach(() => {
+    cy.login('admin', 'admin', false)
+    cy.visit('/catalog/search')
+  })
+
+  describe('record with draft', () => {
+    it('should delete the record, delete its associated draft and refresh the interface', () => {
+      // First create a record and its draft
+      cy.get('[data-cy="create-record"]').click()
+      cy.get('gn-ui-form-field[ng-reflect-model=abstract] textarea').type(
+        'record abstract'
+      )
+      cy.get('md-editor-publish-button').click()
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000) // waiting for publication
+      cy.get('gn-ui-form-field[ng-reflect-model=abstract] textarea').type(
+        'draft abstract'
+      )
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000) // waiting for draft saving to kick in
+      cy.visit('/catalog/search?_sort=-changeDate')
+      cy.get('[data-cy="table-row"]')
+        .contains('My new record')
+        .should('have.length', 1)
+      cy.get('[data-cy="dashboard-drafts-count"]').should('contain', '1')
+      // Delete the record
+      cy.get('[data-test="record-menu-button"]').first().click()
+      cy.get('[data-test="record-menu-delete-button"]').click()
+      cy.get('[data-cy="confirm-button"]').click()
+      cy.get('[data-cy="table-row"]')
+        .contains('My new record')
+        .should('have.length', 0)
+      cy.get('[data-cy="dashboard-drafts-count"]').should('contain', '0')
+    })
+  })
+
+  describe('draft without record', () => {
+    it('should delete the draft and refresh the interface', () => {
+      // First create a draft
+      cy.get('[data-cy="create-record"]').click()
+      cy.get('gn-ui-form-field[ng-reflect-model=abstract] textarea').type(
+        'draft abstract'
+      )
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000) // waiting for draft saving to kick in
+      cy.visit('/my-space/my-draft')
+      cy.get('[data-cy="table-row"]')
+        .contains('My new record')
+        .should('have.length', 1)
+      cy.get('[data-cy="dashboard-drafts-count"]').should('contain', '1')
+      // Delete the draft
+      cy.get('[data-test="record-menu-button"]').click()
+      cy.get('[data-test="record-menu-delete-button"]').click()
+      cy.get('[data-cy="confirm-button"]').click()
+      cy.get('[data-cy="table-row"]')
+        .contains('New record')
+        .should('have.length', 0)
+      cy.get('[data-cy="dashboard-drafts-count"]').should('contain', '0')
+    })
+  })
+})
