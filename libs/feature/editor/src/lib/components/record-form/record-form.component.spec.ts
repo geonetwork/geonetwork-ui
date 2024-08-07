301e7a3d984a7a59372059d8383a2449a24fb6da
Angelika Kinas <angelika.kinas@camptocamp.com>
2024-08-07 16:41:48 +0200

    Fix unit test record form

--- libs/feature/editor/src/lib/components/record-form/record-form.component.spec.ts
+++ libs/feature/editor/src/lib/components/record-form/record-form.component.spec.ts
@@ -1,9 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { EditorFacade } from '../../+state/editor.facade'
 import { RecordFormComponent } from './record-form.component'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreModule } from '@ngrx/store'
+import { PlatformServiceInterface } from '@geonetwork-ui/common/domain/platform.service.interface'
+import { Gn4Converter } from '@geonetwork-ui/api/metadata-converter'
+import { of } from 'rxjs'
 
 class EditorFacadeMock {
   updateRecordField = jest.fn()
+}
+
+class Gn4MetadataMapperMock {
+  readRecords = jest.fn((records) =>
+    Promise.all(records.map((r) => this.readRecord(r)))
+  )
+  readRecord = jest.fn((record) => Promise.resolve(record))
+}
+
+class PlatformServiceInterfaceMock {
+  getMe = jest.fn(() => of({}))
 }
 
 describe('RecordFormComponent', () => {
@@ -12,11 +28,23 @@ describe('RecordFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RecordFormComponent],
+      imports: [
+        RecordFormComponent,
+        EffectsModule.forRoot(),
+        StoreModule.forRoot({}),
+      ],
       providers: [
         {
           provide: EditorFacade,
           useClass: EditorFacadeMock,
+        },
+        {
+          provide: PlatformServiceInterface,
+          useClass: PlatformServiceInterfaceMock,
+        },
+        {
+          provide: Gn4Converter,
+          useClass: Gn4MetadataMapperMock,
         },
       ],
     }).compileComponents()
