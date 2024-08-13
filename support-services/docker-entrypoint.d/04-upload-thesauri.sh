c8c4ee7032dbf0edb635dbf33eb7870403ad00e9
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-08-13 13:57:29 +0200

    ci: add delay when loading thesauri in GN
    This will avoid the thesauri to somehow be wiped (??) during GN init, thus
    making subsequent E2E fail

--- support-services/docker-entrypoint.d/04-upload-thesauri.sh
+++ support-services/docker-entrypoint.d/04-upload-thesauri.sh
@@ -6,6 +6,9 @@ xsrf_token=$(cat /xsrf_token)
 
 echo "Uploading thesauri to GeoNetwork..."
 
+# we're waiting a bit here for GN to be ready to accept thesauri (i.e. the default ones are set up)
+sleep 4
+
 for f in /docker-entrypoint.d/thesauri/*.rdf
 do
   curl -s "http://$host/geonetwork/srv/api/registries/vocabularies" \
@@ -19,12 +22,12 @@ do
 done
 
 # this shows the registered thesauri in GN
-#curl "http://$host/geonetwork/srv/fre/thesaurus?_content_type=json" \
-#  -H 'Accept: application/json, text/plain, */*' \
-#  -H 'Content-Type: multipart/form-data' \
-#  -H 'Accept: application/json, text/plain, */*' \
-#  -H "Cookie: JSESSIONID=$jsessionid; XSRF-TOKEN=$xsrf_token" \
-#  -H "X-XSRF-TOKEN: $xsrf_token"
-#echo ""
+curl -s --no-progress-meter "http://$host/geonetwork/srv/fre/thesaurus?_content_type=json" \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Content-Type: multipart/form-data' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H "Cookie: JSESSIONID=$jsessionid; XSRF-TOKEN=$xsrf_token" \
+  -H "X-XSRF-TOKEN: $xsrf_token"
+echo ""
 
 echo "Thesauri uploaded to GeoNetwork."
