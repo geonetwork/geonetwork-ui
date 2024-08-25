788bdddc7d639ea3c04cf7fb39012080a50e955c
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2024-08-25 10:20:59 +0200

    change owner on saving

--- apps/metadata-editor/src/app/edit/components/publish-button/publish-button.component.spec.ts
+++ apps/metadata-editor/src/app/edit/components/publish-button/publish-button.component.spec.ts
@@ -1,31 +1,80 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { PublishButtonComponent } from './publish-button.component'
 import { EditorFacade } from '@geonetwork-ui/feature/editor'
-import { BehaviorSubject, firstValueFrom } from 'rxjs'
+import { BehaviorSubject, firstValueFrom, of } from 'rxjs'
 import { TranslateModule } from '@ngx-translate/core'
+import { HttpClientModule } from '@angular/common/http'
+import { PlatformServiceInterface } from '@geonetwork-ui/common/domain/platform.service.interface'
+import { USER_FIXTURE } from '@geonetwork-ui/common/fixtures'
+import {
+  GroupsApiService,
+  RecordsApiService,
+} from '@geonetwork-ui/data-access/gn4'
 
 class EditorFacadeMock {
   changedSinceSave$ = new BehaviorSubject(false)
   saving$ = new BehaviorSubject(false)
+  activeUser$ = new BehaviorSubject({})
+  record$ = new BehaviorSubject({
+    ownerOrganization: { name: 'Group 1', id: 1 },
+    uniqueIdentifier: 304,
+  })
   saveRecord = jest.fn()
+}
+
+const user = USER_FIXTURE()
+const groups = [
+  { id: 1, name: 'Group 1' },
+  { id: 2, name: 'Group 2' },
+  { id: 3, name: 'Group 3' },
+]
+
+class PlatformServiceMock {
+  getMe = jest.fn(() => new BehaviorSubject(user))
+}
+
+class GroupsApiServiceMock {
+  getGroups = jest.fn().mockReturnValue(of(groups))
+}
+
+class RecordsApiServiceMock {
+  setRecordOwnership = jest.fn().mockReturnValue(of({}))
 }
 
 describe('PublishButtonComponent', () => {
   let component: PublishButtonComponent
   let fixture: ComponentFixture<PublishButtonComponent>
   let facade: EditorFacadeMock
+  let recordsApiService: RecordsApiService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PublishButtonComponent, TranslateModule.forRoot()],
+      imports: [
+        PublishButtonComponent,
+        TranslateModule.forRoot(),
+        HttpClientModule,
+      ],
       providers: [
         {
           provide: EditorFacade,
           useClass: EditorFacadeMock,
         },
+        {
+          provide: PlatformServiceInterface,
+          useClass: PlatformServiceMock,
+        },
+        {
+          provide: GroupsApiService,
+          useClass: GroupsApiServiceMock,
+        },
+        {
+          provide: RecordsApiService,
+          useClass: RecordsApiServiceMock,
+        },
       ],
     }).compileComponents()
 
+    recordsApiService = TestBed.inject(RecordsApiService)
     facade = TestBed.inject(EditorFacade) as any
     fixture = TestBed.createComponent(PublishButtonComponent)
     component = fixture.componentInstance
@@ -65,11 +114,16 @@ describe('PublishButtonComponent', () => {
   })
 
   describe('#saveRecord', () => {
-    beforeEach(() => {
-      component.saveRecord()
+    beforeEach(async () => {
+      await component.saveRecord()
     })
     it('should call facade.saveRecord', () => {
       expect(facade.saveRecord).toHaveBeenCalled()
+      expect(recordsApiService.setRecordOwnership).toHaveBeenCalledWith(
+        304,
+        1,
+        46798
+      )
     })
   })
 })
--- apps/metadata-editor/src/app/edit/components/publish-button/publish-button.component.ts
+++ apps/metadata-editor/src/app/edit/components/publish-button/publish-button.component.ts
@@ -7,7 +7,13 @@ import { MatTooltipModule } from '@angular/material/tooltip'
 import { TranslateModule } from '@ngx-translate/core'
 import { MatIconModule } from '@angular/material/icon'
 import { combineLatest, Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { map, take } from 'rxjs/operators'
+import {
+  GroupsApiService,
+  RecordsApiService,
+} from '@geonetwork-ui/data-access/gn4'
+import { PlatformServiceInterface } from '@geonetwork-ui/common/domain/platform.service.interface'
+import { UserModel } from '@geonetwork-ui/common/domain/model/user'
 
 export type RecordSaveStatus = 'saving' | 'upToDate' | 'hasChanges'
 
@@ -42,9 +48,42 @@ export class PublishButtonComponent {
     })
   )
 
-  constructor(private facade: EditorFacade) {}
+  record$ = this.facade.record$
+  activeUser$: Observable<UserModel>
+
+  constructor(
+    private facade: EditorFacade,
+    private recordsApiService: RecordsApiService,
+    private platformService: PlatformServiceInterface,
+    private groupApiService: GroupsApiService
+  ) {
+    this.activeUser$ = this.platformService.getMe()
+  }
 
   saveRecord() {
-    this.facade.saveRecord()
+    combineLatest([this.activeUser$, this.record$])
+      .pipe(take(1))
+      .subscribe(([userId, record]) => {
+        this.groupApiService.getGroups().subscribe((groups) => {
+          const groupId = groups.find(
+            (grp) => grp.name === record.ownerOrganization.name
+          )
+          // setting to 0 by default because the API expects a number and not null & most records don't have a group
+          this.recordsApiService
+            .setRecordOwnership(
+              record.uniqueIdentifier,
+              groupId ? groupId.id : 0,
+              Number(userId.id)
+            )
+            .subscribe(
+              () => {
+                this.facade.saveRecord()
+              },
+              (error) => {
+                console.log('Error setting ownership :', error)
+              }
+            )
+        })
+      })
   }
 }
