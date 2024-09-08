38303b29c67b02bcbebb263059c001ecb3cf8a40
Olivia Guyot <olivia.guyot@camptocamp.com>
2024-09-08 11:12:45 +0200

    feat(ui): improve date inputs to match other types of inputs

--- libs/ui/inputs/src/lib/date-picker/date-picker.component.ts
+++ libs/ui/inputs/src/lib/date-picker/date-picker.component.ts
@@ -8,6 +8,7 @@ import {
 import { MatNativeDateModule } from '@angular/material/core'
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatIconModule } from '@angular/material/icon'
+import { ButtonComponent } from '../button/button.component'
 
 @Component({
   selector: 'gn-ui-date-picker',
@@ -15,7 +16,12 @@ import { MatIconModule } from '@angular/material/icon'
   styleUrls: ['./date-picker.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [MatIconModule, MatNativeDateModule, MatDatepickerModule],
+  imports: [
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    ButtonComponent,
+  ],
 })
 export class DatePickerComponent {
   @Input() date: Date
