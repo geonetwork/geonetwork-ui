197304cb05f92fdaf96a3fe565915ca6ac1860f8
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-09-24 16:47:49 +0200

    feat(search): navigate to record on autocomplete select

--- apps/metadata-editor/src/app/dashboard/search-header/search-header.component.ts
+++ apps/metadata-editor/src/app/dashboard/search-header/search-header.component.ts
@@ -2,16 +2,12 @@ import { CommonModule } from '@angular/common'
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
 import { LetDirective } from '@ngrx/component'
-import {
-  FeatureSearchModule,
-  SearchService,
-} from '@geonetwork-ui/feature/search'
+import { FeatureSearchModule } from '@geonetwork-ui/feature/search'
 import { UiElementsModule } from '@geonetwork-ui/ui/elements'
 import { AvatarServiceInterface } from '@geonetwork-ui/api/repository'
-import { PlatformServiceInterface } from '@geonetwork-ui/common/domain/platform.service.interface'
 import { TranslateModule } from '@ngx-translate/core'
 import { CatalogRecord } from '@geonetwork-ui/common/domain/model/record'
-import { RouterFacade } from '@geonetwork-ui/feature/router'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'md-editor-search-header',
@@ -33,13 +29,11 @@ export class SearchHeaderComponent {
   activeBtn = false
 
   constructor(
-    public platformService: PlatformServiceInterface,
     private avatarService: AvatarServiceInterface,
-    private searchService: SearchService,
-    private routerFacade: RouterFacade
+    private router: Router
   ) {}
 
   handleItemSelection(item: CatalogRecord) {
-    this.searchService.updateFilters({ any: item.title })
+    this.router.navigate(['edit', item.uniqueIdentifier])
   }
 }
