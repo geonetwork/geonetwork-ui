a3757eae030b8955543b18be86ef0625cecb0286
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-11-13 10:18:19 +0100

    feat(search-filters): display summary and filter labels only if values present

--- apps/metadata-editor/src/app/dashboard/search-filters/search-filters.component.ts
+++ apps/metadata-editor/src/app/dashboard/search-filters/search-filters.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TranslateModule } from '@ngx-translate/core'
-import { FeatureSearchModule } from '@geonetwork-ui/feature/search'
 import {
   NgIconComponent,
   provideIcons,
   provideNgIconsConfig,
 } from '@ng-icons/core'
 import { iconoirFilterList } from '@ng-icons/iconoir'
+import {
+  FeatureSearchModule,
+  SearchFacade,
+} from '@geonetwork-ui/feature/search'
 import { MatIconModule } from '@angular/material/icon'
 import { SearchFiltersSummaryItemComponent } from '../search-filters-summary-item/search-filters-summary-item.component'
+import { map } from 'rxjs'
 
 @Component({
   selector: 'md-editor-search-filters',
@@ -37,10 +41,25 @@ export class SearchFiltersComponent implements OnInit {
   @Input() searchFields: string[] = []
   searchConfig: { fieldName: string; title: string }[]
 
+  searchFilterActive$ = this.searchFacade.searchFilters$.pipe(
+    map((filters) => this.hasNonEmptyValues(filters))
+  )
+
+  constructor(private searchFacade: SearchFacade) {}
+
   ngOnInit(): void {
     this.searchConfig = this.searchFields.map((filter) => ({
       fieldName: filter,
       title: `search.filters.${filter}`,
     }))
+  }
+
+  hasNonEmptyValues(filters: any): boolean {
+    return Object.values(filters).some(
+      (value) =>
+        value !== undefined &&
+        (typeof value !== 'object' ||
+          (typeof value === 'object' && Object.keys(value).length > 0))
+    )
   }
 }
