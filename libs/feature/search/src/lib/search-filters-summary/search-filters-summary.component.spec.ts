85353ee3b57108b2f64d6166710b226b7e60d03b
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-11-26 09:12:55 +0100

    test(search-filter-summary): add unit tests

--- libs/feature/search/src/lib/search-filters-summary/search-filters-summary.component.spec.ts
+++ libs/feature/search/src/lib/search-filters-summary/search-filters-summary.component.spec.ts
@@ -1,21 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { SearchFiltersSummaryComponent } from './search-filters-summary.component'
+import { MockComponent, MockProvider } from 'ng-mocks'
+import { SearchService } from '../utils/service/search.service'
+import { SearchFacade } from '../state/search.facade'
+import { BehaviorSubject, firstValueFrom, of } from 'rxjs'
+import { TranslateModule } from '@ngx-translate/core'
+import { SearchFiltersSummaryItemComponent } from '../search-filters-summary-item/search-filters-summary-item.component'
+import { FieldFilters } from '@geonetwork-ui/common/domain/model/search'
 
+class SearchFacadeMock {
+  searchFilters$ = new BehaviorSubject<FieldFilters>({
+    format: {},
+    isSpatial: {},
+    license: {},
+    'userInfo.keyword': {},
+  })
+}
+class SearchServiceMock {
+  setFilters = jest.fn()
+}
 describe('SearchFiltersSummaryComponent', () => {
   let component: SearchFiltersSummaryComponent
   let fixture: ComponentFixture<SearchFiltersSummaryComponent>
+  let searchFacade: SearchFacade
+  let searchService: SearchService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchFiltersSummaryComponent],
+      imports: [TranslateModule.forRoot()],
+      declarations: [
+        SearchFiltersSummaryComponent,
+        MockComponent(SearchFiltersSummaryItemComponent),
+      ],
+      providers: [
+        MockProvider(SearchFacade, SearchFacadeMock, 'useClass'),
+        MockProvider(SearchService, SearchServiceMock, 'useClass'),
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(SearchFiltersSummaryComponent)
+    searchFacade = TestBed.inject(SearchFacade)
+    searchService = TestBed.inject(SearchService)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should set searchFilterActive$ observable to false for empty filters', async () => {
+    const isActive = await firstValueFrom(component.searchFilterActive$)
+    expect(isActive).toBeFalsy()
+  })
+
+  it('should set searchFilterActive$ observable to true for NON empty value filters', async () => {
+    const filters = {
+      format: {},
+      isSpatial: {},
+      license: {},
+      'userInfo.keyword': { 'admin|admin|admin|Administrator': true },
+    }
+    ;(searchFacade.searchFilters$ as BehaviorSubject<FieldFilters>).next(
+      filters
+    )
+    const isActive = await firstValueFrom(component.searchFilterActive$)
+    expect(isActive).toBeTruthy()
+  })
+
+  it('should set searchFilterActive$ observable to true for NON empty dateRange filters', async () => {
+    const filters = {
+      format: {},
+      isSpatial: {},
+      license: {},
+      changeDate: {
+        start: new Date('2021-01-01'),
+        end: new Date('2021-01-02'),
+      },
+    }
+    ;(searchFacade.searchFilters$ as BehaviorSubject<FieldFilters>).next(
+      filters
+    )
+    const isActive = await firstValueFrom(component.searchFilterActive$)
+    expect(isActive).toBeTruthy()
+  })
+
+  it('should clear filters', () => {
+    component.clearFilters()
+    expect(searchService.setFilters).toHaveBeenCalledWith({})
   })
 })
