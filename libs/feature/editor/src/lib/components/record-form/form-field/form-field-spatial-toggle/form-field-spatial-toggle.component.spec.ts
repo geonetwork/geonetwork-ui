5684c88f0d334986d7ce02193e2f5d2309c514c6
Angelika Kinas <angelika.kinas@camptocamp.com>
2024-12-12 15:12:20 +0100

    chore: fix unit test

--- libs/feature/editor/src/lib/components/record-form/form-field/form-field-spatial-toggle/form-field-spatial-toggle.component.spec.ts
+++ libs/feature/editor/src/lib/components/record-form/form-field/form-field-spatial-toggle/form-field-spatial-toggle.component.spec.ts
@@ -1,21 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { FormFieldSpatialToggleComponent } from './form-field-spatial-toggle.component';
+import { FormFieldSpatialToggleComponent } from './form-field-spatial-toggle.component'
+import { MockProvider } from 'ng-mocks'
+import { EditorFacade } from '../../../../+state/editor.facade'
+import { BehaviorSubject } from 'rxjs'
 
+class EditorFacadeMock {
+  record$ = new BehaviorSubject([])
+}
 describe('FormFieldSpatialToggleComponent', () => {
-  let component: FormFieldSpatialToggleComponent;
-  let fixture: ComponentFixture<FormFieldSpatialToggleComponent>;
+  let component: FormFieldSpatialToggleComponent
+  let fixture: ComponentFixture<FormFieldSpatialToggleComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [FormFieldSpatialToggleComponent]
-    });
-    fixture = TestBed.createComponent(FormFieldSpatialToggleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      imports: [FormFieldSpatialToggleComponent],
+      providers: [MockProvider(EditorFacade, EditorFacadeMock, 'useClass')],
+    })
+    fixture = TestBed.createComponent(FormFieldSpatialToggleComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
