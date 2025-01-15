673c093aef3b00ae5e0dfbbb12c6e9032d0020fc
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-01-15 12:15:09 +0100

    chore: npx storybook@latest upgrade --config-dir apps/demo/.storybook

--- apps/demo/.storybook/preview.js
+++ apps/demo/.storybook/preview.js
@@ -2,3 +2,4 @@ import { ThemeService } from '@geonetwork-ui/util/shared'
 import '!style-loader!css-loader!./styles.css'
 
 ThemeService.applyCssVariables('#c82850', '#001638', '#212029', '#fdfbff')
+export const tags = ['autodocs']
