871ee8dabe036ebfe234504c700fcf17728d136c
mmohad <melodia.mohad@ign.fr>
2025-01-29 18:15:18 +0100

    change(mapper): add tempory GPFDL mapper

--- libs/api/metadata-converter/src/lib/gn4/gn4.field.mapper.ts
+++ libs/api/metadata-converter/src/lib/gn4/gn4.field.mapper.ts
@@ -395,7 +395,7 @@ export class Gn4FieldMapper {
       /^OGC:WFS/.test(protocol) ||
       /^OGC:WMTS/.test(protocol) ||
       /ogc\W*api\W*features/i.test(protocol) ||
-      /^WWW:DOWNLOAD-/.test(protocol)
+      (/^WWW:DOWNLOAD-/.test(protocol) && /data.geopf.fr/.test(url)) // TO DO : change with the good protocol when decided
     ) {
       return 'service'
     }
