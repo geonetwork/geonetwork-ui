ee90676bbb789e98a7b26e12d36c805cb1f349c2
Tobias Kohr <tobias.kohr@camptocamp.com>
2025-02-19 16:46:39 +0100

    feat(table): impove css positioning and keep loading state in table-view.component

--- libs/ui/dataviz/src/lib/table/table.data.source.ts
+++ libs/ui/dataviz/src/lib/table/table.data.source.ts
@@ -6,7 +6,6 @@ import { TableItemModel } from './table.component'
 
 export class TableDataSource implements DataSource<TableItemModel> {
   private dataItems$ = new BehaviorSubject<DataItem[]>([])
-  private loading$ = new BehaviorSubject<boolean>(false)
 
   connect(): Observable<TableItemModel[]> {
     return this.dataItems$.asObservable().pipe(
@@ -21,13 +20,10 @@ export class TableDataSource implements DataSource<TableItemModel> {
 
   disconnect(): void {
     this.dataItems$.complete()
-    this.loading$.complete()
   }
 
   async showData(itemsPromise: Promise<DataItem[]>) {
-    this.loading$.next(true)
     const items = await itemsPromise
-    this.loading$.next(false)
     this.dataItems$.next(items)
   }
 }
