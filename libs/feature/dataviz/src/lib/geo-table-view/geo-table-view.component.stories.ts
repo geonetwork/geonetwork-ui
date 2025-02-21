b08e5293e518cb274ab80653dd9bb8992d1e4172
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-02-21 23:53:34 +0100

    chore(dataviz): remove table-scroll component, use data-table in geo-data-view

--- libs/feature/dataviz/src/lib/geo-table-view/geo-table-view.component.stories.ts
+++ libs/feature/dataviz/src/lib/geo-table-view/geo-table-view.component.stories.ts
@@ -8,27 +8,22 @@ import {
 } from '@storybook/angular'
 import { GeoTableViewComponent } from './geo-table-view.component'
 import { importProvidersFrom } from '@angular/core'
-import {
-  FeatureDetailComponent,
-  MapContainerComponent,
-} from '@geonetwork-ui/ui/map'
 import { pointFeatureCollectionFixture } from '@geonetwork-ui/common/fixtures'
-import { TableComponent } from '@geonetwork-ui/ui/dataviz'
 import { HttpClientModule } from '@angular/common/http'
 import { TRANSLATE_DEFAULT_CONFIG } from '@geonetwork-ui/util/i18n'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {
+  BaseFileReader,
+  DataItem,
+  PropertyInfo,
+} from '@geonetwork-ui/data-fetcher'
 
 export default {
   title: 'Map/GeoTable',
   component: GeoTableViewComponent,
   decorators: [
     moduleMetadata({
-      imports: [
-        FeatureDetailComponent,
-        MapContainerComponent,
-        TableComponent,
-        BrowserAnimationsModule,
-      ],
+      imports: [BrowserAnimationsModule],
     }),
     applicationConfig({
       providers: [
@@ -42,8 +37,22 @@ export default {
   ],
 } as Meta<GeoTableViewComponent>
 
+export class MockBaseReader extends BaseFileReader {
+  override getData(): Promise<{
+    items: DataItem[]
+    properties: PropertyInfo[]
+  }> {
+    return Promise.resolve({
+      items: pointFeatureCollectionFixture().features,
+      properties: [],
+    })
+  }
+}
+const reader = new MockBaseReader('')
+reader.load()
+
 export const Primary: StoryObj<GeoTableViewComponent> = {
   args: {
-    data: pointFeatureCollectionFixture(),
+    dataset: reader,
   },
 }
