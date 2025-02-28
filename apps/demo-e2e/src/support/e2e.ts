c9a29dda0c512ec2d10150280203258de16e6269
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-02-28 10:45:59 +0100

    wip: e2e for wc

---
+++ apps/demo-e2e/src/support/e2e.ts
@@ -0,0 +1,25 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+import { VIEWPORT_SIZE } from '../../../../tools/e2e/settings'
+
+beforeEach(() => {
+  cy.viewport(VIEWPORT_SIZE[0], VIEWPORT_SIZE[1])
+
+  // all tests should show english translations
+  window.localStorage.setItem('geonetwork-ui-language', 'en')
+})
