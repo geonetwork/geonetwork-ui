aee8fbc9e7587b46360ba7b6c9efa20569c42fe2
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-02-28 10:28:58 +0100

    feat: fix and improve search wc

--- apps/webcomponents/src/app/components/gn-search-input/gn-search-input.component.ts
+++ apps/webcomponents/src/app/components/gn-search-input/gn-search-input.component.ts
@@ -19,33 +19,25 @@ import { CatalogRecord } from '@geonetwork-ui/common/domain/model/record'
   encapsulation: ViewEncapsulation.ShadowDom,
   providers: [SearchFacade, SearchService],
 })
-export class GnSearchInputComponent
-  extends BaseComponent
-  implements AfterViewChecked
-{
+export class GnSearchInputComponent extends BaseComponent {
   @Input() openOnSearch: string
   @Input() openOnSelect: string
   @ViewChild('searchInput') searchInput: FuzzySearchComponent
 
-  ngAfterViewChecked() {
-    if (this.openOnSearch) {
-      this.searchInput.inputSubmitted.subscribe(this.search.bind(this))
-    }
-    if (this.openOnSelect) {
-      this.searchInput.itemSelected.subscribe(this.select.bind(this))
-    }
-  }
-
   search(any: string) {
-    const landingPage = this.openOnSearch.replace(/\$\{search}/, any)
-    window.open(landingPage, '_self').focus()
+    if (this.openOnSearch) {
+      const landingPage = this.openOnSearch.replace(/\$\{search}/, any)
+      window.open(landingPage, '_self').focus()
+    }
   }
 
   select(record: CatalogRecord) {
-    const landingPage = this.openOnSelect.replace(
-      /\$\{uuid}/,
-      record.uniqueIdentifier
-    )
-    window.open(landingPage, '_self').focus()
+    if (this.openOnSelect) {
+      const landingPage = this.openOnSelect.replace(
+        /\$\{uuid}/,
+        record.uniqueIdentifier
+      )
+      window.open(landingPage, '_self').focus()
+    }
   }
 }
