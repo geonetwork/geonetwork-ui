4c46cec60c57842c03e37673e72cad3baab313b3
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-03-20 11:23:12 +0100

    feat: rework link card

--- libs/ui/elements/src/lib/link-card/link-card.component.ts
+++ libs/ui/elements/src/lib/link-card/link-card.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
-import { DatasetOnlineResource } from '@geonetwork-ui/common/domain/model/record'
+import {
+  DatasetDownloadDistribution,
+  DatasetOnlineResource,
+  DatasetServiceDistribution,
+} from '@geonetwork-ui/common/domain/model/record'
 import { CommonModule } from '@angular/common'
 import {
   NgIconComponent,
@@ -7,6 +11,8 @@ import {
   provideNgIconsConfig,
 } from '@ng-icons/core'
 import { matOpenInNew } from '@ng-icons/material-icons/baseline'
+import { getBadgeColor, getFileFormat } from '@geonetwork-ui/util/shared'
+import { TranslateModule } from '@ngx-translate/core'
 
 @Component({
   selector: 'gn-ui-link-card',
@@ -14,7 +20,7 @@ import { matOpenInNew } from '@ng-icons/material-icons/baseline'
   styleUrls: ['./link-card.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, NgIconComponent],
+  imports: [CommonModule, NgIconComponent, TranslateModule],
   providers: [
     provideIcons({
       matOpenInNew,
@@ -23,13 +29,42 @@ import { matOpenInNew } from '@ng-icons/material-icons/baseline'
   ],
 })
 export class LinkCardComponent {
+  private _size: 'L' | 'M' | 'S' | 'XS'
   @Input() link: DatasetOnlineResource
-  @Input() compact = false
+  @Input() set size(value: 'L' | 'M' | 'S' | 'XS') {
+    this._size = value
+    switch (value) {
+      case 'L':
+        this.cardClass = 'gn-ui-card-l py-2 px-5'
+        break
+      case 'M':
+        this.cardClass = 'gn-ui-card-m py-2 px-5'
+        break
+      case 'S':
+        this.cardClass = 'gn-ui-card-s p-4'
+        break
+      case 'XS':
+        this.cardClass = 'gn-ui-card-xs py-2 px-5'
+        break
+    }
+  }
+  get size(): 'L' | 'M' | 'S' | 'XS' {
+    return this._size
+  }
+  cardClass = ''
 
   get title() {
     if (this.link.name && this.link.description) {
       return `${this.link.name} | ${this.link.description}`
     }
     return this.link.name || this.link.description || ''
+  }
+
+  getLinkFormat(link: any) {
+    return getFileFormat(link)
+  }
+
+  getLinkColor(link: any) {
+    return getBadgeColor(getFileFormat(link))
   }
 }
