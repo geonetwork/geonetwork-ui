60e3277726d3948b0ad16227f88508c7ef24c8c7
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-03-20 11:23:27 +0100

    feat: rework download item card

--- libs/ui/elements/src/lib/download-item/download-item.component.ts
+++ libs/ui/elements/src/lib/download-item/download-item.component.ts
@@ -9,7 +9,7 @@ import { DatasetOnlineResource } from '@geonetwork-ui/common/domain/model/record
 import { TranslateModule } from '@ngx-translate/core'
 import { CommonModule } from '@angular/common'
 import { NgIcon, provideIcons } from '@ng-icons/core'
-import { matCloudDownloadOutline } from '@ng-icons/material-icons/outline'
+import { iconoirDownload } from '@ng-icons/iconoir'
 
 @Component({
   selector: 'gn-ui-download-item',
@@ -20,15 +20,37 @@ import { matCloudDownloadOutline } from '@ng-icons/material-icons/outline'
   standalone: true,
   viewProviders: [
     provideIcons({
-      matCloudDownloadOutline,
+      iconoirDownload,
     }),
   ],
 })
 export class DownloadItemComponent {
+  private _size: 'L' | 'M' | 'S' | 'XS'
   @Input() link: DatasetOnlineResource
   @Input() color: string
   @Input() format: string
   @Input() isFromApi: boolean
+  @Input() set size(value: 'L' | 'M' | 'S' | 'XS') {
+    this._size = value
+    switch (value) {
+      case 'L':
+        this.cardClass = 'gn-ui-card-l py-2 px-5'
+        break
+      case 'M':
+        this.cardClass = 'gn-ui-card-m py-2 px-5'
+        break
+      case 'S':
+        this.cardClass = 'gn-ui-card-s p-4'
+        break
+      case 'XS':
+        this.cardClass = 'gn-ui-card-xs py-2 px-5'
+        break
+    }
+  }
+  get size(): 'L' | 'M' | 'S' | 'XS' {
+    return this._size
+  }
+  cardClass = ''
   @Output() exportUrl = new EventEmitter<string>()
 
   openUrl() {
