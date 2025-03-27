e8f7f605b302b8034a93381c3181468d23137a77
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-03-27 16:39:40 +0100

    feat: map applicationProfile

--- libs/common/domain/src/lib/model/record/metadata.model.ts
+++ libs/common/domain/src/lib/model/record/metadata.model.ts
@@ -164,6 +164,7 @@ export interface DatasetServiceDistribution {
   name?: string
   description?: string
   translations?: OnlineResourceTranslations
+  applicationProfile?: string
 }
 
 export interface DatasetDownloadDistribution {
@@ -178,6 +179,7 @@ export interface DatasetDownloadDistribution {
   description?: string
   accessServiceProtocol?: ServiceProtocol
   translations?: OnlineResourceTranslations
+  applicationProfile?: string
 }
 
 export interface OnlineLinkResource {
@@ -187,6 +189,7 @@ export interface OnlineLinkResource {
   description?: string
   translations?: OnlineResourceTranslations
   mimeType?: string
+  applicationProfile?: string
 }
 
 export type DatasetOnlineResource = (
