5b95df608924d643c9157cc8231684d06c8c1fa3
Guillaume DE OLIVEIRA <guillaume.deoliveira@camptocamp.com>
2025-03-27 15:42:23 +0100

    Revert "unification du style pour éviter les répétitions"
    This reverts commit 25723949f2ddcb6b749f3abd19defa210b94ea57.

--- apps/datahub/src/app/record/record-section/record-section.component.ts
+++ apps/datahub/src/app/record/record-section/record-section.component.ts
@@ -7,11 +7,8 @@ import { UiLayoutModule } from '@geonetwork-ui/ui/layout'
   standalone: true,
   imports: [CommonModule, UiLayoutModule],
   template: `
-    <section [id]="id" class="container-lg pb-10 mt-5 px-4 lg:mx-auto">
-      <h1
-        *ngIf="title"
-        class="title-font text-title font-medium mr-4 pb-4 text-[28px]"
-      >
+    <section [id]="id" class="container-lg px-4 lg:mx-auto">
+      <h1 *ngIf="title" class="title-font font-medium mb-3 text-[28px]">
         {{ title }}
       </h1>
       <div class="section-content">
