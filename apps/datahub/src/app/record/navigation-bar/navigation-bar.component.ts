3fca785f7991ad57fd32ab573544bceb6adcb231
Tobias Kohr <tobias.kohr@camptocamp.com>
2025-03-28 14:08:30 +0100

    feat(navigation-menu): include favorite-star and language-switcher
    note: star translation breaks one nav-bar is used, star size needs to be fixed as well

--- apps/datahub/src/app/record/navigation-bar/navigation-bar.component.ts
+++ apps/datahub/src/app/record/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,15 @@
 import { CommonModule } from '@angular/common'
-import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core'
 import { marker } from '@biesbjerg/ngx-translate-extract-marker'
-import { SearchService } from '@geonetwork-ui/feature/search'
+import { DatasetRecord } from '@geonetwork-ui/common/domain/model/record'
+import {
+  FavoriteStarComponent,
+  SearchService,
+} from '@geonetwork-ui/feature/search'
+import { LanguageSwitcherComponent } from '@geonetwork-ui/ui/catalog'
 import { NavigationButtonComponent } from '@geonetwork-ui/ui/inputs'
 import { UiLayoutModule } from '@geonetwork-ui/ui/layout'
+import { getGlobalConfig, getThemeConfig } from '@geonetwork-ui/util/app-config'
 import { NgIcon, provideIcons } from '@ng-icons/core'
 import { matExpandMoreOutline } from '@ng-icons/material-icons/outline'
 import { TranslateModule } from '@ngx-translate/core'
@@ -27,10 +33,13 @@ marker('record.metadata.userFeedbacks')
     TranslateModule,
     UiLayoutModule,
     NavigationButtonComponent,
+    LanguageSwitcherComponent,
+    FavoriteStarComponent,
   ],
   viewProviders: [provideIcons({ matExpandMoreOutline })],
 })
 export class NavigationBarComponent {
+  @Input() metadata: DatasetRecord
   displayMobileMenu = false
   anchorLinks = [
     {
@@ -59,6 +68,8 @@ export class NavigationBarComponent {
     },
   ]
   activeLabel = this.anchorLinks[0].label
+  showLanguageSwitcher = getGlobalConfig().LANGUAGES?.length > 0
+  foregroundColor = getThemeConfig().HEADER_FOREGROUND_COLOR || '#ffffff'
 
   constructor(private searchService: SearchService) {}
 
