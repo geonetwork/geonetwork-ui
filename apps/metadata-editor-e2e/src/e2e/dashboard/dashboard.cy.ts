eaa2e305edd69b53fd057bf161a4d3556fc563bf
Guillaume DE OLIVEIRA <guillaume.deoliveira@camptocamp.com>
2025-04-01 17:34:36 +0200

    Display and filter feature catalogs

--- apps/metadata-editor-e2e/src/e2e/dashboard/dashboard.cy.ts
+++ apps/metadata-editor-e2e/src/e2e/dashboard/dashboard.cy.ts
@@ -512,7 +512,7 @@ describe('editing restrictions as non admin', () => {
     // it should have access to their organization records
     cy.get('gn-ui-results-table')
       .find('[data-cy="table-row"]')
-      .eq(3)
+      .eq(4)
       .as('record')
     cy.get('@record')
       .children('div')
--- libs/ui/layout/src/lib/expandable-panel/expandable-panel.component.ts
+++ libs/ui/layout/src/lib/expandable-panel/expandable-panel.component.ts
@@ -1,9 +1,14 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ContentChild,
   ElementRef,
   Input,
+  TemplateRef,
   ViewChild,
+  OnDestroy,
+  ChangeDetectorRef,
+  AfterViewInit,
 } from '@angular/core'
 import { NgIcon, provideIcons } from '@ng-icons/core'
 import { CommonModule } from '@angular/common'
@@ -18,20 +23,49 @@ import { matAdd, matRemove } from '@ng-icons/material-icons/baseline'
   imports: [CommonModule, NgIcon],
   viewProviders: [provideIcons({ matAdd, matRemove })],
 })
-export class ExpandablePanelComponent {
-  @Input() title: string
-  @Input() collapsed = true
-  @ViewChild('contentDiv') contentDiv: ElementRef
-  maxHeight = this.setMaxHeight()
+export class ExpandablePanelComponent implements AfterViewInit, OnDestroy {
+  @Input() title?: string
+  @Input() iconColor? = ''
+  @ContentChild('titleTemplate') titleTemplate?: TemplateRef<HTMLElement>
+  @ViewChild('contentDiv') contentDiv?: ElementRef
+
+  maxHeight = '0px'
+  showContent = true
+  private _collapsed = true
+  private contentObserver?: ResizeObserver
+
+  constructor(private readonly changeDetector: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.contentObserver = new ResizeObserver(() => {
+      this.computeMaxHeight()
+      this.changeDetector.detectChanges()
+    })
+    this.contentObserver.observe(this.contentDiv.nativeElement)
+  }
+
+  @Input() set collapsed(value: boolean) {
+    if (value !== this._collapsed) {
+      this._collapsed = value
+      this.computeMaxHeight()
+    }
+  }
+
+  get collapsed(): boolean {
+    return this._collapsed
+  }
 
   toggle(): void {
     this.collapsed = !this.collapsed
-    this.maxHeight = this.setMaxHeight()
   }
 
-  setMaxHeight() {
-    return `${
-      this.collapsed ? '0' : this.contentDiv.nativeElement.scrollHeight
-    }px`
+  computeMaxHeight(): void {
+    if (!this.contentDiv) return
+    const height = this.contentDiv.nativeElement.scrollHeight
+    this.maxHeight = this.collapsed ? '0px' : `${height}px`
+  }
+
+  ngOnDestroy() {
+    this.contentObserver?.disconnect()
   }
 }
