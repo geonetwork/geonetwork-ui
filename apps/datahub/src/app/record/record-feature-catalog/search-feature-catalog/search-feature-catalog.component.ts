37aaf3d2ecf38c32c635a7607c9be8d59fc3d328
Guillaume DE OLIVEIRA <guillaume.deoliveira@camptocamp.com>
2025-04-02 11:35:02 +0200

    design search compo

--- apps/datahub/src/app/record/record-feature-catalog/search-feature-catalog/search-feature-catalog.component.ts
+++ apps/datahub/src/app/record/record-feature-catalog/search-feature-catalog/search-feature-catalog.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TranslateModule } from '@ngx-translate/core'
-
+import { NgIcon, provideIcons } from '@ng-icons/core'
+import { matSearchOutline } from '@ng-icons/material-icons/outline'
 @Component({
   selector: 'datahub-search-feature-catalog',
   templateUrl: './search-feature-catalog.component.html',
   styleUrls: ['./search-feature-catalog.component.css'],
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, NgIcon, TranslateModule],
+  viewProviders: [provideIcons({ matSearchOutline })],
 })
 export class SearchFeatureCatalogComponent {
-  // Vous pouvez ajouter ici la logique pour les compteurs si nécessaire
+  //filtering logic TODO
 }
