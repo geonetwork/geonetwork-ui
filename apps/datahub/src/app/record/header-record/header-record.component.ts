92fc7d41ddfb71775286b03daa0ddec1dede0c29
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-04-07 09:38:08 +0200

    feat: adaptations to header record

--- apps/datahub/src/app/record/header-record/header-record.component.ts
+++ apps/datahub/src/app/record/header-record/header-record.component.ts
@@ -1,14 +1,30 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
-import { getThemeConfig } from '@geonetwork-ui/util/app-config'
-import { DatasetRecord } from '@geonetwork-ui/common/domain/model/record'
+import {
+  FavoriteStarComponent,
+  SearchService,
+} from '@geonetwork-ui/feature/search'
+import { getGlobalConfig, getThemeConfig } from '@geonetwork-ui/util/app-config'
+import {
+  DatasetRecord,
+  ReuseRecord,
+  ServiceRecord,
+} from '@geonetwork-ui/common/domain/model/record'
 import { MdViewFacade } from '@geonetwork-ui/feature/record'
 import { combineLatest, map } from 'rxjs'
 import { TranslateModule } from '@ngx-translate/core'
 import { BadgeComponent } from '@geonetwork-ui/ui/inputs'
+import { LanguageSwitcherComponent } from '@geonetwork-ui/ui/catalog'
 import { CommonModule } from '@angular/common'
 import { NgIcon, provideIcons } from '@ng-icons/core'
 import { matLocationSearchingOutline } from '@ng-icons/material-icons/outline'
+import { matArrowBack, matCreditCard } from '@ng-icons/material-icons/baseline'
 import { DateService } from '@geonetwork-ui/util/shared'
+import { iconoirAppleShortcuts, iconoirCode } from '@ng-icons/iconoir'
+import { marker } from '@biesbjerg/ngx-translate-extract-marker'
+
+marker('record.kind.dataset')
+marker('record.kind.reuse')
+marker('record.kind.service')
 
 @Component({
   selector: 'datahub-header-record',
@@ -16,11 +32,26 @@ import { DateService } from '@geonetwork-ui/util/shared'
   styleUrls: ['./header-record.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, TranslateModule, BadgeComponent, NgIcon],
-  viewProviders: [provideIcons({ matLocationSearchingOutline })],
+  imports: [
+    CommonModule,
+    LanguageSwitcherComponent,
+    TranslateModule,
+    FavoriteStarComponent,
+    BadgeComponent,
+    NgIcon,
+  ],
+  viewProviders: [
+    provideIcons({
+      matLocationSearchingOutline,
+      matArrowBack,
+      iconoirCode,
+      matCreditCard,
+      iconoirAppleShortcuts,
+    }),
+  ],
 })
 export class HeaderRecordComponent {
-  @Input() metadata: DatasetRecord
+  @Input() metadata: DatasetRecord | ServiceRecord | ReuseRecord
   backgroundCss =
     getThemeConfig().HEADER_BACKGROUND ||
     `center /cover url('assets/img/header_bg.webp')`
@@ -28,7 +59,8 @@ export class HeaderRecordComponent {
 
   constructor(
     public facade: MdViewFacade,
-    private dateService: DateService
+    private dateService: DateService,
+    private searchService: SearchService
   ) {}
 
   isGeodata$ = combineLatest([
@@ -43,5 +75,30 @@ export class HeaderRecordComponent {
 
   get lastUpdate() {
     return this.dateService.formatDate(this.metadata.recordUpdated)
+  }
+
+  back() {
+    this.searchService.updateFilters({})
+  }
+
+  getBadge() {
+    switch (this.metadata.kind) {
+      default:
+      case 'dataset':
+        return {
+          text: 'record.kind.dataset',
+          icon: 'iconoirAppleShortcuts',
+        }
+      case 'reuse':
+        return {
+          text: 'record.kind.reuse',
+          icon: 'matCreditCard',
+        }
+      case 'service':
+        return {
+          text: 'record.kind.service',
+          icon: 'iconoirCode',
+        }
+    }
   }
 }
