91c17aed20e6a46f20fb1bf497b5df9ce29a1193
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-04-24 17:05:33 +0200

    refactor: clean unused isGeodata

--- libs/ui/search/src/lib/results-list-item/results-list-item.component.ts
+++ libs/ui/search/src/lib/results-list-item/results-list-item.component.ts
@@ -27,7 +27,6 @@ export class ResultsListItemComponent implements OnChanges, AfterViewInit {
   @Input() favoriteTemplate: TemplateRef<{ $implicit: CatalogRecord }>
   @Input() metadataQualityDisplay: boolean
   @Input() linkHref: string
-  @Input() isGeodata: boolean
   @Output() mdSelect = new EventEmitter<CatalogRecord>()
   initialized = false
 
@@ -60,7 +59,6 @@ export class ResultsListItemComponent implements OnChanges, AfterViewInit {
       this.mdSelect.emit(record)
     )
     componentFactory.instance.linkHref = this.linkHref
-    componentFactory.instance.isGeodata = this.isGeodata
     componentFactory.changeDetectorRef.detectChanges()
   }
 }
