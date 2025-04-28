830f2a9fff5fabb79192a36404a64f477f2c6bd9
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-04-28 15:32:36 +0200

    fix: uts

--- apps/datahub/src/app/record/record-related-records/record-related-records.component.spec.ts
+++ apps/datahub/src/app/record/record-related-records/record-related-records.component.spec.ts
@@ -1,7 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { RouterFacade } from '@geonetwork-ui/feature/router'
 import { RecordRelatedRecordsComponent } from './record-related-records.component'
 import { MockBuilder } from 'ng-mocks'
+import { ChangeDetectionStrategy } from '@angular/core'
+
+class RouterFacadeMock {
+  goToMetadata = jest.fn()
+}
 
 describe('RelatedRecordsComponent', () => {
   let component: RecordRelatedRecordsComponent
@@ -9,7 +14,17 @@ describe('RelatedRecordsComponent', () => {
 
   beforeEach(() => MockBuilder(RecordRelatedRecordsComponent))
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: RouterFacade,
+          useClass: RouterFacadeMock,
+        },
+      ],
+    })
+      .compileComponents()
+
     fixture = TestBed.createComponent(RecordRelatedRecordsComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
