8332040787e3ae0f31d82438160ecbced10b13da
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-05-07 16:12:31 +0200

    fix: e2e

--- apps/datahub-e2e/src/e2e/datasetDetailPage.cy.ts
+++ apps/datahub-e2e/src/e2e/datasetDetailPage.cy.ts
@@ -764,17 +764,15 @@ describe('dataset pages', () => {
     cy.visit('/dataset/a3774ef6-809d-4dd1-984f-9254f49cbd0a')
 
     // it should display the related records
-    cy.get('#related')
-      .find('datahub-record-related-records')
+    cy.get('datahub-record-related-records')
       .find('gn-ui-related-record-card')
       .should('have.length.gt', 0)
 
     // it should display a similar related record
-    cy.get('#related')
-      .find('datahub-record-related-records')
+    cy.get('datahub-record-related-records')
       .find('gn-ui-related-record-card')
       .first()
-      .find('h4')
+      .find('[data-cy="recordTitle"]')
       .should(
         'have.text',
         ` Metadata for E2E testing purpose. (this title is too long and should be cut, this title is too long and should be cut, this title is too long and should be cut, this title is too long and should be cut, this title is too long and should be cut) `
