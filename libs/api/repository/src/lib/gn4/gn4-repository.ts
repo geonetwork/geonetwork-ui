97a12299c98483a68baeb19fdd4f87ce68d097c3
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-05-07 16:35:01 +0200

    fix: add missing block list

--- libs/api/repository/src/lib/gn4/gn4-repository.ts
+++ libs/api/repository/src/lib/gn4/gn4-repository.ts
@@ -193,7 +193,7 @@ export class Gn4Repository implements RecordsRepositoryInterface {
         'bucket',
         null,
         JSON.stringify(
-          this.gn4SearchHelper.getRelatedRecordPayload(similarTo, 4)
+          this.gn4SearchHelper.getRelatedRecordPayload(similarTo, 10)
         )
       )
       .pipe(
