d36c9466f53862cddb29dddf47500948d56ea9d8
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2025-05-21 13:39:46 +0200

    fix(ME): take(1) on canEdit obs

--- libs/ui/search/src/lib/results-table/results-table.component.ts
+++ libs/ui/search/src/lib/results-table/results-table.component.ts
@@ -35,7 +35,7 @@ import { iconoirUser, iconoirLock, iconoirTranslate } from '@ng-icons/iconoir'
 import { CdkOverlayOrigin, Overlay, OverlayRef } from '@angular/cdk/overlay'
 import { TemplatePortal } from '@angular/cdk/portal'
 import { matMoreVert } from '@ng-icons/material-icons/baseline'
-import { Observable, of } from 'rxjs'
+import { Observable, of, take } from 'rxjs'
 
 @Component({
   selector: 'gn-ui-results-table',
@@ -176,11 +176,13 @@ export class ResultsTableComponent {
   }
 
   handleRecordClick(item: unknown) {
-    this.canEdit(item as CatalogRecord).subscribe((canEdit) => {
-      if (canEdit) {
-        this.recordClick.emit(item as CatalogRecord)
-      }
-    })
+    this.canEdit(item as CatalogRecord)
+      .pipe(take(1))
+      .subscribe((canEdit) => {
+        if (canEdit) {
+          this.recordClick.emit(item as CatalogRecord)
+        }
+      })
   }
 
   handleDuplicate(item: unknown) {
