295a684c837a7c77bb446dd269b45e36a541f7bd
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-05-23 14:07:30 +0200

    refactor(ui): remove translate module from ui/inputs
    also make remaining components standalone & remove root module

--- libs/ui/inputs/src/lib/drag-and-drop-file-input/drag-and-drop-file-input.component.spec.ts
+++ libs/ui/inputs/src/lib/drag-and-drop-file-input/drag-and-drop-file-input.component.spec.ts
@@ -1,16 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { DragAndDropFileInputComponent } from './drag-and-drop-file-input.component'
-import { NgxDropzoneModule } from 'ngx-dropzone'
 
 describe('DragAndDropFileInputComponent', () => {
   let component: DragAndDropFileInputComponent
   let fixture: ComponentFixture<DragAndDropFileInputComponent>
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DragAndDropFileInputComponent],
-      imports: [NgxDropzoneModule],
-    }).compileComponents()
+    await TestBed.configureTestingModule({}).compileComponents()
   })
 
   beforeEach(() => {
