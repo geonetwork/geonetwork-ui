18c23f898b396db87f44b4c16b58e251a467f460
Tobias Kohr <tobias.kohr@camptocamp.com>
2025-07-23 10:27:30 +0200

    feat(metadata-quality-panel): plug in quality calculation

--- libs/feature/editor/src/lib/components/metadata-quality-panel/metadata-quality-panel.component.ts
+++ libs/feature/editor/src/lib/components/metadata-quality-panel/metadata-quality-panel.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { CatalogRecord } from '@geonetwork-ui/common/domain/model/record'
 import { ButtonComponent } from '@geonetwork-ui/ui/inputs'
+import {
+  getQualityValidators,
+  ValidatorMapperKeys,
+} from '@geonetwork-ui/util/shared'
 import {
   NgIconComponent,
   provideIcons,
@@ -8,13 +13,6 @@ import {
 } from '@ng-icons/core'
 import { iconoirBadgeCheck, iconoirSystemShut } from '@ng-icons/iconoir'
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core'
-
-//FIXME: remove mock
-const METADATA_QUALITY_MOCK = {
-  title: true,
-  abstract: true,
-  keywords: false,
-}
 
 @Component({
   selector: 'gn-ui-metadata-quality-panel',
@@ -39,12 +37,27 @@ const METADATA_QUALITY_MOCK = {
   styleUrl: './metadata-quality-panel.component.css',
 })
 export class MetadataQualityPanelComponent {
-  //FIXME: to be replaced by input or data provided by service
-  properties = Object.entries(METADATA_QUALITY_MOCK).map(([key, value]) => ({
-    // use same translations as in fields.config.ts
-    label: `editor.record.form.field.${key}`,
-    value,
-  }))
+  propsToValidate: ValidatorMapperKeys[] = [
+    'title',
+    'description',
+    'keywords',
+    'legalConstraints',
+    'contact',
+    'updateFrequency',
+    'topic',
+    'organisation',
+    'capabilities',
+    'source',
+  ]
+  properties: { label: string; value: boolean }[] = []
+  @Input() set record(value: CatalogRecord) {
+    this.properties = getQualityValidators(value, this.propsToValidate).map(
+      ({ name, validator }) => ({
+        label: `editor.record.form.field.${name}`, // use same translations as in fields.config.ts
+        value: validator(),
+      })
+    )
+  }
 
   getExtraClass(checked: boolean): string {
     const baseClasses =
