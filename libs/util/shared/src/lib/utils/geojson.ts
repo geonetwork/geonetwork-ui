8c3391707497bec35f8a8116b16e092fc1904c8a
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-08-07 13:10:17 +0200

    feat(util): add bbox computing from geojson
    also use it in the feature/map lib

--- libs/util/shared/src/lib/utils/geojson.ts
+++ libs/util/shared/src/lib/utils/geojson.ts
@@ -1,8 +1,11 @@
-import { Feature, FeatureCollection, Geometry } from 'geojson'
+import type { Feature, FeatureCollection, Geometry, Position } from 'geojson'
 
+/**
+ * @returns The geometry if available, otherwise null.
+ */
 export function getGeometryFromGeoJSON(
   data: FeatureCollection | Feature | Geometry
-): Geometry {
+): Geometry | null {
   if (data.type === 'FeatureCollection') {
     return data?.features?.[0]?.geometry || null
   }
@@ -23,4 +26,71 @@ export function getGeometryFromGeoJSON(
     return data || null
   }
   return null
+}
+
+// FIXME: this type should be more generic across the project
+export type BoundingBox = [number, number, number, number]
+
+export function getGeometryBoundingBox(geometry: Geometry): BoundingBox {
+  // use the bounding box if specified in the GeoJSON object
+  if (geometry.bbox) {
+    return geometry.bbox.length > 4
+      ? [geometry.bbox[0], geometry.bbox[1], geometry.bbox[3], geometry.bbox[4]]
+      : (geometry.bbox as BoundingBox)
+  }
+
+  const coordinatesReducer = (prev: BoundingBox, coords: Position) =>
+    [
+      Math.min(prev[0], coords[0]),
+      Math.min(prev[1], coords[1]),
+      Math.max(prev[2], coords[0]),
+      Math.max(prev[3], coords[1]),
+    ] as BoundingBox
+  const coordinatesArrayReducer = (
+    prev: BoundingBox,
+    coordsArray: Position[]
+  ) => {
+    const bbox = coordsArray.reduce(coordinatesReducer, emptyExtent)
+    return [
+      Math.min(prev[0], bbox[0]),
+      Math.min(prev[1], bbox[1]),
+      Math.max(prev[2], bbox[2]),
+      Math.max(prev[3], bbox[3]),
+    ] as BoundingBox
+  }
+  const emptyExtent = [Infinity, Infinity, -Infinity, -Infinity] as BoundingBox
+
+  switch (geometry.type) {
+    case 'MultiPolygon':
+      return geometry.coordinates.reduce((prev, polygonCoords) => {
+        const bbox = polygonCoords.reduce(coordinatesArrayReducer, emptyExtent)
+        return [
+          Math.min(prev[0], bbox[0]),
+          Math.min(prev[1], bbox[1]),
+          Math.max(prev[2], bbox[2]),
+          Math.max(prev[3], bbox[3]),
+        ] as BoundingBox
+      }, emptyExtent)
+    case 'GeometryCollection':
+      return geometry.geometries.reduce((prev, geom) => {
+        const bbox = getGeometryBoundingBox(geom)
+        return [
+          Math.min(prev[0], bbox[0]),
+          Math.min(prev[1], bbox[1]),
+          Math.max(prev[2], bbox[2]),
+          Math.max(prev[3], bbox[3]),
+        ] as BoundingBox
+      }, emptyExtent)
+    case 'Polygon':
+    case 'MultiLineString':
+      return geometry.coordinates.reduce(coordinatesArrayReducer, emptyExtent)
+    case 'LineString':
+    case 'MultiPoint':
+      return geometry.coordinates.reduce<BoundingBox>(
+        coordinatesReducer,
+        emptyExtent
+      )
+    case 'Point':
+      return coordinatesReducer(emptyExtent, geometry.coordinates)
+  }
 }
