536c311bf6b43857a2591cc4927db19baea03795
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-09-11 14:46:15 +0200

    feat: cleanup imports in libs and modules

--- libs/feature/record/src/lib/feature-record.module.ts
+++ libs/feature/record/src/lib/feature-record.module.ts
@@ -3,38 +3,21 @@ import { CommonModule } from '@angular/common'
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
 import { FeatureMapModule } from '@geonetwork-ui/feature/map'
-import { DropdownSelectorComponent } from '@geonetwork-ui/ui/inputs'
 import { MdViewFacade } from './state'
 import { MdViewEffects } from './state/mdview.effects'
 import {
   METADATA_VIEW_FEATURE_STATE_KEY,
   reducer,
 } from './state/mdview.reducer'
-import { MatTabsModule } from '@angular/material/tabs'
-import { TranslateDirective, TranslatePipe } from '@ngx-translate/core'
 import { FeatureCatalogModule } from '@geonetwork-ui/feature/catalog'
-import { DataTableComponent } from '@geonetwork-ui/ui/dataviz'
-import { NgIconsModule, provideNgIconsConfig } from '@ng-icons/core'
 
 @NgModule({
   imports: [
-    CommonModule,
     StoreModule.forFeature(METADATA_VIEW_FEATURE_STATE_KEY, reducer),
     EffectsModule.forFeature([MdViewEffects]),
     FeatureMapModule,
     FeatureCatalogModule,
-    MatTabsModule,
-    TranslateDirective,
-    TranslatePipe,
-    DataTableComponent,
-    NgIconsModule,
-    DropdownSelectorComponent,
   ],
-  providers: [
-    MdViewFacade,
-    provideNgIconsConfig({
-      size: '1.5em',
-    }),
-  ],
+  providers: [MdViewFacade],
 })
 export class FeatureRecordModule {}
