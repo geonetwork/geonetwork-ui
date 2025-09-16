5302eb21bf5968d9ad2fe5fc659a6faae3223e89
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-09-16 22:27:36 +0200

    refactor(editor): simplify feature editor module

--- libs/feature/editor/src/lib/feature-editor.module.ts
+++ libs/feature/editor/src/lib/feature-editor.module.ts
@@ -1,24 +1,19 @@
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http'
+import { provideHttpClient } from '@angular/common/http'
 import { NgModule } from '@angular/core'
-import { MatFormFieldModule } from '@angular/material/form-field'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreModule } from '@ngrx/store'
 import { EditorEffects } from './+state/editor.effects'
 import { EditorFacade } from './+state/editor.facade'
 import * as fromEditor from './+state/editor.reducer'
-import { Gn4PlatformService } from '@geonetwork-ui/api/repository'
 
 @NgModule({
   imports: [
-    MatFormFieldModule,
-    HttpClientModule,
-    HttpClientXsrfModule,
     StoreModule.forFeature(
       fromEditor.EDITOR_FEATURE_KEY,
       fromEditor.editorReducer
     ),
     EffectsModule.forFeature([EditorEffects]),
   ],
-  providers: [EditorFacade, Gn4PlatformService],
+  providers: [EditorFacade, provideHttpClient()],
 })
 export class FeatureEditorModule {}
