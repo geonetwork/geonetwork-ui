731b4d25d07d7e51e8d9c7f9121149216a14c7cf
cmoinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2025-09-17 14:09:41 +0200

    feat: create gn-datahub component
    apps/datahub/src/app/record/record-metadata/record-metadata.component.ts

---
+++ apps/webcomponents/src/app/components/gn-datahub/gn-datahub.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'wc-gn-datahub',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './gn-datahub.component.html',
+  styleUrl: './gn-datahub.component.css',
+})
+export class GnDatahubComponent {}
