a0d26036d74e3230ccb19c5426c34738816e8239
Antoine Abt <antoine@abt.im>
2025-10-22 11:22:08 +0200

    chore: generate stac lib
    with command: nx g @nx/angular:lib --name=stac --directory=libs/data-access/stac

---
+++ libs/data-access/stac/src/lib/stac/stac.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'lib-stac',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stac.component.html',
+  styleUrl: './stac.component.css',
+})
+export class StacComponent {}
