461dbc9b5053e1a03c8e95a486c265e2abd52045
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2025-10-28 17:58:21 +0100

    refactor: remove useless BehaviorSubjects

--- libs/feature/dataviz/src/lib/stac-view/stac-view.component.ts
+++ libs/feature/dataviz/src/lib/stac-view/stac-view.component.ts
@@ -9,7 +9,6 @@ import {
   DatasetServiceDistribution,
   DatasetTemporalExtent,
 } from '@geonetwork-ui/common/domain/model/record'
-import { BehaviorSubject } from 'rxjs'
 import { DatePickerComponent } from '@geonetwork-ui/ui/inputs'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
 import { matDeleteOutline } from '@ng-icons/material-icons/outline'
@@ -26,40 +25,40 @@ import { matDeleteOutline } from '@ng-icons/material-icons/outline'
 export class StacViewComponent implements OnInit {
   @Input() link: DatasetServiceDistribution
 
-  initialTemporalExtent$ = new BehaviorSubject<DatasetTemporalExtent>(null)
-  currentTemporalExtent$ = new BehaviorSubject<DatasetTemporalExtent>(null)
-  isTemporalFilterModified$ = new BehaviorSubject<boolean>(false)
+  initialTemporalExtent: DatasetTemporalExtent | null = null
+  currentTemporalExtent: DatasetTemporalExtent | null = null
+  isTemporalFilterModified = false
 
   onStartDateChange(date: Date) {
-    this.currentTemporalExtent$.next({
+    this.currentTemporalExtent = {
       start: date,
-      end: this.currentTemporalExtent$.value?.end,
-    })
-    this.isTemporalFilterModified$.next(true)
+      end: this.currentTemporalExtent?.end,
+    }
+    this.isTemporalFilterModified = true
   }
 
   onEndDateChange(date: Date) {
-    this.currentTemporalExtent$.next({
-      start: this.currentTemporalExtent$.value?.start,
+    this.currentTemporalExtent = {
+      start: this.currentTemporalExtent?.start,
       end: date,
-    })
-    this.isTemporalFilterModified$.next(true)
+    }
+    this.isTemporalFilterModified = true
   }
 
   onResetFilters() {
-    this.currentTemporalExtent$.next(this.initialTemporalExtent$.value)
-    this.isTemporalFilterModified$.next(false)
+    this.currentTemporalExtent = this.initialTemporalExtent
+    this.isTemporalFilterModified = false
   }
 
   ngOnInit() {
     // TODO: initialExtent pré-saisis sur l'étendue temporelle de la collection
-    // si la collection n’a pas d'étendue temporelle, les champs ne sont pas pré-saisis.
+    // si la collection n'a pas d'étendue temporelle, les champs ne sont pas pré-saisis.
     const initialExtent: DatasetTemporalExtent = {
       start: null,
       end: null,
     }
 
-    this.initialTemporalExtent$.next(initialExtent)
-    this.currentTemporalExtent$.next(initialExtent)
+    this.initialTemporalExtent = initialExtent
+    this.currentTemporalExtent = initialExtent
   }
 }
