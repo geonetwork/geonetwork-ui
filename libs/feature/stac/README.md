e3d9f35901b132135e8acac02cc0f7352cbc0ff5
Tobias Kohr <tobias.kohr@camptocamp.com>
2025-11-04 10:56:07 +0100

    refactor(stac): remove empty stac lib

--- libs/feature/stac/README.md
+++
@@ -1,7 +0,0 @@
-# stac
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test stac` to execute the unit tests.
--- libs/feature/stac/jest.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export default {
-  displayName: 'feature-stac',
-  preset: '../../../jest.preset.js',
-  coverageDirectory: '../../../coverage/libs/feature/stac',
-}
