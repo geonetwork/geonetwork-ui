8dc059bd1051651474430bcac2c34c8404250e17
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-12-23 10:48:27 +0100

    wip

--- libs/util/i18n/src/lib/embedded.translate.loader.ts
+++ libs/util/i18n/src/lib/embedded.translate.loader.ts
@@ -1,14 +1,22 @@
 import { TranslateLoader } from '@ngx-translate/core'
 import { map, Observable, of } from 'rxjs'
 import { dropEmptyTranslations } from './translate.loader.utils.js'
-import de from '../../../../../translations/de.json'
-import en from '../../../../../translations/en.json'
-import es from '../../../../../translations/es.json'
-import fr from '../../../../../translations/fr.json'
-import it from '../../../../../translations/it.json'
-import nl from '../../../../../translations/nl.json'
-import pt from '../../../../../translations/pt.json'
-import sk from '../../../../../translations/sk.json'
+// @ts-expect-error TS2856 -- Import attributes provoke an error; should be fixed when upgrading Typescript
+import de from '../../../../../translations/de.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import en from '../../../../../translations/en.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import es from '../../../../../translations/es.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import fr from '../../../../../translations/fr.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import it from '../../../../../translations/it.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import nl from '../../../../../translations/nl.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import pt from '../../../../../translations/pt.json' with { type: 'json' }
+// @ts-expect-error TS2856
+import sk from '../../../../../translations/sk.json' with { type: 'json' }
 
 export class EmbeddedTranslateLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<Record<string, string>> {
