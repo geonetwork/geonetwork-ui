e1a622a70cec2aa1660673075c6da88ecbb85a4e
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-03-09 14:00:36 +0100

    feat: make datahub completely standalone

--- apps/datahub/src/app/app.component.ts
+++ apps/datahub/src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
 import { getThemeConfig } from '@geonetwork-ui/util/app-config'
-import { ThemeService } from '@geonetwork-ui/util/shared'
+import {
+  handleScrollOnNavigation,
+  ThemeService,
+} from '@geonetwork-ui/util/shared'
+import { SearchRouterContainerDirective } from '@geonetwork-ui/feature/router'
+import { Router, RouterOutlet } from '@angular/router'
+import { CommonModule, ViewportScroller } from '@angular/common'
 
 @Component({
   selector: 'datahub-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SearchRouterContainerDirective],
 })
 export class AppComponent implements OnInit {
+  private router = inject(Router)
+  private viewportScroller = inject(ViewportScroller)
+
   ngOnInit(): void {
+    // Disable automatic scroll restoration to avoid race conditions
+    this.viewportScroller.setHistoryScrollRestoration('manual')
+    handleScrollOnNavigation(this.router, this.viewportScroller)
+    ThemeService.applyCssVariables(
+      getThemeConfig().PRIMARY_COLOR,
+      getThemeConfig().SECONDARY_COLOR,
+      getThemeConfig().MAIN_COLOR,
+      getThemeConfig().BACKGROUND_COLOR,
+      getThemeConfig().MAIN_FONT || "'Rubik', sans-serif",
+      getThemeConfig().TITLE_FONT || "'Readex Pro', sans-serif",
+      getThemeConfig().FONTS_STYLESHEET_URL || 'assets/css/default-fonts.css'
+    )
+    ThemeService.generateBgOpacityClasses(
+      'primary',
+      getThemeConfig().PRIMARY_COLOR,
+      [10, 25, 50, 75, 100]
+    )
+
     const favicon = getThemeConfig().FAVICON
     if (favicon) ThemeService.setFavicon(favicon)
   }
---
+++ apps/datahub/src/app/app.providers.ts
@@ -0,0 +1,173 @@
+import {
+  importProvidersFrom,
+  makeEnvironmentProviders,
+  Provider,
+} from '@angular/core'
+import { getGeometryFromGeoJSON, PROXY_PATH } from '@geonetwork-ui/util/shared'
+import {
+  getGlobalConfig,
+  getMapContextLayerFromConfig,
+  getOptionalMapConfig,
+  getOptionalSearchConfig,
+  getThemeConfig,
+} from '@geonetwork-ui/util/app-config'
+import { LOGIN_URL, METADATA_LANGUAGE } from '@geonetwork-ui/api/repository'
+import {
+  EXTERNAL_VIEWER_OPEN_NEW_TAB,
+  EXTERNAL_VIEWER_URL_TEMPLATE,
+  WEB_COMPONENT_EMBEDDER_URL,
+} from '@geonetwork-ui/feature/record'
+import { THUMBNAIL_PLACEHOLDER } from '@geonetwork-ui/ui/elements'
+import { LANGUAGES_LIST } from '@geonetwork-ui/ui/catalog'
+import {
+  FILTER_GEOMETRY,
+  RECORD_DATASET_URL_TOKEN,
+  RECORD_REUSE_URL_TOKEN,
+  RECORD_SERVICE_URL_TOKEN,
+} from '@geonetwork-ui/feature/search'
+import {
+  DefaultRouterModule,
+  ROUTE_PARAMS,
+  ROUTER_ROUTE_DATASET,
+  ROUTER_ROUTE_ORGANIZATION,
+  ROUTER_ROUTE_REUSE,
+  ROUTER_ROUTE_SEARCH,
+  ROUTER_ROUTE_SERVICE,
+  RouterService,
+} from '@geonetwork-ui/feature/router'
+import {
+  ORGANIZATION_PAGE_URL_TOKEN,
+  ORGANIZATION_URL_TOKEN,
+} from '@geonetwork-ui/feature/catalog'
+import {
+  BASEMAP_LAYERS,
+  DO_NOT_USE_DEFAULT_BASEMAP,
+  MAP_VIEW_CONSTRAINTS,
+} from '@geonetwork-ui/ui/map'
+import {
+  MAX_FEATURE_COUNT,
+  REUSE_FORM_URL,
+} from './record/record-data-preview/record-data-preview.component'
+import { DatahubRouterService } from './router/datahub-router.service'
+import { SearchPageComponent } from './home/search/search-page/search-page.component'
+import { RecordPageComponent } from './record/record-page/record-page.component'
+import { OrganizationPageComponent } from './organization/organization-page/organization-page.component'
+
+export const DATAHUB_ROUTER_PROVIDERS = makeEnvironmentProviders([
+  importProvidersFrom(
+    DefaultRouterModule.forRoot({
+      searchStateId: 'mainSearch',
+      searchRouteComponent: SearchPageComponent,
+      recordRouteComponent: RecordPageComponent,
+      serviceRouteComponent: RecordPageComponent,
+      reuseRouteComponent: RecordPageComponent,
+      organizationRouteComponent: OrganizationPageComponent,
+    })
+  ),
+  { provide: RouterService, useClass: DatahubRouterService },
+])
+
+/**
+ * These providers translate a loaded config from the `@geonetwork-ui/util/app-config` module
+ * into DI token values used throughout the Datahub app
+ */
+export const DATAHUB_CONFIG_PROVIDERS: Array<Provider> = [
+  {
+    provide: PROXY_PATH,
+    useFactory: () => getGlobalConfig().PROXY_PATH,
+  },
+  {
+    provide: LOGIN_URL,
+    useFactory: () => getGlobalConfig().LOGIN_URL,
+  },
+  {
+    provide: WEB_COMPONENT_EMBEDDER_URL,
+    useFactory: () => getGlobalConfig().WEB_COMPONENT_EMBEDDER_URL,
+  },
+  {
+    provide: METADATA_LANGUAGE,
+    useFactory: () => getGlobalConfig().METADATA_LANGUAGE,
+  },
+  {
+    provide: THUMBNAIL_PLACEHOLDER,
+    useFactory: () => getThemeConfig().THUMBNAIL_PLACEHOLDER,
+  },
+  {
+    provide: LANGUAGES_LIST,
+    useFactory: () => getGlobalConfig().LANGUAGES,
+  },
+  {
+    provide: FILTER_GEOMETRY,
+    useFactory: () => {
+      if (getOptionalSearchConfig()?.FILTER_GEOMETRY_DATA) {
+        return Promise.resolve(
+          JSON.parse(getOptionalSearchConfig().FILTER_GEOMETRY_DATA)
+        ).then(getGeometryFromGeoJSON)
+      }
+      if (getOptionalSearchConfig()?.FILTER_GEOMETRY_URL) {
+        return fetch(getOptionalSearchConfig().FILTER_GEOMETRY_URL)
+          .then((resp) => resp.json())
+          .then(getGeometryFromGeoJSON)
+          .catch(() =>
+            console.log(
+              'No spatial filter was applied since a valid geometry could not be found'
+            )
+          )
+      }
+      return null
+    },
+  },
+  {
+    provide: RECORD_DATASET_URL_TOKEN,
+    useValue: `${ROUTER_ROUTE_DATASET}/\${uuid}`,
+  },
+  {
+    provide: RECORD_SERVICE_URL_TOKEN,
+    useValue: `${ROUTER_ROUTE_SERVICE}/\${uuid}`,
+  },
+  {
+    provide: RECORD_REUSE_URL_TOKEN,
+    useValue: `${ROUTER_ROUTE_REUSE}/\${uuid}`,
+  },
+  {
+    provide: ORGANIZATION_PAGE_URL_TOKEN,
+    useValue: `${ROUTER_ROUTE_ORGANIZATION}/\${name}`,
+  },
+  {
+    provide: ORGANIZATION_URL_TOKEN,
+    useValue: `${ROUTER_ROUTE_SEARCH}?${ROUTE_PARAMS.PUBLISHER}=\${name}`,
+  },
+  {
+    provide: DO_NOT_USE_DEFAULT_BASEMAP,
+    useFactory: () => getOptionalMapConfig()?.DO_NOT_USE_DEFAULT_BASEMAP,
+  },
+  {
+    provide: BASEMAP_LAYERS,
+    useFactory: () =>
+      getOptionalMapConfig()?.MAP_LAYERS.map(getMapContextLayerFromConfig) ??
+      [],
+  },
+  {
+    provide: MAP_VIEW_CONSTRAINTS,
+    useFactory: () => ({
+      maxExtent: getOptionalMapConfig()?.MAX_EXTENT,
+      maxZoom: getOptionalMapConfig()?.MAX_ZOOM,
+    }),
+  },
+  {
+    provide: MAX_FEATURE_COUNT,
+    useFactory: () => getOptionalMapConfig()?.MAX_FEATURE_COUNT,
+  },
+  {
+    provide: EXTERNAL_VIEWER_URL_TEMPLATE,
+    useFactory: () => getOptionalMapConfig()?.EXTERNAL_VIEWER_URL_TEMPLATE,
+  },
+  {
+    provide: EXTERNAL_VIEWER_OPEN_NEW_TAB,
+    useFactory: () => getOptionalMapConfig()?.EXTERNAL_VIEWER_OPEN_NEW_TAB,
+  },
+  {
+    provide: REUSE_FORM_URL,
+    useFactory: () => getGlobalConfig().REUSE_FORM_URL,
+  },
+]
