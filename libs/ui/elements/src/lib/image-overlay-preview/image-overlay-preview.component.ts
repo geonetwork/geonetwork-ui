5f12bca05a180ece5b435a8c64c0af3c8cc1920d
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-03-13 11:36:36 +0100

    wip image preview

--- libs/ui/elements/src/lib/image-overlay-preview/image-overlay-preview.component.ts
+++ libs/ui/elements/src/lib/image-overlay-preview/image-overlay-preview.component.ts
@@ -1,10 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
-import * as basicLightbox from 'basiclightbox'
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core'
 import { ContentGhostComponent } from '../content-ghost/content-ghost.component'
 import { ThumbnailComponent } from '../thumbnail/thumbnail.component'
 import { ButtonComponent } from '@geonetwork-ui/ui/inputs'
 import { NgIcon, provideIcons, provideNgIconsConfig } from '@ng-icons/core'
 import { matZoomOutMap } from '@ng-icons/material-icons/baseline'
+import { Overlay, OverlayRef } from '@angular/cdk/overlay'
+import { TemplatePortal } from '@angular/cdk/portal'
 
 @Component({
   selector: 'gn-ui-image-overlay-preview',
@@ -22,7 +32,33 @@ import { matZoomOutMap } from '@ng-icons/material-icons/baseline'
 export class ImageOverlayPreviewComponent {
   @Input() imageUrl: string
   @Output() isPlaceholderShown = new EventEmitter<boolean>()
-  openLightbox(src: string) {
-    basicLightbox.create(`<img src="${src}"/>`).show()
+
+  @ViewChild('imageTemplate') imageTemplate: TemplateRef<HTMLElement>
+
+  private overlay = inject(Overlay)
+  private viewContainerRef = inject(ViewContainerRef)
+  private overlayRef: OverlayRef
+
+  openLightbox() {
+    const positionStrategy = this.overlay
+      .position()
+      .global()
+      .centerHorizontally()
+      .centerVertically()
+    const scrollStrategy = this.overlay.scrollStrategies.block()
+
+    this.overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      backdropClass: 'bg-black/80',
+      positionStrategy,
+      scrollStrategy,
+    })
+    this.overlayRef.backdropClick().subscribe(() => this.overlayRef.dispose())
+    const portal = new TemplatePortal(this.imageTemplate, this.viewContainerRef)
+    this.overlayRef.attach(portal)
+  }
+
+  closeLightbox() {
+    this.overlayRef?.dispose()
   }
 }
