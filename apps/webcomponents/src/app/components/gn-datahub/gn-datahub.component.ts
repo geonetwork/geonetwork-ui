7b263eece107031f5b64d6374b845ac4a65fb39d
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-03-31 12:13:09 +0200

    feat(wc): correctly setup repoitory url for repsitory (gn4)
    for the web components we cannot just call `provideRepositoryUrl`, we need to
    use the global standaloneConfigObject to define the target API.

--- apps/webcomponents/src/app/components/gn-datahub/gn-datahub.component.ts
+++ apps/webcomponents/src/app/components/gn-datahub/gn-datahub.component.ts
@@ -8,19 +8,20 @@ import {
   OnInit,
   ViewEncapsulation,
 } from '@angular/core'
-import { loadAppConfig } from '@geonetwork-ui/util/app-config'
-
-// We need only the providers and app component from the Datahub so we work around this linting rule
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { DATAHUB_CONFIG_PROVIDERS } from '@geonetwork-ui/apps/datahub/app.providers.ts'
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { AppComponent } from '@geonetwork-ui/apps/datahub/app.component.ts'
+import { getGlobalConfig, loadAppConfig } from '@geonetwork-ui/util/app-config'
 import { AsyncPipe } from '@angular/common'
 import { Router } from '@angular/router'
 import { OverlayContainer } from '@angular/cdk/overlay'
 import { WebcomponentOverlayContainer } from '../../webcomponent-overlay-container'
 import { provideGn4 } from '@geonetwork-ui/api/repository'
 import { copyFontFacesToDocument } from '../base.component'
+import { standaloneConfigurationObject } from '../../configuration'
+
+// We need only the providers and app component from the Datahub so we work around this linting rule
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { DATAHUB_CONFIG_PROVIDERS } from '@geonetwork-ui/apps/datahub/app.providers.ts'
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { AppComponent } from '@geonetwork-ui/apps/datahub/app.component.ts'
 
 @Component({
   selector: 'wc-gn-datahub',
@@ -56,6 +57,8 @@ export class GnDatahubComponent implements OnInit {
 
     // once the config is loaded, do initial navigation
     this.configReady$ = loadAppConfig(this.configUrl).then(() => {
+      standaloneConfigurationObject.apiConfiguration.basePath =
+        getGlobalConfig().GN4_API_URL
       this.router.initialNavigation()
       return true
     })
