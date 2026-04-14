a6450331764c411a5f397e5d509aaaf2cc770c1b
Tobias Kohr <tobias.kohr@camptocamp.com>
2026-04-14 13:39:47 +0200

    chore(libs): export metadata.model for DatasetServiceDistribution

--- libs/common/domain/src/index.ts
+++ libs/common/domain/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/organizations.service.interface'
 export * from './lib/platform.service.interface'
 export * from './lib/repository/records-repository.interface'
+export * from './lib/model/record/metadata.model'
