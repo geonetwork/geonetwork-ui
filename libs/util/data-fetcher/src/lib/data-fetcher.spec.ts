02e8531fe97919520566c4399b78411f3490728f
Olivia Guyot <olivia.guyot@camptocamp.com>
2026-08-08 23:50:06 +0200

    feat(data-fetcher): adapt data fetcher entrypoint

--- libs/util/data-fetcher/src/lib/data-fetcher.spec.ts
+++ libs/util/data-fetcher/src/lib/data-fetcher.spec.ts
@@ -1,6 +1,3 @@
-/**
- * @jest-environment jest-fixed-jsdom
- */
 import fetchMock from '@fetch-mock/jest'
 import fs from 'fs'
 import path from 'path'
@@ -20,7 +17,7 @@ jest.mock('@camptocamp/ogc-client', () => ({
       return Promise.resolve(this)
     }
     getVersion() {
-      return '2.0.0'
+      return '1.0.0'
     }
     getFeatureTypes() {
       return [
@@ -50,7 +47,7 @@ jest.mock('@camptocamp/ogc-client', () => ({
       return false
     }
     supportsStartIndex() {
-      return true
+      return false // wfs parsing will fallback to duckdb
     }
   },
 }))
@@ -213,7 +210,7 @@ describe('data-fetcher', () => {
             'Lieu de surveillance : Mnémonique': '001-P-022',
             'Passage : Commentaire': '',
             'Passage : Commentaire de qualification': '',
-            'Passage : Date': new Date('2008-04-15T00:00'),
+            'Passage : Date': new Date('2008-04-15T00:00Z'),
             'Passage : Date de qualification': '',
             'Passage : Date de validation': null,
             'Passage : Niveau de qualité': 'Non qualifié',
@@ -262,11 +259,12 @@ describe('data-fetcher', () => {
         )
         expect(csv[0]).toEqual({
           geometry: null,
-          id: '1',
+          id: 1,
           properties: {
             COMMUNE: 'ANGLEFORT',
             DEP_NOM: 'AIN',
-            DEP_NUM: 1,
+            DEP_NUM: '01',
+            ID: 1,
             FACADE: 'Métropole',
             FRANCE: 'Métropole',
             LAT: null,
@@ -295,16 +293,16 @@ describe('data-fetcher', () => {
         expect(csv[0]).toEqual({
           geometry: null,
           properties: {
-            datentree: '2014/04/28',
+            datentree: new Date('2014-04-28T00:00:00.000Z'),
             datsortie: '',
-            est_ref: 'T',
+            est_ref: true,
             geo_point_2d: '50.6707388795,3.12919014364',
             geo_shape:
               '{"type": "Polygon", "coordinates": [[[3.130236575335796, 50.67090491119582], [3.130196047181812, 50.670893754384174], [3.130125439956143, 50.67088756464525], [3.130060708753599, 50.67086021750229], [3.130054727309224, 50.670857038069045], [3.129984041095332, 50.67082032848472], [3.129973127038841, 50.670815458161684], [3.129913382013352, 50.670804967468996], [3.129842767795712, 50.670796013649756], [3.129772104845962, 50.67077912701344], [3.129737218571207, 50.67077082679582], [3.129701483522717, 50.670767400220804], [3.129630838418297, 50.67075738740604], [3.129560202839928, 50.67074011474696], [3.12952761792054, 50.670726176531744], [3.12948954526489, 50.67071429902424], [3.129418896435583, 50.67070279644338], [3.129354364974042, 50.67068147910331], [3.129348248238403, 50.67068057014457], [3.129277605476768, 50.670671391687776], [3.129206964156518, 50.67065179462064], [3.129169858009355, 50.6706368200518], [3.129136288853051, 50.670629945127125], [3.129065684737461, 50.670624768819], [3.128995011837286, 50.670609308579806], [3.128966316281854, 50.670592153560484], [3.128924340373513, 50.67057790187465], [3.128853691981017, 50.670566443838936], [3.128824779915709, 50.67054743143591], [3.128783029049124, 50.670538303502305], [3.128712354136129, 50.6705164717115], [3.12868056080572, 50.67050270295193], [3.128641707043342, 50.67049445140144], [3.12857109024058, 50.670495251369246], [3.128500491704133, 50.67049215665287], [3.128429889593509, 50.67048766198871], [3.12835926556623, 50.67048563509952], [3.128288709820203, 50.670499267364384], [3.128275587191795, 50.67050312169485], [3.128218178439842, 50.67053350338606], [3.128167295579174, 50.67054811136943], [3.128147653457073, 50.670559223232864], [3.128077100972724, 50.67059079397759], [3.128073096444349, 50.67059307744774], [3.128070443278575, 50.67063795810101], [3.128077229717219, 50.670641235512576], [3.128147891402413, 50.670652397839696], [3.128218487505666, 50.670654460772724], [3.128289102312713, 50.67065279055033], [3.128359682859011, 50.670648769190585], [3.128430305531155, 50.670650176889275], [3.128500899000195, 50.670651207678894], [3.128571488264486, 50.67065059622947], [3.128642121274498, 50.670656033028116], [3.128701270507386, 50.670682157290244], [3.128712786687018, 50.67068510671143], [3.128783433947726, 50.67069607126823], [3.128854051799123, 50.670706569178236], [3.12891010772934, 50.670726818716055], [3.128924729508478, 50.67072936107498], [3.128995355948543, 50.67074317022289], [3.129050063126999, 50.67077154237412], [3.129066072568328, 50.670775554976096], [3.129136689505304, 50.670785630957376], [3.129200468336664, 50.670816263980264], [3.129207394627525, 50.67081897583537], [3.129278022612412, 50.670833305298984], [3.129346609199469, 50.67086096291947], [3.129348714109228, 50.67086130173213], [3.129419329881256, 50.67087085707333], [3.129490003817872, 50.67089199747086], [3.129517575152819, 50.670905662731315], [3.129560669914921, 50.670921016819186], [3.129631330715412, 50.6709479542136], [3.129635728709819, 50.67095040844225], [3.12970200260002, 50.67096822403337], [3.129768060034338, 50.67099514820583], [3.129772666350974, 50.670996247170486], [3.129843333855963, 50.67101477599904], [3.129913942364241, 50.67102140561133], [3.1299845266682382, 50.671007781378414], [3.130016689349797, 50.67099488829572], [3.130055027756231, 50.6709729611134], [3.130125678550235, 50.67097957261391], [3.130196259176135, 50.67097546047528], [3.130230775074116, 50.67094978620116], [3.130236575335796, 50.67090491119582]]]}',
             id_s_inond: 'SIN_1',
             id_tri: 'FRA_TRI_LILL',
             scenario: '01For',
-            typ_inond: 1,
+            typ_inond: '01',
           },
           type: 'Feature',
         })
@@ -322,10 +320,13 @@ describe('data-fetcher', () => {
             code_dep: '34',
             code_epci: 200017341,
             code_region: '76',
+            datasetid: 'perimetre-des-epci-concernes-par-un-contrat-de-ville',
             geo_point_2d: [43.7929180957, 3.37305747018],
             nom_dep: 'HERAULT',
             nom_epci: 'CC Lodévois et Larzac',
             nom_region: 'OCCITANIE',
+            objectid: 25,
+            recordid: '172bdb9d0cd4923786c45994dbf078bfac9cc0dc',
             st_area_shape: 554841824.0549872,
             st_perimeter_shape: 125726.64842881361,
           },
@@ -343,6 +344,7 @@ describe('data-fetcher', () => {
             coordinates: [3.37305747018, 43.7929180957],
             type: 'Point',
           },
+          id: 25,
           properties: {
             code_dep: '34',
             code_epci: 200017341,
@@ -369,7 +371,9 @@ describe('data-fetcher', () => {
           id: 0,
           properties: {
             ENS_POTENT: 'Th 023',
+            FID: 'ENS_CG02.1',
             Gestion: 'o',
+            ID: 0,
             ID_ENS: 'TH 023',
             PERIMETRE: 1448.09054340757,
             SUPERFICIE: 86511.35571961474,
@@ -385,16 +389,15 @@ describe('data-fetcher', () => {
         const gml = await readDataset(
           'http://localfile/fixtures/wfs-gml.xml',
           'gml',
-          { namespace: 'ms:n_mat_eolien_p_r32', wfsVersion: '2.0.0' }
+          { wfsFeatureType: 'ms:n_mat_eolien_p_r32' }
         )
         expect(gml[0]).toEqual({
           type: 'Feature',
           geometry: {
             type: 'Point',
-            coordinates: [1.548145, 50.054755, 0],
+            coordinates: [1.548145, 50.054755],
           },
           properties: {
-            boundedBy: [1.548145, 50.054755, 1.548145, 50.054755],
             id_map: 1862,
             id_mat: 1862,
             nom_parc: 'PARC EOLIEN DE CHASSE MAREE II',
@@ -417,24 +420,16 @@ describe('data-fetcher', () => {
             en_service: 'NON',
             etat_eolie: 'AB',
             alt_base: null,
-            code_icpe: undefined,
-            date_crea: undefined,
             date_decis: null,
-            date_depot: undefined,
             date_maj: null,
-            date_prod: undefined,
-            date_real: undefined,
             diam_rotor: null,
-            exploitant: undefined,
             gardesol: null,
+            gml_id: '',
             ht_nacelle: null,
-            id_parc: undefined,
-            id_pc: undefined,
-            n_parcel: undefined,
-            operateur: undefined,
-            precis_pos: undefined,
-            srce_geom: undefined,
-            sys_coord: undefined,
+            upperCorner: '50.054755 1.548145',
+            lowerCorner: '50.054755 1.548145',
+            operateur: null,
+            sys_coord: null,
             x_pc: null,
             y_pc: null,
           },
@@ -447,18 +442,16 @@ describe('data-fetcher', () => {
           'http://localfile/fixtures/wfs-gml.xml',
           'wfs',
           {
-            namespace: 'ms:n_mat_eolien_p_r32',
-            wfsVersion: '2.0.0',
+            wfsFeatureType: 'ms:n_mat_eolien_p_r32',
           }
         )
         expect(wfs[0]).toEqual({
           type: 'Feature',
           geometry: {
             type: 'Point',
-            coordinates: [1.548145, 50.054755, 0],
+            coordinates: [1.548145, 50.054755],
           },
           properties: {
-            boundedBy: [1.548145, 50.054755, 1.548145, 50.054755],
             id_map: 1862,
             id_mat: 1862,
             nom_parc: 'PARC EOLIEN DE CHASSE MAREE II',
@@ -481,26 +474,18 @@ describe('data-fetcher', () => {
             en_service: 'NON',
             etat_eolie: 'AB',
             alt_base: null,
-            code_icpe: undefined,
-            date_crea: undefined,
             date_decis: null,
-            date_depot: undefined,
             date_maj: null,
-            date_prod: undefined,
-            date_real: undefined,
             diam_rotor: null,
-            exploitant: undefined,
             gardesol: null,
             ht_nacelle: null,
-            id_parc: undefined,
-            id_pc: undefined,
-            n_parcel: undefined,
-            operateur: undefined,
-            precis_pos: undefined,
-            srce_geom: undefined,
-            sys_coord: undefined,
+            operateur: null,
+            sys_coord: null,
             x_pc: null,
             y_pc: null,
+            gml_id: '',
+            lowerCorner: '50.054755 1.548145',
+            upperCorner: '50.054755 1.548145',
           },
         })
       })
@@ -561,6 +546,16 @@ describe('data-fetcher', () => {
       })
       it('does not use ogc-client cache', () => {
         expect(useCache).not.toHaveBeenCalled()
+      })
+    })
+    describe('invalid file', () => {
+      it('throws an explicit error', () => {
+        return expect(
+          readDataset('http://localfile/fixtures/wfs-gml.xml', 'json')
+        ).rejects.toMatchObject({
+          info: expect.stringContaining('Malformed JSON'),
+          type: 'parse',
+        })
       })
     })
   })
