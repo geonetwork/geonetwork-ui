1f15f88a54f2c25b73f31bf7a92a59b599ae42d7
Tobias Kohr <tobias.kohr@camptocamp.com>
2026-08-21 17:38:19 +0200

    fix(date-range-picker): use new date adapter to partially fix typed date formatting

--- libs/ui/inputs/src/lib/date-range-picker/date-range-picker.component.spec.ts
+++ libs/ui/inputs/src/lib/date-range-picker/date-range-picker.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { DateAdapter } from '@angular/material/core'
+import { DateFnsAdapter } from '@angular/material-date-fns-adapter'
 import { DateRangePickerComponent } from './date-range-picker.component'
+import { provideI18n } from '@geonetwork-ui/util/i18n'
 
 describe('DateRangePickerComponent', () => {
   let component: DateRangePickerComponent
@@ -8,6 +11,7 @@ describe('DateRangePickerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DateRangePickerComponent],
+      providers: [provideI18n()],
     }).compileComponents()
 
     fixture = TestBed.createComponent(DateRangePickerComponent)
@@ -17,5 +21,11 @@ describe('DateRangePickerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('parses typed dates with the localized adapter, not Date.parse', () => {
+    expect(fixture.debugElement.injector.get(DateAdapter)).toBeInstanceOf(
+      DateFnsAdapter
+    )
   })
 })
