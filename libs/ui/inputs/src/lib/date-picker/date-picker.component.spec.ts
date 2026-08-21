c14252a1a52de5c3fbe862c25912acb1c3ee9a52
Tobias Kohr <tobias.kohr@camptocamp.com>
2026-08-21 17:38:19 +0200

    fix(date-picker): use new date adapter to fix typed date formatting

--- libs/ui/inputs/src/lib/date-picker/date-picker.component.spec.ts
+++ libs/ui/inputs/src/lib/date-picker/date-picker.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { DateAdapter } from '@angular/material/core'
+import { DateFnsAdapter } from '@angular/material-date-fns-adapter'
 import { DatePickerComponent } from './date-picker.component'
 import { provideI18n } from '@geonetwork-ui/util/i18n'
 
@@ -19,5 +21,11 @@ describe('DatePickerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('parses typed dates with the localized adapter, not Date.parse', () => {
+    expect(fixture.debugElement.injector.get(DateAdapter)).toBeInstanceOf(
+      DateFnsAdapter
+    )
   })
 })
