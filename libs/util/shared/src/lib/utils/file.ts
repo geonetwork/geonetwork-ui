d5cad3fb439500d8ee372632540b2c469622163b
vladyslav-tk <vladyslav.tkachenko@camptocamp.com>
2026-08-27 08:59:42 +0000

    refactoring

---
+++ libs/util/shared/src/lib/utils/file.ts
@@ -0,0 +1,15 @@
+export function isFileExtensionValid(
+  fileName: string,
+  acceptedExtensions: string[]
+): boolean {
+  return acceptedExtensions.some((ext) => fileName.endsWith(ext))
+}
+
+export function readFileAsText(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = () => reject(reader.error)
+    reader.readAsText(file)
+  })
+}
